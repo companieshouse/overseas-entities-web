4cc4166bb93e7bd4843a06ad110a14d1ef300fa7
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-03-21 06:17:42 +0000

    Add auth on presenter page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { get } from "../controllers/index.controller";
 
 import * as config from "../config";
+import {
+  authentication,
+  landing,
+  presenter
+} from "../controllers";
 
 const router = Router();
 
-router.get(config.LANDING_URL, get);
+router.get(config.LANDING_URL, landing.get);
+router.get(config.PRESENTER_URL, authentication, presenter.get);
 
 export default router;

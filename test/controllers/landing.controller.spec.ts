31b20c9feddae6c79368067b8bc3bdde6cc74a1e
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-03-21 10:21:47 +0000

    Fix typo on landing page unit test

--- test/controllers/landing.controller.spec.ts
+++ test/controllers/landing.controller.spec.ts
@@ -7,7 +7,7 @@ import { LANDING_URL } from "../../src/config";
 const INDEX_PAGE_TITLE = "Register an overseas entity and tell us about its beneficial owners";
 
 describe("LANDING controller", () => {
-  test("renders the langind page", async () => {
+  test("renders the landing page", async () => {
     // dispatch a request to the homepage using supertest
     const resp = await request(app).get(LANDING_URL);
 

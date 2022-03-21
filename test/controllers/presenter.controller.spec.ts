6fd722df9eb38203f35eb212aa72676448452795
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-03-21 06:17:30 +0000

    Add placeholder controller and view for presenter page

---
+++ test/controllers/presenter.controller.spec.ts
@@ -0,0 +1,25 @@
+jest.mock("ioredis");
+jest.mock('../../src/controllers/authentication.controller');
+
+import { describe, expect, test, jest } from '@jest/globals';
+import { NextFunction, Request, Response } from "express";
+import request from "supertest";
+
+import app from "../../src/app";
+import { authentication } from "../../src/controllers/authentication.controller";
+import { PRESENTER_URL } from "../../src/config";
+import { signedInCookie } from '../__mocks__/session.mock';
+
+const mockAuthenticationMiddleware = authentication as jest.Mock;
+mockAuthenticationMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
+
+const PRESENTER_PAGE_TITLE = "PRESENTER PAGE";
+
+describe("PRESENTER controller", () => {
+  test("renders the presenter page", async () => {
+    const resp = await request(app).get(PRESENTER_URL).set("Cookie", signedInCookie);
+
+    expect(resp.status).toEqual(200);
+    expect(resp.text).toContain(PRESENTER_PAGE_TITLE);
+  });
+});

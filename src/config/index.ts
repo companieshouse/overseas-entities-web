22e566755a6ea7fe745e00eaf7da1d8b32acbf06
Matthew Culpin <mculpin@companieshouse.gov.uk>
2022-03-21 13:28:20 +0000

    ROE-28 Moving config around

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,11 +13,11 @@ export const APPLICATION_NAME = "overseas-entities-web";
 export const NODE_ENV = process.env["NODE_ENV"];
 export const PORT = getEnvironmentValue("PORT", "3000");
 export const CDN_HOST = getEnvironmentValue("CDN_HOST");
+export const SHOW_SERVICE_OFFLINE_PAGE = getEnvironmentValue("SHOW_SERVICE_OFFLINE_PAGE");
 
 // ROUTING PATH
 export const REGISTER_AN_OVERSEAS_ENTITY_URL = "/register-an-overseas-entity";
 export const LANDING_URL = REGISTER_AN_OVERSEAS_ENTITY_URL;
-export const SHOW_SERVICE_OFFLINE_PAGE = getEnvironmentValue("SHOW_SERVICE_OFFLINE_PAGE");
 
 // TEMPLATE PATH
 export const LANDING_PAGE = "index";

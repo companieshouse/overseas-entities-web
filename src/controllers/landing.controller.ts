7debc2f9eaa342c411cd3bb29a6549e4b5175f58
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-03-21 06:18:09 +0000

    Rename the index controller, test and view to landing

--- src/controllers/landing.controller.ts
+++ src/controllers/landing.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 
+import { logger } from "../utils/logger";
 import * as config from "../config";
 
 export const get = (req: Request, res: Response) => {
+  logger.info(`GET LANDING_PAGE`);
   return res.render(config.LANDING_PAGE);
 };

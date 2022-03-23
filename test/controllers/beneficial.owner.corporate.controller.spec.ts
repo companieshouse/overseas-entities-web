40d56757b764a184d8f2dd22116a43ac223cf246
Matthew Culpin <mculpin@companieshouse.gov.uk>
2022-03-23 16:42:51 +0000

    ROE-79/95 Unit test for the controller

---
+++ test/controllers/beneficial.owner.corporate.controller.spec.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "@jest/globals";
+import request from "supertest";
+import app from "../../src/app";
+import { BENEFIFICAL_OWNER_CORPORATE_URL } from "../../src/config";
+
+const PAGE_TITLE = "Tell us about the corporate beneficial owner";
+
+describe("Beneficial owner corproate controller", () => {
+  test("renders the page", async () => {
+    const resp = await request(app).get(BENEFIFICAL_OWNER_CORPORATE_URL);
+
+    // make some assertions on the response
+    expect(resp.status).toEqual(200);
+    expect(resp.text).toContain(PAGE_TITLE);
+  });
+});

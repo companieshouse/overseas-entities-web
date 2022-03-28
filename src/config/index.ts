3bb9e6869405e025b86592e9bb50ea8d83e44705
mwestacott <mwestacott2@companieshouse.gov.uk>
2022-03-28 09:51:29 +0100

    ROE-101 Add Individual Beneficial Owner Page
    * New page added with appropriate URLs, routing and controller
    * Minor refactoring of config index module to benefit from reuse
    of defined constant names

--- src/config/index.ts
+++ src/config/index.ts
@@ -18,24 +18,26 @@ export const COOKIE_DOMAIN = getEnvironmentValue("COOKIE_DOMAIN");
 export const CACHE_SERVER = getEnvironmentValue("CACHE_SERVER");
 export const SHOW_SERVICE_OFFLINE_PAGE = getEnvironmentValue("SHOW_SERVICE_OFFLINE_PAGE");
 
-// ROUTING PATH
-export const REGISTER_AN_OVERSEAS_ENTITY_URL = "/register-an-overseas-entity";
-
-export const BENEFICIAL_OWNER_TYPE_URL = REGISTER_AN_OVERSEAS_ENTITY_URL + "/beneficial-owner-type";
-export const BENEFICIAL_OWNER_OTHER_URL = REGISTER_AN_OVERSEAS_ENTITY_URL +  "/beneficial-owner-other";
-export const ENTITY_URL = REGISTER_AN_OVERSEAS_ENTITY_URL + "/entity";
-export const LANDING_URL = REGISTER_AN_OVERSEAS_ENTITY_URL;
-export const MANAGING_OFFICER_URL = REGISTER_AN_OVERSEAS_ENTITY_URL + "/managing-officer";
-export const PRESENTER_URL = REGISTER_AN_OVERSEAS_ENTITY_URL + "/presenter";
-
-// TEMPLATE PATH
+// TEMPLATEs
 export const BENEFICIAL_OWNER_OTHER_PAGE = "beneficial-owner-other";
 export const BENEFICIAL_OWNER_TYPE_PAGE = "beneficial-owner-type";
 export const ENTITY_PAGE = "entity";
 export const LANDING_PAGE = "landing";
 export const MANAGING_OFFICER_PAGE = "managing-officer";
 export const PRESENTER_PAGE = "presenter";
+export const BENEFICIAL_OWNER_INDIVIDUAL_PAGE = "beneficial-owner-individual";
 
 export const ERROR_PAGE = "error-page";
 export const NOT_FOUND_PAGE = "page-not-found";
 export const SERVICE_OFFLINE_PAGE = "service-offline";
+
+// ROUTING PATHs
+export const LANDING_URL = "/register-an-overseas-entity";
+
+export const REGISTER_AN_OVERSEAS_ENTITY_URL = LANDING_URL + "/";
+export const BENEFICIAL_OWNER_TYPE_URL = REGISTER_AN_OVERSEAS_ENTITY_URL + BENEFICIAL_OWNER_TYPE_PAGE;
+export const BENEFICIAL_OWNER_OTHER_URL = REGISTER_AN_OVERSEAS_ENTITY_URL +  BENEFICIAL_OWNER_OTHER_PAGE;
+export const ENTITY_URL = REGISTER_AN_OVERSEAS_ENTITY_URL + ENTITY_PAGE;
+export const MANAGING_OFFICER_URL = REGISTER_AN_OVERSEAS_ENTITY_URL + MANAGING_OFFICER_PAGE;
+export const PRESENTER_URL = REGISTER_AN_OVERSEAS_ENTITY_URL + PRESENTER_PAGE;
+export const BENEFICIAL_OWNER_INDIVIDUAL_URL = REGISTER_AN_OVERSEAS_ENTITY_URL + BENEFICIAL_OWNER_INDIVIDUAL_PAGE;

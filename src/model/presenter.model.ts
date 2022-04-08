67a129a5c536b21ee4ebae0fe173bed70f9756dd
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-04-08 19:01:27 +0100

    Update data types for Address, Entity and Presenter

--- src/model/presenter.model.ts
+++ src/model/presenter.model.ts
@@ -3,19 +3,19 @@ export const PresenterKey = "presenter";
   The Presenter fields will have to match the name field on the HTML file to
   be able to do the mapping correctly
 */
-export const PresenterKeys: string[] = ["fullName", "phoneNumber", "role", "roleTitle", "registrationNumber"];
+export const PresenterKeys: string[] = ["full_name", "phone_number", "role", "role_title", "anti_money_laundering_registration_number"];
 export interface Presenter {
-    fullName?: string
-    phoneNumber?: string
+    full_name?: string
+    phone_number?: string
     role?: presenterRole
-    roleTitle?: string
-    registrationNumber?: number
+    role_title?: string
+    anti_money_laundering_registration_number?: string
 }
 
 enum presenterRole {
-  administrator,
-  agent,
-  solicitor,
-  beneficialOwner,
-  other
+  administrator = "administrator",
+  agent = "agent",
+  solicitor = "solicitor",
+  beneficial_owner = "beneficial_owner",
+  other = "other"
 }

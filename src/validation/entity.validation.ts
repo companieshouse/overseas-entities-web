c21442b6191e432369d43043c9a3e4920d46021b
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-04-26 16:34:25 +0100

    Add custom check for conditional fields

--- src/validation/entity.validation.ts
+++ src/validation/entity.validation.ts
@@ -1,4 +1,5 @@
 import { check } from "express-validator";
+import { logger } from "../utils/logger";
 
 import { ErrorMessages } from "./error.messages";
 
@@ -7,9 +8,20 @@ export const entityValidator = [
   check("incorporation_country").not().isEmpty({ ignore_whitespace: true }).withMessage(`${ErrorMessages.MANDANDORY_FIELD}`),
   check("principalAddressLine1").not().isEmpty({ ignore_whitespace: true }).withMessage(`${ErrorMessages.MANDANDORY_FIELD}`),
   check("principalAddressTown").not().isEmpty({ ignore_whitespace: true }).withMessage(`${ErrorMessages.MANDANDORY_FIELD}`),
-  check("principalAddressCountry").not().isEmpty({ ignore_whitespace: true }).withMessage(`${ErrorMessages.MANDANDORY_FIELD}`),
   check("principalAddressPostcode").not().isEmpty({ ignore_whitespace: true }).isLength({ max: 20 }).withMessage(`${ErrorMessages.MANDANDORY_FIELD}`),
   check("is_service_address_same_as_principal_address").not().isEmpty({ ignore_whitespace: true }).withMessage(`${ErrorMessages.SELECT_ONE_OPTION}`),
+  check("serviceAddressLine1").custom((value, { req }) => {
+    logger.debug(`VALUE: ${value}, BODY: ${JSON.stringify(req.body, null, 4)}`);
+    return req.body.is_service_address_same_as_principal_address !== '0' ? true : value;
+  }).not().isEmpty({ ignore_whitespace: true }).withMessage(`${ErrorMessages.MANDANDORY_FIELD}`),
+  check("serviceAddressTown").custom((value, { req }) => {
+    logger.debug(`VALUE: ${value}, BODY: ${JSON.stringify(req.body, null, 4)}`);
+    return req.body.is_service_address_same_as_principal_address !== '0' ? true : value;
+  }).not().isEmpty({ ignore_whitespace: true }).withMessage(`${ErrorMessages.MANDANDORY_FIELD}`),
+  check("serviceAddressPostcode").custom((value, { req }) => {
+    logger.debug(`VALUE: ${value}, BODY: ${JSON.stringify(req.body, null, 4)}`);
+    return req.body.is_service_address_same_as_principal_address !== '0' ? true : value;
+  }).not().isEmpty({ ignore_whitespace: true }).isLength({ max: 20 }).withMessage(`${ErrorMessages.MANDANDORY_FIELD}`),
   check('email').isEmail(),
   check("legal_form").not().isEmpty({ ignore_whitespace: true }).withMessage(`${ErrorMessages.MANDANDORY_FIELD}`),
   check("law_governed").not().isEmpty({ ignore_whitespace: true }).withMessage(`${ErrorMessages.MANDANDORY_FIELD}`),
--- src/validation/error.messages.ts
+++ src/validation/error.messages.ts
@@ -1,5 +1,5 @@
 export enum ErrorMessages {
     MANDANDORY_FIELD = "is a mandandory field",
-    SELECT_ONE_OPTION = "select on option",
+    SELECT_ONE_OPTION = "select an option",
     SELECT_ONE_FIELD = "select at least one field"
   }

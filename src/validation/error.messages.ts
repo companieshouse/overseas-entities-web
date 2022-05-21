f196da7afe18a46471db2787f3d145a17692192e
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-05-21 18:09:18 +0100

    Add error messages

---
+++ src/validation/error.messages.ts
@@ -0,0 +1,29 @@
+export enum ErrorMessages {
+  ENTITY_NAME = "Enter the name of the overseas entity",
+  MANAGING_OFFICER_CORPORATE_NAME = "Enter the corporate managing officer's name",
+  EMAIL = "Enter an email address",
+  LEGAL_FORM = "Enter the legal form",
+  LAW_GOVERNED = "Enter the governing law",
+  FULL_NAME = "Enter a full name",
+  FIRST_NAME = "Enter the individual person's first name",
+  LAST_NAME = "Enter the individual person's last name",
+  ROLE = "Enter a description of the individual person's role and responsibilities",
+  NATIONALITY = "Enter the individual person's nationality",
+  // Public Register
+  PUBLIC_REGISTER_NAME = "Enter the name of the register",
+  PUBLIC_REGISTER_NUMBER = "Enter the registration number",
+  // Address
+  PROPERTY_NAME_OR_NUMBER = "Enter a property name or number",
+  ADDRESS_LINE1 = "Enter an address",
+  CITY_OR_TOWN = "Enter a city or town",
+  COUNTRY = "Enter a country",
+  // Date
+  DAY = "Date must include a day ",
+  MONTH = "Date must include a month",
+  YEAR = "Date must include a year",
+  // No radio selected
+  SELECT_IF_REGISTER_IN_COUNTRY_FORMED_IN = "Select yes if the overseas entity is already on a public register in the country it was formed in",
+  SELECT_IF_SERVICE_ADDRESS_SAME_AS_PRINCIPAL_ADDRESS = "Select yes if the correspondence address the same as the principal or registered office address",
+  SELECT_IF_ANY_BENEFICIAL_OWNERS_BEEN_IDENTIFIED = "Select if any beneficial owners have been identified",
+  SELECT_THE_TYPE_OF_BENEFICIAL_OWNER_OR_MANAGING_OFFICER_YOU_WANT_TO_ADD = "Select the type of beneficial owner or managing officer you want to add",
+}

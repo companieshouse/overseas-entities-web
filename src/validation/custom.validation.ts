6f5ba195665559402581537cc5dee8692364b49b
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-05-24 17:49:51 +0100

    Fix function parameters with default values should be last

--- src/validation/custom.validation.ts
+++ src/validation/custom.validation.ts
@@ -1,13 +1,13 @@
 // Custom validation utils - For now checking is not empty
 
-export const checkFieldIfRadioButtonSelected = (selected: boolean, value: string = "", errMsg: string) => {
+export const checkFieldIfRadioButtonSelected = (selected: boolean, errMsg: string, value: string = "") => {
   if ( selected && !value.trim() ) {
     throw new Error(errMsg);
   }
   return true;
 };
 
-export const checkDate = (day: string = "", month: string = "", year: string = "", errMsg: string) => {
+export const checkDate = (errMsg: string, day: string = "", month: string = "", year: string = "") => {
   if ( !day.trim() || !month.trim() || !year.trim() ) {
     throw new Error(errMsg);
   }
--- src/validation/entity.validation.ts
+++ src/validation/entity.validation.ts
@@ -11,14 +11,14 @@ export const entity = [
   body("principal_address_town").not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.CITY_OR_TOWN),
   body("principal_address_country").not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.COUNTRY),
   body("is_service_address_same_as_principal_address").not().isEmpty().withMessage(ErrorMessages.SELECT_IF_SERVICE_ADDRESS_SAME_AS_PRINCIPAL_ADDRESS),
-  body("service_address_property_name_number").custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_service_address_same_as_principal_address === '0', value, ErrorMessages.PROPERTY_NAME_OR_NUMBER) ),
-  body("service_address_line_1").custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_service_address_same_as_principal_address === '0', value, ErrorMessages.ADDRESS_LINE1) ),
-  body("service_address_town").custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_service_address_same_as_principal_address === '0', value, ErrorMessages.CITY_OR_TOWN) ),
-  body("service_address_country").custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_service_address_same_as_principal_address === '0', value, ErrorMessages.COUNTRY) ),
+  body("service_address_property_name_number").custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_service_address_same_as_principal_address === '0', ErrorMessages.PROPERTY_NAME_OR_NUMBER, value) ),
+  body("service_address_line_1").custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_service_address_same_as_principal_address === '0', ErrorMessages.ADDRESS_LINE1, value) ),
+  body("service_address_town").custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_service_address_same_as_principal_address === '0', ErrorMessages.CITY_OR_TOWN, value) ),
+  body("service_address_country").custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_service_address_same_as_principal_address === '0', ErrorMessages.COUNTRY, value) ),
   body('email').not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.EMAIL),
   body("legal_form").not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.LEGAL_FORM),
   body("law_governed").not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.LAW_GOVERNED),
   body("is_on_register_in_country_formed_in").not().isEmpty().withMessage(ErrorMessages.SELECT_IF_REGISTER_IN_COUNTRY_FORMED_IN),
-  body("public_register_name").custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', value, ErrorMessages.PUBLIC_REGISTER_NAME) ),
-  body("registration_number").custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', value, ErrorMessages.PUBLIC_REGISTER_NUMBER) )
+  body("public_register_name").custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.PUBLIC_REGISTER_NAME, value) ),
+  body("registration_number").custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.PUBLIC_REGISTER_NUMBER, value) )
 ];

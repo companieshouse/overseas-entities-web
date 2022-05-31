dd3ad1d549107da4adeb0f9f7c5981d1d625a12d
mattch1 <mculpin@companieshouse.gov.uk>
2022-05-31 11:51:38 +0100

    ROE-431 Add error text and updates to controller and template

--- src/controllers/secure.register.filter.controller.ts
+++ src/controllers/secure.register.filter.controller.ts
@@ -1,18 +1,26 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import * as config from "../config";
+import { logger } from "../utils/logger";
 
 export const get = (req: Request, res: Response) => {
-  res.render(config.SECURE_REGISTER_FILTER_PAGE, {
+  logger.debugRequest(req, `GET ${config.SECURE_REGISTER_FILTER_PAGE}`);
+  return res.render(config.SECURE_REGISTER_FILTER_PAGE, {
     backLinkUrl: config.LANDING_URL
   });
 };
 
-export const post = (req: Request, res: Response) => {
-  if (req.body.secureRegister === "yes") {
-    return res.render(config.USE_PAPER_PAGE, {
-      backLinkUrl: config.SECURE_REGISTER_FILTER_URL
-    });
-  } else {
-    res.redirect(config.INTERRUPT_CARD_URL);
+export const post = (req: Request, res: Response, next: NextFunction) => {
+  logger.debugRequest(req, `POST ${config.SECURE_REGISTER_FILTER_PAGE}`);
+  try {
+    if (req.body.secure_register === '1') {
+      return res.render(config.USE_PAPER_PAGE, {
+        backLinkUrl: config.SECURE_REGISTER_FILTER_URL
+      });
+    } else if (req.body.secure_register === '0') {
+      return res.redirect(config.INTERRUPT_CARD_URL);
+    }
+  } catch (error) {
+    logger.errorRequest(req, error);
+    next(error);
   }
 };

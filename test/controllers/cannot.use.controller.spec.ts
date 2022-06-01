72d5a00c062433f47b0553ab6183df9e18d3f400
mattch1 <mculpin@companieshouse.gov.uk>
2022-06-01 09:13:34 +0100

    ROE-431 Outer describe for unit tests

--- test/controllers/cannot.use.controller.spec.ts
+++ test/controllers/cannot.use.controller.spec.ts
@@ -3,11 +3,13 @@ import * as config from "../../src/config";
 import request from "supertest";
 import app from "../../src/app";
 
-describe("GET tests", () => {
-  test(`renders the ${config.CANNOT_USE_PAGE} page`, async () => {
-    const resp = await request(app)
-      .get(config.CANNOT_USE_URL);
-    expect(resp.status).toEqual(200);
-    expect(resp.text).toContain("You cannot use this service");
+describe("CANNOT USE controller", () => {
+  describe("GET tests", () => {
+    test(`renders the ${config.CANNOT_USE_PAGE} page`, async () => {
+      const resp = await request(app)
+        .get(config.CANNOT_USE_URL);
+      expect(resp.status).toEqual(200);
+      expect(resp.text).toContain("You cannot use this service");
+    });
   });
 });
--- test/controllers/use.paper.controller.spec.ts
+++ test/controllers/use.paper.controller.spec.ts
@@ -3,11 +3,13 @@ import * as config from "../../src/config";
 import request from "supertest";
 import app from "../../src/app";
 
-describe("GET tests", () => {
-  test(`renders the ${config.USE_PAPER_URL} page`, async () => {
-    const resp = await request(app)
-      .get(config.USE_PAPER_URL);
-    expect(resp.status).toEqual(200);
-    expect(resp.text).toContain("You cannot use this service");
+describe("USE PAPER controller", () => {
+  describe("GET tests", () => {
+    test(`renders the ${config.USE_PAPER_URL} page`, async () => {
+      const resp = await request(app)
+        .get(config.USE_PAPER_URL);
+      expect(resp.status).toEqual(200);
+      expect(resp.text).toContain("You cannot use this service");
+    });
   });
 });

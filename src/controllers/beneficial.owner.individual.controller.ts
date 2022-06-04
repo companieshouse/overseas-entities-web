cccfb4c52e6335cdf1e794f7145abce9ac21d45c
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-06-04 14:23:55 +0100

    Update Beneficial Owner Individual controller with remove, update and get populated data

--- src/controllers/beneficial.owner.individual.controller.ts
+++ src/controllers/beneficial.owner.individual.controller.ts
@@ -1,15 +1,20 @@
 import { NextFunction, Request, Response } from "express";
 import { v4 as uuidv4 } from 'uuid';
 
-import { getFromApplicationData, mapFieldsToDataObject, prepareData, removeFromApplicationData, setApplicationData } from "../utils/application.data";
-import { ApplicationDataType } from "../model";
-import { logger } from "../utils/logger";
-import * as config from "../config";
-import { BeneficialOwnerIndividualKey, BeneficialOwnerIndividualKeys } from "../model/beneficial.owner.individual.model";
+import {
+  getFromApplicationData,
+  mapDataObjectToFields,
+  mapFieldsToDataObject,
+  mapNOCObjectToFields,
+  prepareData,
+  removeFromApplicationData,
+  setApplicationData,
+} from "../utils/application.data";
 import {
   AddressKeys,
   BeneficialOwnerNoc,
   HasSameResidentialAddressKey,
+  ID,
   InputDateKeys,
   IsOnSanctionsListKey,
   NonLegalFirmNoc,
@@ -27,43 +32,77 @@ import {
   StartDateKey,
   StartDateKeys,
 } from "../model/date.model";
+import { logger } from "../utils/logger";
+import { ApplicationDataType } from "../model";
+import { BENEFICIAL_OWNER_INDIVIDUAL_PAGE, BENEFICIAL_OWNER_TYPE_URL } from "../config";
+import { BeneficialOwnerIndividualKey, BeneficialOwnerIndividualKeys } from "../model/beneficial.owner.individual.model";
 
 export const get = (req: Request, res: Response) => {
-  logger.debugRequest(req, `GET ${config.BENEFICIAL_OWNER_INDIVIDUAL_PAGE}`);
+  logger.debugRequest(req, `GET ${BENEFICIAL_OWNER_INDIVIDUAL_PAGE}`);
 
-  const { id } = req.params;
-
-  return res.render(config.BENEFICIAL_OWNER_INDIVIDUAL_PAGE, {
-    id,
-    backLinkUrl: config.BENEFICIAL_OWNER_TYPE_URL,
-    ...getFromApplicationData(req.session, BeneficialOwnerIndividualKey, id)
+  return res.render(BENEFICIAL_OWNER_INDIVIDUAL_PAGE, {
+    backLinkUrl: BENEFICIAL_OWNER_TYPE_URL
   });
+};
+
+export const getByID = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    logger.debugRequest(req, `GET ${BENEFICIAL_OWNER_INDIVIDUAL_PAGE}`);
+
+    const id = req.params[ID];
+    const data = getFromApplicationData(req.session, BeneficialOwnerIndividualKey, id);
+
+    const usualResidentialAddress = (data) ? mapDataObjectToFields(data[UsualResidentialAddressKey], UsualResidentialAddressKeys, AddressKeys) : {};
+    const serviceAddress = (data) ? mapDataObjectToFields(data[ServiceAddressKey], ServiceAddressKeys, AddressKeys) : {};
+    const dobDate = (data) ? mapDataObjectToFields(data[DateOfBirthKey], DateOfBirthKeys, InputDateKeys) : {};
+    const startDate = (data) ? mapDataObjectToFields(data[StartDateKey], StartDateKeys, InputDateKeys) : {};
+    const nocFields = (data) ? mapNOCObjectToFields(data) : {}; // Needed to map array of a single field to a string
+
+    return res.render(BENEFICIAL_OWNER_INDIVIDUAL_PAGE, {
+      backLinkUrl: BENEFICIAL_OWNER_TYPE_URL,
+      id,
+      ...data,
+      ...usualResidentialAddress,
+      ...serviceAddress,
+      ...nocFields,
+      [DateOfBirthKey]: dobDate,
+      [StartDateKey]: startDate
+    });
+  } catch (error) {
+    logger.errorRequest(req, error);
+    next(error);
+  }
 };
 
 export const post = (req: Request, res: Response, next: NextFunction) => {
   try {
-    logger.debugRequest(req, `POST ${config.BENEFICIAL_OWNER_INDIVIDUAL_PAGE}`);
+    logger.debugRequest(req, `POST ${BENEFICIAL_OWNER_INDIVIDUAL_PAGE}`);
 
-    const data: ApplicationDataType = prepareData(req.body, BeneficialOwnerIndividualKeys);
-
-    data[UsualResidentialAddressKey] = mapFieldsToDataObject(req.body, UsualResidentialAddressKeys, AddressKeys);
-    data[ServiceAddressKey] = mapFieldsToDataObject(req.body, ServiceAddressKeys, AddressKeys);
-    data[DateOfBirthKey] = mapFieldsToDataObject(req.body, DateOfBirthKeys, InputDateKeys);
-    data[StartDateKey] = mapFieldsToDataObject(req.body, StartDateKeys, InputDateKeys);
-
-    // It needs concatenations because if in the check boxes we select only one option
-    // nunjucks returns just a string and with concat we will return an array.
-    data[BeneficialOwnerNoc] = (data[BeneficialOwnerNoc]) ? [].concat(data[BeneficialOwnerNoc]) : [];
-    data[TrusteesNoc] = (data[TrusteesNoc]) ? [].concat(data[TrusteesNoc]) : [];
-    data[NonLegalFirmNoc] = (data[NonLegalFirmNoc]) ? [].concat(data[NonLegalFirmNoc]) : [];
-
-    data[HasSameResidentialAddressKey] = (data[HasSameResidentialAddressKey]) ? +data[HasSameResidentialAddressKey] : '';
-    data[IsOnSanctionsListKey] = (data[IsOnSanctionsListKey]) ? +data[IsOnSanctionsListKey] : '';
-    data["id"] = uuidv4();
+    const data: ApplicationDataType = setBeneficialOwnerData(req.body, uuidv4());
 
     setApplicationData(req.session, data, BeneficialOwnerIndividualKey);
 
-    return res.redirect(config.BENEFICIAL_OWNER_TYPE_URL);
+    return res.redirect(BENEFICIAL_OWNER_TYPE_URL);
+  } catch (error) {
+    logger.errorRequest(req, error);
+    next(error);
+  }
+};
+
+export const update = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    logger.debugRequest(req, `UPDATE ${BENEFICIAL_OWNER_INDIVIDUAL_PAGE}`);
+
+    // Remove old Beneficial Owner
+    removeFromApplicationData(req.session, BeneficialOwnerIndividualKey, req.params[ID]);
+
+    // Set Beneficial Owner data
+    const data: ApplicationDataType = setBeneficialOwnerData(req.body, req.params[ID]);
+
+    // Save new Beneficial Owner
+    setApplicationData(req.session, data, BeneficialOwnerIndividualKey);
+
+    return res.redirect(BENEFICIAL_OWNER_TYPE_URL);
   } catch (error) {
     logger.errorRequest(req, error);
     next(error);
@@ -72,13 +111,35 @@ export const post = (req: Request, res: Response, next: NextFunction) => {
 
 export const remove = (req: Request, res: Response, next: NextFunction) => {
   try {
-    logger.debug(`REMOVE ${config.MANAGING_OFFICER_PAGE}`);
+    logger.debug(`REMOVE ${BENEFICIAL_OWNER_INDIVIDUAL_PAGE}`);
 
-    removeFromApplicationData(req.session, BeneficialOwnerIndividualKey, req.params.id);
+    removeFromApplicationData(req.session, BeneficialOwnerIndividualKey, req.params[ID]);
 
-    return res.redirect(config.BENEFICIAL_OWNER_TYPE_URL);
+    return res.redirect(BENEFICIAL_OWNER_TYPE_URL);
   } catch (error) {
     logger.errorRequest(req, error);
     next(error);
   }
+};
+
+const setBeneficialOwnerData = (reqBody: any, id: string): ApplicationDataType => {
+  const data: ApplicationDataType = prepareData(reqBody, BeneficialOwnerIndividualKeys);
+
+  data[UsualResidentialAddressKey] = mapFieldsToDataObject(reqBody, UsualResidentialAddressKeys, AddressKeys);
+  data[ServiceAddressKey] = mapFieldsToDataObject(reqBody, ServiceAddressKeys, AddressKeys);
+  data[DateOfBirthKey] = mapFieldsToDataObject(reqBody, DateOfBirthKeys, InputDateKeys);
+  data[StartDateKey] = mapFieldsToDataObject(reqBody, StartDateKeys, InputDateKeys);
+
+  // It needs concatenations because if in the check boxes we select only one option
+  // nunjucks returns just a string and with concat we will return an array.
+  data[BeneficialOwnerNoc] = (data[BeneficialOwnerNoc]) ? [].concat(data[BeneficialOwnerNoc]) : [];
+  data[TrusteesNoc] = (data[TrusteesNoc]) ? [].concat(data[TrusteesNoc]) : [];
+  data[NonLegalFirmNoc] = (data[NonLegalFirmNoc]) ? [].concat(data[NonLegalFirmNoc]) : [];
+
+  data[HasSameResidentialAddressKey] = (data[HasSameResidentialAddressKey]) ? +data[HasSameResidentialAddressKey] : '';
+  data[IsOnSanctionsListKey] = (data[IsOnSanctionsListKey]) ? +data[IsOnSanctionsListKey] : '';
+
+  data[ID] = id;
+
+  return data;
 };

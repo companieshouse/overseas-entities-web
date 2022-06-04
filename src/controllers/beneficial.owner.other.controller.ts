96f64aeeedffc1ed51b2ca90d27b53b7872fa592
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-06-04 14:23:48 +0100

    Update Beneficial Owner Corporate controller with remove, update and get populated data

--- src/controllers/beneficial.owner.other.controller.ts
+++ src/controllers/beneficial.owner.other.controller.ts
@@ -1,27 +1,66 @@
 import { NextFunction, Request, Response } from "express";
 import { v4 as uuidv4 } from 'uuid';
 
-import * as config from "../config";
+import {
+  getFromApplicationData,
+  mapDataObjectToFields,
+  mapFieldsToDataObject,
+  mapNOCObjectToFields,
+  prepareData,
+  removeFromApplicationData,
+  setApplicationData,
+} from "../utils/application.data";
+import {
+  AddressKeys,
+  BeneficialOwnerNoc,
+  HasSamePrincipalAddressKey,
+  ID,
+  InputDateKeys,
+  IsOnRegisterInCountryFormedInKey,
+  IsOnSanctionsListKey,
+  NonLegalFirmNoc,
+  TrusteesNoc,
+} from "../model/data.types.model";
+import {
+  PrincipalAddressKey,
+  PrincipalAddressKeys,
+  ServiceAddressKey,
+  ServiceAddressKeys,
+} from "../model/address.model";
 import { logger } from "../utils/logger";
 import { ApplicationDataType } from "../model";
-import { getFromApplicationData, mapFieldsToDataObject, prepareData, removeFromApplicationData, setApplicationData } from "../utils/application.data";
-import { BeneficialOwnerOtherKey, BeneficialOwnerOtherKeys } from "../model/beneficial.owner.other.model";
-import {
-  AddressKeys, BeneficialOwnerNoc, HasSamePrincipalAddressKey, InputDateKeys, IsOnRegisterInCountryFormedInKey, IsOnSanctionsListKey, NonLegalFirmNoc, TrusteesNoc
-} from "../model/data.types.model";
-import { PrincipalAddressKey, PrincipalAddressKeys, ServiceAddressKey, ServiceAddressKeys } from "../model/address.model";
 import { StartDateKey, StartDateKeys } from "../model/date.model";
+import { BENEFICIAL_OWNER_OTHER_PAGE, BENEFICIAL_OWNER_TYPE_URL } from "../config";
+import { BeneficialOwnerOtherKey, BeneficialOwnerOtherKeys } from "../model/beneficial.owner.other.model";
 
-export const get = (req: Request, res: Response, next: NextFunction) => {
+export const get = (req: Request, res: Response) => {
+  logger.debugRequest(req, `GET ${BENEFICIAL_OWNER_OTHER_PAGE}`);
+
+  return res.render(BENEFICIAL_OWNER_OTHER_PAGE, {
+    backLinkUrl: BENEFICIAL_OWNER_TYPE_URL
+  });
+};
+
+export const getByID = (req: Request, res: Response, next: NextFunction) => {
   try {
-    logger.debugRequest(req, `GET ${config.BENEFICIAL_OWNER_OTHER_PAGE}`);
+    logger.debugRequest(req, `GET ${BENEFICIAL_OWNER_OTHER_PAGE}`);
 
-    const { id } = req.params;
+    const id = req.params[ID];
+    const data = getFromApplicationData(req.session, BeneficialOwnerOtherKey, id);
 
-    return res.render(config.BENEFICIAL_OWNER_OTHER_PAGE, {
+    const principalAddress = (data) ? mapDataObjectToFields(data[PrincipalAddressKey], PrincipalAddressKeys, AddressKeys) : {};
+    const serviceAddress = (data) ? mapDataObjectToFields(data[ServiceAddressKey], ServiceAddressKeys, AddressKeys) : {};
+    const startDate = (data) ? mapDataObjectToFields(data[StartDateKey], StartDateKeys, InputDateKeys) : {};
+    const nocFields = (data) ? mapNOCObjectToFields(data) : {}; // Needed to map array of a single field to a string
+
+    return res.render(BENEFICIAL_OWNER_OTHER_PAGE, {
+      backLinkUrl: BENEFICIAL_OWNER_TYPE_URL,
       id,
-      backLinkUrl: config.BENEFICIAL_OWNER_TYPE_URL,
-      ...getFromApplicationData(req.session, BeneficialOwnerOtherKey, id)
+      ...data,
+      ...principalAddress,
+      ...serviceAddress,
+      ...nocFields,
+      [StartDateKey]: startDate
     });
   } catch (error) {
     logger.errorRequest(req, error);
@@ -32,29 +71,33 @@ export const get = (req: Request, res: Response, next: NextFunction) => {
 export const post = (req: Request, res: Response, next: NextFunction) => {
 
   try {
-    logger.debugRequest(req, `POST ${config.BENEFICIAL_OWNER_OTHER_PAGE}`);
+    logger.debugRequest(req, `POST ${BENEFICIAL_OWNER_OTHER_PAGE}`);
 
-    const data: ApplicationDataType = prepareData(req.body, BeneficialOwnerOtherKeys);
-
-    data[PrincipalAddressKey] = mapFieldsToDataObject(req.body, PrincipalAddressKeys, AddressKeys);
-    data[ServiceAddressKey] = mapFieldsToDataObject(req.body, ServiceAddressKeys, AddressKeys);
-
-    data[StartDateKey] = mapFieldsToDataObject(req.body, StartDateKeys, InputDateKeys);
-
-    // It needs concatenations because if in the check boxes we select only one option
-    // nunjucks returns just a string and with concat we will return an array.
-    data[BeneficialOwnerNoc] = (data[BeneficialOwnerNoc]) ? [].concat(data[BeneficialOwnerNoc]) : [];
-    data[TrusteesNoc] = (data[TrusteesNoc]) ? [].concat(data[TrusteesNoc]) : [];
-    data[NonLegalFirmNoc] = (data[NonLegalFirmNoc]) ? [].concat(data[NonLegalFirmNoc]) : [];
-
-    data[HasSamePrincipalAddressKey] = (data[HasSamePrincipalAddressKey]) ? +data[HasSamePrincipalAddressKey] : '';
-    data[IsOnSanctionsListKey] = (data[IsOnSanctionsListKey]) ? +data[IsOnSanctionsListKey] : '';
-    data[IsOnRegisterInCountryFormedInKey] = (data[IsOnRegisterInCountryFormedInKey]) ? +data[IsOnRegisterInCountryFormedInKey] : '';
-    data["id"] = uuidv4();
+    const data: ApplicationDataType = setBeneficialOwnerData(req.body, uuidv4());
 
     setApplicationData(req.session, data, BeneficialOwnerOtherKey);
 
-    return res.redirect(config.BENEFICIAL_OWNER_TYPE_URL);
+    return res.redirect(BENEFICIAL_OWNER_TYPE_URL);
+  } catch (error) {
+    logger.errorRequest(req, error);
+    next(error);
+  }
+};
+
+export const update = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    logger.debugRequest(req, `UPDATE ${BENEFICIAL_OWNER_OTHER_PAGE}`);
+
+    // Remove old Beneficial Owner
+    removeFromApplicationData(req.session, BeneficialOwnerOtherKey, req.params[ID]);
+
+    // Set Beneficial Owner data
+    const data: ApplicationDataType = setBeneficialOwnerData(req.body, req.params[ID]);
+
+    // Save new Beneficial Owner
+    setApplicationData(req.session, data, BeneficialOwnerOtherKey);
+
+    return res.redirect(BENEFICIAL_OWNER_TYPE_URL);
   } catch (error) {
     logger.errorRequest(req, error);
     next(error);
@@ -63,14 +106,36 @@ export const post = (req: Request, res: Response, next: NextFunction) => {
 
 export const remove = (req: Request, res: Response, next: NextFunction) => {
   try {
-    logger.debug(`REMOVE ${config.MANAGING_OFFICER_PAGE}`);
+    logger.debug(`REMOVE ${BENEFICIAL_OWNER_OTHER_PAGE}`);
 
-    removeFromApplicationData(req.session, BeneficialOwnerOtherKey, req.params.id);
+    removeFromApplicationData(req.session, BeneficialOwnerOtherKey, req.params[ID]);
 
-    return res.redirect(config.BENEFICIAL_OWNER_TYPE_URL);
+    return res.redirect(BENEFICIAL_OWNER_TYPE_URL);
   } catch (error) {
     logger.errorRequest(req, error);
     next(error);
   }
+};
+
+const setBeneficialOwnerData = (reqBody: any, id: string): ApplicationDataType => {
+  const data: ApplicationDataType = prepareData(reqBody, BeneficialOwnerOtherKeys);
+
+  data[PrincipalAddressKey] = mapFieldsToDataObject(reqBody, PrincipalAddressKeys, AddressKeys);
+  data[ServiceAddressKey] = mapFieldsToDataObject(reqBody, ServiceAddressKeys, AddressKeys);
+  data[StartDateKey] = mapFieldsToDataObject(reqBody, StartDateKeys, InputDateKeys);
+
+  // It needs concatenations because if in the check boxes we select only one option
+  // nunjucks returns just a string and with concat we will return an array.
+  data[BeneficialOwnerNoc] = (data[BeneficialOwnerNoc]) ? [].concat(data[BeneficialOwnerNoc]) : [];
+  data[TrusteesNoc] = (data[TrusteesNoc]) ? [].concat(data[TrusteesNoc]) : [];
+  data[NonLegalFirmNoc] = (data[NonLegalFirmNoc]) ? [].concat(data[NonLegalFirmNoc]) : [];
+
+  data[HasSamePrincipalAddressKey] = (data[HasSamePrincipalAddressKey]) ? +data[HasSamePrincipalAddressKey] : '';
+  data[IsOnSanctionsListKey] = (data[IsOnSanctionsListKey]) ? +data[IsOnSanctionsListKey] : '';
+  data[IsOnRegisterInCountryFormedInKey] = (data[IsOnRegisterInCountryFormedInKey]) ? +data[IsOnRegisterInCountryFormedInKey] : '';
+
+  data[ID] = id;
+
+  return data;
 };
 

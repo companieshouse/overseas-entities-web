c1e89825f034a7b7b6ace84b35a75b8ab242ed69
mattch1 <mculpin@companieshouse.gov.uk>
2022-06-10 13:03:40 +0100

    ROE-704 General name needed

--- src/validation/presenter.validation.ts
+++ src/validation/presenter.validation.ts
@@ -1,9 +1,9 @@
 import { body } from "express-validator";
 
 import { ErrorMessages } from "./error.messages";
-import { INVALID_CHARACTERS_FULL_NAME } from "./regex/regex.validation";
+import { VALID_CHARACTERS } from "./regex/regex.validation";
 
 export const presenter = [
-  body("full_name").not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.FULL_NAME).isLength({ max: 160 }).withMessage(ErrorMessages.MAX_FULL_NAME_LENGTH).matches(INVALID_CHARACTERS_FULL_NAME).withMessage(ErrorMessages.INVALID_CHARACTERS),
+  body("full_name").not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.FULL_NAME).isLength({ max: 160 }).withMessage(ErrorMessages.MAX_FULL_NAME_LENGTH).matches(VALID_CHARACTERS).withMessage(ErrorMessages.INVALID_CHARACTERS),
   body("email").not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.EMAIL).isLength({ max: 250 }).withMessage(ErrorMessages.MAX_EMAIL_LENGTH)
 ];

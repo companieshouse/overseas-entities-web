3c94669198b0681aab724d945e446b2cc24480b2
mattch1 <mculpin@companieshouse.gov.uk>
2022-06-10 11:52:52 +0100

    ROE-704 Moved regex in another file and updated the name

--- src/validation/presenter.validation.ts
+++ src/validation/presenter.validation.ts
@@ -1,10 +1,9 @@
 import { body } from "express-validator";
 
 import { ErrorMessages } from "./error.messages";
-
-const VALID_CHARACTERS = /^[-,.:; 0-9A-Z&@$£¥€'"«»''""?!/\\()[\]{}<>*=#%+ÀÁÂÃÄÅĀĂĄÆǼÇĆĈĊČÞĎÐÈÉÊËĒĔĖĘĚĜĞĠĢĤĦÌÍÎÏĨĪĬĮİĴĶĹĻĽĿŁÑŃŅŇŊÒÓÔÕÖØŌŎŐǾŒŔŖŘŚŜŞŠŢŤŦÙÚÛÜŨŪŬŮŰŲŴẀẂẄỲÝŶŸŹŻŽa-zÀÖØſƒǺǿẀẅỲỳ]*$/;
+import { INVALID_CHARACTERS_FULL_NAME } from "./regex/regex.validation";
 
 export const presenter = [
-  body("full_name").not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.FULL_NAME).isLength({ max: 160 }).withMessage(ErrorMessages.MAX_FULL_NAME_LENGTH).matches(VALID_CHARACTERS).withMessage(ErrorMessages.INVALID_CHARACTERS),
+  body("full_name").not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.FULL_NAME).isLength({ max: 160 }).withMessage(ErrorMessages.MAX_FULL_NAME_LENGTH).matches(INVALID_CHARACTERS_FULL_NAME).withMessage(ErrorMessages.INVALID_CHARACTERS),
   body("email").not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.EMAIL).isLength({ max: 250 }).withMessage(ErrorMessages.MAX_EMAIL_LENGTH)
 ];

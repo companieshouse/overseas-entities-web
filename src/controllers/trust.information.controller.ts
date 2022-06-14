d2fe3daa5c5149df295b8fb7ba13b8b08b9cd20b
Edwin Mead <emead@companieshouse.gov.uk>
2022-06-14 11:44:32 +0100

    Add trust information controller

--- src/controllers/trust.information.controller.ts
+++ src/controllers/trust.information.controller.ts
@@ -4,14 +4,14 @@ import { logger } from "../utils/logger";
 import * as config from "../config";
 
 export const get = (req: Request, res: Response) => {
-    logger.debugRequest(req, `GET ${config.TRUST_INFO_PAGE}`);
-  
-    return res.render(config.TRUST_INFO_PAGE, {
-        backLinkUrl: config.BENEFICIAL_OWNER_TYPE_PAGE,
-        templateName: config.TRUST_INFO_PAGE
-        //...appData
-    });
-  };
+  logger.debugRequest(req, `GET ${config.TRUST_INFO_PAGE}`);
+
+  return res.render(config.TRUST_INFO_PAGE, {
+    backLinkUrl: config.BENEFICIAL_OWNER_TYPE_PAGE,
+    templateName: config.TRUST_INFO_PAGE
+    // . ...appData
+  });
+};
 
 export const post = (req: Request, res: Response) => {
   logger.debugRequest(req, `POST ${config.TRUST_INFO_PAGE}`);

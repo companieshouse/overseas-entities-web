e3f859d69f0f0e7560a801631982cebf8810319e
svarallo <svarallo@companieshouse.gov.uk>
2022-06-16 11:02:34 +0100

    ROE-385 addressing duplication

--- src/validation/beneficial.owner.gov.validation.ts
+++ src/validation/beneficial.owner.gov.validation.ts
@@ -6,17 +6,17 @@ import { VALID_CHARACTERS } from "./regex/regex.validation";
 
 export const beneficialOwnerGov = [
   body("name")
-    .isLength({ max: 160 }).withMessage(ErrorMessages.MAX_NAME_LENGTH)
-    .matches(VALID_CHARACTERS).withMessage(ErrorMessages.NAME_INVALID_CHARACTERS),
+    .matches(VALID_CHARACTERS).withMessage(ErrorMessages.NAME_INVALID_CHARACTERS)
+    .isLength({ max: 160 }).withMessage(ErrorMessages.MAX_NAME_LENGTH),
 
   ...principal_address_beneficial_owner_validation,
   ...service_address_beneficial_owner_validation,
 
-  body("legal_form")
-    .isLength({ max: 4000 }).withMessage(ErrorMessages.MAX_LEGAL_FORM_LENGTH)
-    .matches(VALID_CHARACTERS).withMessage(ErrorMessages.LEGAL_FORM_INVALID_CHARACTERS),
   body("law_governed")
     .isLength({ max: 4000 }).withMessage(ErrorMessages.MAX_LAW_GOVERNED_LENGTH)
     .matches(VALID_CHARACTERS).withMessage(ErrorMessages.LAW_GOVERNED_INVALID_CHARACTERS),
+  body("legal_form")
+    .isLength({ max: 4000 }).withMessage(ErrorMessages.MAX_LEGAL_FORM_LENGTH)
+    .matches(VALID_CHARACTERS).withMessage(ErrorMessages.LEGAL_FORM_INVALID_CHARACTERS),
 
 ];

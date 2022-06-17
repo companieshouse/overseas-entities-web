809dc7f47e44b2d602eb509d5185c00e7812d942
Rohan Sachdev <rsachdev@companieshouse.gov.uk>
2022-06-17 13:55:56 +0100

    Refactor trust check to remove duplication

--- src/utils/trusts.ts
+++ src/utils/trusts.ts
@@ -1,25 +1,31 @@
 import { ApplicationData } from "model";
+import { BeneficialOwnerIndividual } from "model/beneficial.owner.individual.model";
+import { BeneficialOwnerOther } from "model/beneficial.owner.other.model";
 
 // Checks whether any beneficial owners have trust data
 export const checkEntityHasTrusts = (appData: ApplicationData): boolean => {
-  let hasTrusts: boolean = false;
+  if (appData) {
+    const allBenficialOwnersToCheck: (BeneficialOwnerIndividual[] | BeneficialOwnerOther[] | undefined)[] = [
+      appData.beneficial_owners_individual,
+      appData.beneficial_owners_corporate
+    ];
 
-  if (appData !== null) {
-    if (appData.beneficial_owners_individual !== undefined) {
-      appData.beneficial_owners_individual.forEach(element => {
-        if (element.trustees_nature_of_control_types !== undefined && element.trustees_nature_of_control_types.length > 0) {
-          hasTrusts = true;
+    for (const benficialOwners of allBenficialOwnersToCheck) {
+      if (benficialOwners) {
+        if (containsTrusts(benficialOwners)) {
+          return true;
         }
-      });
-    }
-
-    if (appData.beneficial_owners_corporate !== undefined) {
-      appData.beneficial_owners_corporate.forEach(element => {
-        if (element.trustees_nature_of_control_types !== undefined && element.trustees_nature_of_control_types.length > 0) {
-          hasTrusts = true;
-        }
-      });
+      }
     }
   }
-  return hasTrusts;
+  return false;
+};
+
+const containsTrusts = (beneficialOwners: BeneficialOwnerIndividual[] | BeneficialOwnerOther[]): boolean => {
+  for (const bo of beneficialOwners) {
+    if (bo && bo.trustees_nature_of_control_types && bo.trustees_nature_of_control_types.length > 0) {
+      return true;
+    }
+  }
+  return false;
 };

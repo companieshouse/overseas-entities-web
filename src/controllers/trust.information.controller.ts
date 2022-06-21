6d912c411070ce2d2935d5dba70fd6d6262677a0
Rohan Sachdev <rsachdev@companieshouse.gov.uk>
2022-06-21 11:18:01 +0100

    Add to post request in trust controller

--- src/controllers/trust.information.controller.ts
+++ src/controllers/trust.information.controller.ts
@@ -3,7 +3,8 @@ import { NextFunction, Request, Response } from "express";
 import { logger } from "../utils/logger";
 import * as config from "../config";
 import { ApplicationData } from "../model";
-import { getApplicationData } from "../utils/application.data";
+import { getApplicationData, setExtraData } from "../utils/application.data";
+import { TrustKey } from "../model/trust.model";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -22,8 +23,26 @@ export const get = (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export const post = (req: Request, res: Response) => {
-  logger.debugRequest(req, `POST ${config.TRUST_INFO_PAGE}`);
+export const post = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    logger.debugRequest(req, `POST ${config.TRUST_INFO_PAGE}`);
+
+    const appData: ApplicationData = getApplicationData(req.session);
+    appData[TrustKey]?.push(req.body[TrustKey]);
+    setExtraData(req.session, appData);
+    console.log(appData);
+
+    /* if (req.body.hasOwnProperty("add")) {
+      console.log("Add button pressed");
+    }
+    if (req.body.hasOwnProperty("submit")) {
+      console.log("Submit button pressed");
+    } */
+    return res.redirect(config.CHECK_YOUR_ANSWERS_PAGE);
+  } catch (error) {
+    logger.errorRequest(req, error);
+    next(error);
+  }
 
   return res.redirect("TODO");
 };

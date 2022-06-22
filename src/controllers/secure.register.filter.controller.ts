cf8d54612e711830dc50a26405ce55113ada5f68
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-06-22 12:51:52 +0100

    Add logic to save user choice and update unit tests

--- src/controllers/secure.register.filter.controller.ts
+++ src/controllers/secure.register.filter.controller.ts
@@ -1,13 +1,19 @@
 import { Request, Response, NextFunction } from "express";
+
 import * as config from "../config";
 import { logger } from "../utils/logger";
+import { ApplicationData } from "../model";
+import { getApplicationData, setExtraData } from "../utils/application.data";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
     logger.debugRequest(req, `GET ${config.SECURE_REGISTER_FILTER_PAGE}`);
+    const appData: ApplicationData = getApplicationData(req.session);
+
     return res.render(config.SECURE_REGISTER_FILTER_PAGE, {
       backLinkUrl: config.SOLD_LAND_FILTER_URL,
-      templateName: config.SECURE_REGISTER_FILTER_PAGE
+      templateName: config.SECURE_REGISTER_FILTER_PAGE,
+      is_secure_register: appData.is_secure_register
     });
   } catch (error) {
     logger.errorRequest(req, error);
@@ -19,6 +25,9 @@ export const post = (req: Request, res: Response, next: NextFunction) => {
   try {
     logger.debugRequest(req, `POST ${config.SECURE_REGISTER_FILTER_PAGE}`);
     const isSecureRegister = req.body.is_secure_register;
+
+    setExtraData(req.session, { ...getApplicationData(req.session), is_secure_register: isSecureRegister });
+
     if (isSecureRegister === '1') {
       return res.redirect(config.USE_PAPER_URL);
     } else if (isSecureRegister === '0') {

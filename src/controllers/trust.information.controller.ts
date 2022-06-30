5ec0430579ec276cdfcc7f8ad9e3dce583b6d882
Edwin Mead <emead@companieshouse.gov.uk>
2022-06-30 12:44:42 +0100

    Store unique trust_id against each trust

--- src/controllers/trust.information.controller.ts
+++ src/controllers/trust.information.controller.ts
@@ -32,6 +32,16 @@ export const post = (req: Request, res: Response, next: NextFunction) => {
       trusts: obj
     };
 
+    var trustCount = 0;
+    if (req.session?.data.extra_data.roe.trusts != undefined) {
+      trustCount = (req.session?.data.extra_data.roe.trusts).length;
+    }
+
+    for (var i in t.trusts) {
+      trustCount++;
+      t.trusts[i].trust_id = trustCount.toString();
+    }
+
     const data: ApplicationDataType = setTrustData(t);
 
     for (const i in data[TrustKey]) {

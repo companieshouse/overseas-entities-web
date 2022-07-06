23db49ac6aff8b7853f262d1fed9ed5a1e3e71a4
mattch1 <mculpin@companieshouse.gov.uk>
2022-07-06 13:33:23 +0100

    ROE-381 Nature of control verification needs to cover all 12 options at once

--- src/validation/custom.validation.ts
+++ src/validation/custom.validation.ts
@@ -4,11 +4,13 @@ import { VALID_CHARACTERS } from "./regex/regex.validation";
 import { DateTime } from "luxon";
 
 
-export const checkFieldIfCheckboxTicked = (selected: boolean, errMsg: string, value: string = "") => {
-  if ( !selected && !value ) {
-    throw new Error(errMsg);
+export const checkFieldIfCheckboxTicked = (selectedArray, errMsg: string, value: string = "") => {
+  for (const selected of selectedArray) {
+    if (selected && value) {
+      return true;
+    }
   }
-  return true;
+  throw new Error(errMsg);
 };
 
 export const checkFieldIfRadioButtonSelected = (selected: boolean, errMsg: string, value: string = "") => {
--- src/validation/fields/nature-of-control.validation.ts
+++ src/validation/fields/nature-of-control.validation.ts
@@ -3,12 +3,12 @@ import { ErrorMessages } from "../error.messages";
 import { body } from "express-validator";
 
 export const nature_of_control_validations = [
-  body("beneficial_owner_nature_of_control_types")
-    .custom((value, { req }) => checkFieldIfCheckboxTicked(req.body.beneficial_owner_nature_of_control_types, ErrorMessages.SELECT_NATURE_OF_CONTROL, value) ),
-
-  body("trustees_nature_of_control_types")
-    .custom((value, { req }) => checkFieldIfCheckboxTicked(req.body.trustees_nature_of_control_types, ErrorMessages.SELECT_NATURE_OF_CONTROL, value) ),
-
-  body("non_legal_firm_members_nature_of_control_types")
-    .custom((value, { req }) => checkFieldIfCheckboxTicked(req.body.non_legal_firm_members_nature_of_control_types, ErrorMessages.SELECT_NATURE_OF_CONTROL, value) )
+  body("")
+    .custom((value, { req }) => checkFieldIfCheckboxTicked(
+      [
+        req.body.beneficial_owner_nature_of_control_types,
+        req.body.trustees_nature_of_control_types,
+        req.body.non_legal_firm_members_nature_of_control_types
+      ],
+      ErrorMessages.SELECT_NATURE_OF_CONTROL, value) ),
 ];

8247478185923d67df5fdae4ce9059e8a064c3d1
markpit <mpitten@companieshouse.gov.uk>
2022-07-06 10:28:29 +0100

    ROE-383 remove console.log and revert package-lock.json

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -20,7 +20,6 @@ export function checkValidations(req: Request, res: Response, next: NextFunction
   try {
     const errorList = validationResult(req);
 
-    console.log(JSON.stringify(errorList, null, 2));
     if (!errorList.isEmpty()) {
       const errors = formatValidationError(errorList.array());
 

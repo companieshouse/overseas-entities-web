b0b45f8cfb64f8c750c42d3b9468b8cf9eaad377
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-07-27 12:02:33 +0100

    Remove unused code from fields validation

--- src/validation/fields/public-register.validation.ts
+++ src/validation/fields/public-register.validation.ts
@@ -17,13 +17,3 @@ export const public_register_validations = [
     .custom((value, { req }) => checkMaxFieldIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.MAX_PUBLIC_REGISTER_NUMBER_LENGTH, 32, value) )
     .custom((value, { req }) => checkInvalidCharactersIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.PUBLIC_REGISTER_NUMBER_INVALID_CHARACTERS, value) )
 ];
-
-// Checking only MAX for BOs fields
-export const public_register_max_validations = [
-  body("public_register_name")
-    .custom((value, { req }) => checkMaxFieldIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.MAX_PUBLIC_REGISTER_NAME_LENGTH, 4000, value) )
-    .custom((value, { req }) => checkInvalidCharactersIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.PUBLIC_REGISTER_NAME_INVALID_CHARACTERS, value) ),
-  body("registration_number")
-    .custom((value, { req }) => checkMaxFieldIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.MAX_PUBLIC_REGISTER_NUMBER_LENGTH, 32, value) )
-    .custom((value, { req }) => checkInvalidCharactersIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.PUBLIC_REGISTER_NUMBER_INVALID_CHARACTERS, value) ),
-];

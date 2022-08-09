f778bd0f5bddace7ac791030262116b0c50dc607
mattch1 <mculpin@companieshouse.gov.uk>
2022-08-09 14:54:41 +0100

    ROE-1089 Add audiable hidden text to beneficial owner types page

--- test/controllers/beneficial.owner.type.controller.spec.ts
+++ test/controllers/beneficial.owner.type.controller.spec.ts
@@ -20,7 +20,11 @@ import {
   BENEFICIAL_OWNER_TYPE_PAGE_HEADING_NONE_IDENTIFIED,
   BENEFICIAL_OWNER_TYPE_LEGEND_TEXT_NONE_IDENTIFIED,
   BENEFICIAL_OWNER_TYPE_LEGEND_TEXT_ALL_IDENTIFIED_ALL_DETAILS,
-  BENEFICIAL_OWNER_TYPE_LEGEND_TEXT
+  BENEFICIAL_OWNER_TYPE_LEGEND_TEXT,
+  BENEFICIAL_OWNER_TYPE_PAGE_HEADING_SOME_IDENTIFIED,
+  BENEFICIAL_OWNER_TYPE_LEGEND_TEXT_SOME_IDENTIFIED,
+  BENEFICIAL_OWNER_TYPE_ADD_BUTTON_NONE_IDENTIFIED,
+  BENEFICIAL_OWNER_TYPE_ADD_BUTTON_ALL_IDENTIFIED, BENEFICIAL_OWNER_TYPE_ADD_BUTTON_SOME_IDENTIFIED
 } from '../__mocks__/text.mock';
 import { APPLICATION_DATA_MOCK, APPLICATION_DATA_NO_TRUSTS_MOCK, ERROR } from '../__mocks__/session.mock';
 import {
@@ -78,6 +82,7 @@ describe("BENEFICIAL OWNER TYPE controller", () => {
       expect(resp.status).toEqual(200);
       expect(resp.text).toContain(BENEFICIAL_OWNER_TYPE_PAGE_HEADING_ALL_IDENTIFIED_ALL_DETAILS);
       expect(resp.text).toContain(BENEFICIAL_OWNER_TYPE_LEGEND_TEXT_ALL_IDENTIFIED_ALL_DETAILS);
+      expect(resp.text).toContain(BENEFICIAL_OWNER_TYPE_ADD_BUTTON_ALL_IDENTIFIED);
     });
 
     test("renders the beneficial owner type page for beneficial owners with just the MOs options", async () => {
@@ -87,6 +92,17 @@ describe("BENEFICIAL OWNER TYPE controller", () => {
       expect(resp.status).toEqual(200);
       expect(resp.text).toContain(BENEFICIAL_OWNER_TYPE_PAGE_HEADING_NONE_IDENTIFIED);
       expect(resp.text).toContain(BENEFICIAL_OWNER_TYPE_LEGEND_TEXT_NONE_IDENTIFIED);
+      expect(resp.text).toContain(BENEFICIAL_OWNER_TYPE_ADD_BUTTON_NONE_IDENTIFIED);
+    });
+
+    test("renders the beneficial owner type page for beneficial owners with both options", async () => {
+      mockGetApplicationData.mockReturnValueOnce({ [BeneficialOwnerStatementKey]: BeneficialOwnersStatementType.SOME_IDENTIFIED_ALL_DETAILS });
+      const resp = await request(app).get(config.BENEFICIAL_OWNER_TYPE_URL);
+
+      expect(resp.status).toEqual(200);
+      expect(resp.text).toContain(BENEFICIAL_OWNER_TYPE_PAGE_HEADING_SOME_IDENTIFIED);
+      expect(resp.text).toContain(BENEFICIAL_OWNER_TYPE_LEGEND_TEXT_SOME_IDENTIFIED);
+      expect(resp.text).toContain(BENEFICIAL_OWNER_TYPE_ADD_BUTTON_SOME_IDENTIFIED);
     });
 
     test("catch error when rendering the page", async () => {

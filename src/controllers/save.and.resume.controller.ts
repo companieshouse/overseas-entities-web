992d874ab1a5841e61481051bc529ee77b4ca84b
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2022-11-23 12:47:10 +0000

    Add OE default web values method

--- src/controllers/save.and.resume.controller.ts
+++ src/controllers/save.and.resume.controller.ts
@@ -1,15 +1,23 @@
 import { NextFunction, Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
 
 import * as config from "../config";
+
 import { ApplicationData } from "../model";
 import { createAndLogErrorRequest, logger } from "../utils/logger";
 import { setExtraData } from "../utils/application.data";
 import { isActiveFeature } from "../utils/feature.flag";
 import { resumeOverseasEntity } from "../service/overseas.entities.service";
-import { HasSoldLandKey, IsSecureRegisterKey, OverseasEntityKey, Transactionkey } from "../model/data.types.model";
+import { HasSoldLandKey, ID, IsSecureRegisterKey, OverseasEntityKey, Transactionkey } from "../model/data.types.model";
 import { WhoIsRegisteringKey, WhoIsRegisteringType } from "../model/who.is.making.filing.model";
 import { OverseasEntityDueDiligence, OverseasEntityDueDiligenceKey } from "../model/overseas.entity.due.diligence.model";
 import { DueDiligence, DueDiligenceKey } from "../model/due.diligence.model";
+import { BeneficialOwnerGov, BeneficialOwnerGovKey } from "../model/beneficial.owner.gov.model";
+import { BeneficialOwnerIndividual, BeneficialOwnerIndividualKey } from "../model/beneficial.owner.individual.model";
+import { BeneficialOwnerOther, BeneficialOwnerOtherKey } from "../model/beneficial.owner.other.model";
+import { ManagingOfficerCorporate, ManagingOfficerCorporateKey } from "../model/managing.officer.corporate.model";
+import { ManagingOfficerIndividual, ManagingOfficerKey } from "../model/managing.officer.model";
+import { Session } from "@companieshouse/node-session-handler";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -27,19 +35,7 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
         throw createAndLogErrorRequest(req, `Error on resuming OE - ${infoMsg}`);
       }
 
-      // Add default values needed for the web journey that are not part of OE API data model
-      appData[HasSoldLandKey] = '0';
-      appData[IsSecureRegisterKey] = '0';
-      appData[Transactionkey] = transactionId;
-      appData[OverseasEntityKey] = overseaEntityId;
-
-      if (Object.keys(appData[OverseasEntityDueDiligenceKey] as OverseasEntityDueDiligence).length) {
-        appData[WhoIsRegisteringKey] =  WhoIsRegisteringType.SOMEONE_ELSE;
-      } else if (Object.keys(appData[DueDiligenceKey] as DueDiligence).length){
-        appData[WhoIsRegisteringKey] = WhoIsRegisteringType.AGENT;
-      }
-
-      setExtraData(req.session, appData);
+      setWebApplicationData(req.session as Session, appData, transactionId, overseaEntityId);
     }
 
     return res.redirect(config.SOLD_LAND_FILTER_URL);
@@ -47,4 +43,41 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
     logger.errorRequest(req, error);
     next(error);
   }
+};
+
+/**
+ * Set default values needed for the web journey that are not part of OE API data model
+ * nor part of the SDK mapper object.
+ * Add IDs needed for the web to select single BOs or MOs on beneficial-owner-type screen and
+ * related checkboxes for different pages
+ * @param session
+ * @param appData
+ * @param transactionId
+ * @param overseaEntityId
+ */
+const setWebApplicationData = (session: Session, appData: ApplicationData, transactionId: string, overseaEntityId: string) => {
+
+  appData[BeneficialOwnerIndividualKey] = (appData[BeneficialOwnerIndividualKey] as BeneficialOwnerIndividual[])
+    .map( e => { return { ...e, [ID]: uuidv4() }; } );
+  appData[BeneficialOwnerOtherKey] = (appData[BeneficialOwnerOtherKey] as BeneficialOwnerOther[] )
+    .map( e => { return { ...e, [ID]: uuidv4() }; } );
+  appData[BeneficialOwnerGovKey] = (appData[BeneficialOwnerGovKey] as BeneficialOwnerGov[])
+    .map( e => { return { ...e, [ID]: uuidv4() }; } );
+  appData[ManagingOfficerKey] = (appData[ManagingOfficerKey] as ManagingOfficerIndividual[])
+    .map( e => { return { ...e, [ID]: uuidv4() }; } );
+  appData[ManagingOfficerCorporateKey] = (appData[ManagingOfficerCorporateKey] as ManagingOfficerCorporate[])
+    .map( e => { return { ...e, [ID]: uuidv4() }; } );
+
+  appData[HasSoldLandKey] = '0';
+  appData[IsSecureRegisterKey] = '0';
+  appData[Transactionkey] = transactionId;
+  appData[OverseasEntityKey] = overseaEntityId;
+
+  if (Object.keys(appData[OverseasEntityDueDiligenceKey] as OverseasEntityDueDiligence).length) {
+    appData[WhoIsRegisteringKey] =  WhoIsRegisteringType.SOMEONE_ELSE;
+  } else if (Object.keys(appData[DueDiligenceKey] as DueDiligence).length){
+    appData[WhoIsRegisteringKey] = WhoIsRegisteringType.AGENT;
+  }
+
+  setExtraData(session, appData);
 };

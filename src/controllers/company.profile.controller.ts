3f847a8f5eb602a9a239be52715d3a24c3b3dc30
olaych <oolatunji@companieshouse.gov.uk>
2022-11-25 14:27:16 +0000

    implements api call

--- src/controllers/company.profile.controller.ts
+++ src/controllers/company.profile.controller.ts
@@ -5,44 +5,35 @@ import { logger } from "../utils/logger";
 import * as config from "../config";
 import { getApplicationData } from "../utils/application.data";
 import CompanyProfileService from "@companieshouse/api-sdk-node/dist/services/company-profile/service";
-import { header } from "express-validator";
-import { ClientRequest } from "http";
 import { getAccessToken } from "utils/session";
-import { IHttpClient } from "@companieshouse/api-sdk-node";
-import client from "@companieshouse/api-sdk-node/dist/client";
-import { AbstractClient, Headers, HttpResponse } from "@companieshouse/api-sdk-node/dist/http";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
 
-  let accessToken = getAccessToken(req.session);
+  const accessToken = getAccessToken(req.session);
   const clientDetails = {
-    header: req.header,
-  } 
+    header: accessToken,
+  };
+  try {
+    const searchCompanyProfile = new CompanyProfileService(clientDetails);
+    const responses = searchCompanyProfile.getCompanyProfile(req.params.companyNumber);
+    logger.debugRequest(req, `GET Update Page`);
+    console.log(`Response from API CALL ${(await responses).resource}`);
 
-  try{
-    let searchCompanyProfile = new CompanyProfileService(clientDetails);
-    let responses = searchCompanyProfile.getCompanyProfile(req.params.companyNumber);
-  logger.debugRequest(req, `GET Update Page`);
-  console.log(`Response from API CALL ${(await responses).resource}`)
+    const session = req.session as Session;
+    const appData: ApplicationData = getApplicationData(session);
+    console.log(`App data is ${appData.overseasEntityData}`);
+    const backLinkUrl: string = config.BENEFICIAL_OWNER_TYPE_URL;
+    const updateUrl: string = config.UPDATE_COMPANY_PROFILES_URL;
 
-  const session = req.session as Session;
-  const appData: ApplicationData = getApplicationData(session);
-  console.log(`App data is ${appData.overseasEntityData}`);
-
-    
-    
-    let backLinkUrl: string = config.BENEFICIAL_OWNER_TYPE_URL;
-    let updateUrl: string = config.UPDATE_COMPANY_PROFILES_URL;
-
-  return res.render(config.UPDATE_COMPANY_PROFILE_PAGE, {
-    backLinkUrl,
-    updateUrl,
-    templateName: config.UPDATE_COMPANY_PROFILE_PAGE,
-    appData
-  });
-}  catch (error) {
-  logger.errorRequest(req, error);
-  next(error);
-}
+    return res.render(config.UPDATE_COMPANY_PROFILE_PAGE, {
+      backLinkUrl,
+      updateUrl,
+      templateName: config.UPDATE_COMPANY_PROFILE_PAGE,
+      appData
+    });
+  }  catch (error) {
+    logger.errorRequest(req, error);
+    next(error);
+  }
 };
 

75bb9acdaca172adfffe75ca1b5e214d143b5612
olaych <oolatunji@companieshouse.gov.uk>
2022-11-25 18:20:55 +0000

    added api call to use companyProfile sdk

--- src/controllers/company.profile.controller.ts
+++ src/controllers/company.profile.controller.ts
@@ -4,20 +4,18 @@ import { ApplicationData } from "model";
 import { logger } from "../utils/logger";
 import * as config from "../config";
 import { getApplicationData } from "../utils/application.data";
-import CompanyProfileService from "@companieshouse/api-sdk-node/dist/services/company-profile/service";
-import { getAccessToken } from "utils/session";
+import { ID } from "../model/data.types.model";
+import { createOAuthApiClient } from "../service/api.service";
+
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
-
-  const accessToken = getAccessToken(req.session);
-  const clientDetails = {
-    header: accessToken,
-  };
   try {
-    const searchCompanyProfile = new CompanyProfileService(clientDetails);
-    const responses = searchCompanyProfile.getCompanyProfile(req.params.companyNumber);
+    const id: string = req.params[ID];
+    const responses = createOAuthApiClient(req.session).companyProfile.getCompanyProfile(id);
+    const companyData = (await responses).resource;
+    // console.log(`ID PARSED IS ${id}`)
     logger.debugRequest(req, `GET Update Page`);
-    console.log(`Response from API CALL ${(await responses).resource}`);
+    // console.log(`Response from the API CALL ${(await responses).resource?.companyName}`);
 
     const session = req.session as Session;
     const appData: ApplicationData = getApplicationData(session);
@@ -29,7 +27,8 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
       backLinkUrl,
       updateUrl,
       templateName: config.UPDATE_COMPANY_PROFILE_PAGE,
-      appData
+      appData,
+      companyData
     });
   }  catch (error) {
     logger.errorRequest(req, error);

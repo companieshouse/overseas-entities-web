9d4771b53c96bf6166ca9196057966f6bb8515eb
mattch1 <mculpin@companieshouse.gov.uk>
2022-11-29 11:54:48 +0000

    ROE-1248 Speech marks

--- src/validation/fields/email.validation.ts
+++ src/validation/fields/email.validation.ts
@@ -3,7 +3,7 @@ import { ErrorMessages } from "../error.messages";
 import { VALID_EMAIL_FORMAT } from "../regex/regex.validation";
 
 export const email_validations = [
-  body('email')
+  body("email")
     .not().isEmpty({ ignore_whitespace: true }).withMessage(ErrorMessages.EMAIL)
     .isLength({ max: 256 }).withMessage(ErrorMessages.MAX_EMAIL_LENGTH)
     .matches(VALID_EMAIL_FORMAT).withMessage(ErrorMessages.EMAIL_INVALID_FORMAT)

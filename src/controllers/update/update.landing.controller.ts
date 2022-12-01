43268142089e0506d4a1c8781912757079295954
corybudd-ch <117734784+corybudd-ch@users.noreply.github.com>
2022-12-01 16:28:42 +0000

    remove debug log

--- src/controllers/update/update.landing.controller.ts
+++ src/controllers/update/update.landing.controller.ts
@@ -6,7 +6,6 @@ import * as config from "../../config";
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
     const landingUrl = config.UPDATE_LANDING_PAGE_URL;
-    logger.debugRequest(req, `GET UPDATE LANDING_PAGE`);
     logger.debugRequest(req, `Redirecting to ${landingUrl}`);
     return res.redirect(landingUrl);
   } catch (error) {

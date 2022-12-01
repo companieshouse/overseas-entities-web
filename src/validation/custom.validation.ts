75e99c15dea07b0377111d1a929599514fd82df7
mattch1 <mculpin@companieshouse.gov.uk>
2022-12-01 15:34:45 +0000

    ROE-1705 Name change

--- src/validation/custom.validation.ts
+++ src/validation/custom.validation.ts
@@ -5,7 +5,7 @@ import { DateTime } from "luxon";
 import { ErrorMessages } from "./error.messages";
 import { trustType } from "../model";
 import { BeneficialOwnersStatementType } from "../model/beneficial.owner.statement.model";
-import { CONCAT_VALUE_SEPARATOR } from "../config";
+import { CONCATENATED_VALUES_SEPARATOR } from "../config";
 
 export const checkFieldIfRadioButtonSelected = (selected: boolean, errMsg: string, value: string = "") => {
   if ( selected && !value.trim() ) {
@@ -90,7 +90,7 @@ export const checkSecondNationality = (nationality: string = "", secondNationali
 
   if ( nationality && nationality === secondNationality ) {
     throw new Error(ErrorMessages.SECOND_NATIONALITY_IS_SAME);
-  } else if ( nationality && secondNationality && `${nationality}${CONCAT_VALUE_SEPARATOR}${secondNationality}`.length > 50) {
+  } else if ( nationality && secondNationality && `${nationality}${CONCATENATED_VALUES_SEPARATOR}${secondNationality}`.length > 50) {
     throw new Error(ErrorMessages.NATIONALITIES_TOO_LONG);
   }
 
@@ -99,7 +99,7 @@ export const checkSecondNationality = (nationality: string = "", secondNationali
 
 export const checkPublicRegisterJurisdiction = (public_register_name: string = "", public_register_jurisdiction: string = "") => {
 
-  if (public_register_name && public_register_jurisdiction && `${public_register_name}${CONCAT_VALUE_SEPARATOR}${public_register_jurisdiction}`.length > 160) {
+  if (public_register_name && public_register_jurisdiction && `${public_register_name}${CONCATENATED_VALUES_SEPARATOR}${public_register_jurisdiction}`.length > 160) {
     throw new Error(ErrorMessages.MAX_ENTITY_PUBLIC_REGISTER_NAME_AND_JURISDICTION_LENGTH);
   }
   return true;

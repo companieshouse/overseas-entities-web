44a2b8820062f7c11767bf98ca28664be4307c41
SteveHicksCH <shicks@companieshouse.gov.uk>
2022-12-07 09:24:44 +0000

    remove nodemon changes

--- test/controllers/confirmation.controller.spec.ts
+++ test/controllers/confirmation.controller.spec.ts
@@ -18,7 +18,6 @@ import { get } from "../../src/controllers/confirmation.controller";
 import { hasBOsOrMOs } from "../../src/middleware/navigation/has.beneficial.owners.or.managing.officers.middleware";
 import { WhoIsRegisteringType } from "../../src/model/who.is.making.filing.model";
 import { getLoggedInUserEmail } from "../../src/utils/session";
-import * as config from "../../src/config";
 
 const mockHasBOsOrMOsMiddleware = hasBOsOrMOs as jest.Mock;
 mockHasBOsOrMOsMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
@@ -56,7 +55,6 @@ describe("Confirmation controller tests", () => {
     expect(resp.text).toContain(ENTITY_OBJECT_MOCK.email);
     expect(resp.text).toContain(`£${PAYMENT_FEE}`);
     expect(resp.text).toContain(userMail);
-    expect(resp.text).toContain(config.VF01_FORM_DOWNLOAD_URL);
     expect(mockDeleteApplicationData).toHaveBeenCalledTimes(1);
   });
 
@@ -72,7 +70,6 @@ describe("Confirmation controller tests", () => {
     expect(resp.text).toContain(CONFIRMATION_PAGE_TITLE);
     expect(resp.text).toContain(ENTITY_OBJECT_MOCK.email);
     expect(resp.text).toContain(userMail);
-    expect(resp.text).not.toContain(config.VF01_FORM_DOWNLOAD_URL);
     expect(resp.text).not.toContain(CONFIRMATION_WHAT_YOU_NEED_TO_DO_NOW);
     expect(resp.text).not.toContain(CONFIRMATION_NUMBER_OF_DAYS);
   });

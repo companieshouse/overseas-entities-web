5e96da39024245ee78c600bf02a229f76841f030
olaych <oolatunji@companieshouse.gov.uk>
2022-12-09 10:00:30 +0000

    UAR-93: fixed typo

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -28,5 +28,5 @@ export * as beneficialOwnerDeleteWarning from "./beneficial.owner.delete.warning
 export * as signOut from "./sign.out.controller";
 export * as trustDetails from "./trust.details.controller";
 export * as resumeSubmission from "./resume.submission.controller";
-export * as confirmOverseasCompanyDetails from "./update/company.profile.controller";
-export * as updateOverseasEntityDetails from "./update/update.oversea.entity.details.controller";
+export * as confirmOverseasEntityDetails from "./update/company.profile.controller";
+export * as updateOverseasEntityDetails from "./update/update.overseas.entity.details.controller";

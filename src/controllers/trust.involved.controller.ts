5ec612c06d70f634eada301a75ebb11930115e76
SteveHicksCH <shicks@companieshouse.gov.uk>
2022-12-16 11:38:22 +0000

    case sensitive rename part 2

--- src/controllers/trust.involved.controller.ts
+++ src/controllers/trust.involved.controller.ts
@@ -5,7 +5,7 @@ import { logger } from '../utils/logger';
 import { ApplicationData } from '../model/application.model';
 import { getApplicationData } from '../utils/application.data';
 import { TrustKey } from '../model/trust.model';
-import { mapTrustWhoIsInvolvedToPage } from '../utils/trust/who.iss.Involved.mapper';
+import { mapTrustWhoIsInvolvedToPage } from '../utils/trust/who.is.involved.mapper';
 import * as PageModel from '../model/trust.page.model';
 
 const TRUST_INVOLVED_TEXTS = {

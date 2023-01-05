561bdf8eb5f77d2b1332149893b3dcee6f365642
markpit <mpitten@companieshouse.gov.uk>
2023-01-05 17:07:25 +0000

    ROE-1791 Remove blank line

--- src/controllers/trust.information.controller.ts
+++ src/controllers/trust.information.controller.ts
@@ -37,7 +37,6 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
     // If multiple selected, data is an array.
     const beneficialOwnerIds = (typeof req.body.beneficialOwners === 'string') ? [req.body.beneficialOwners] : req.body.beneficialOwners;
 
-
     const trustData: trustType.Trust[] = JSON.parse(req.body.trusts, (key, value) => {
       if (typeof value === "string") {
         return value.trim();

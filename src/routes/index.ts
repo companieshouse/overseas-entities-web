d7acf0f6bf8c4197faae2287c8b5967b652fbed7
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-01-06 12:54:00 +0000

    uar-106 save entity demo

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -164,6 +164,7 @@ router.get(config.UPDATE_LANDING_URL, updateLanding.get);
 router.get(config.OVERSEAS_ENTITY_QUERY_URL, authentication, overseasEntityQuery.get);
 router.post(config.OVERSEAS_ENTITY_QUERY_URL, authentication, ...validator.overseasEntityQuery, checkValidations, overseasEntityQuery.post);
 router.get(config.OVERSEAS_ENTITY_REVIEW_URL, authentication, overseasEntityReview.get);
+router.post(config.OVERSEAS_ENTITY_REVIEW_URL, authentication, overseasEntityReview.post);
 router.get(config.OVERSEAS_ENTITY_UPDATE_DETAILS_URL, authentication, overseasEntityUpdateDetails.get);
 router.post(config.OVERSEAS_ENTITY_UPDATE_DETAILS_URL, authentication, ...validator.entity, checkValidations, overseasEntityUpdateDetails.post);
 router.get(config.UPDATE_OVERSEAS_ENTITY_CONFIRM_URL, authentication, confirmOverseasEntityDetails.get);

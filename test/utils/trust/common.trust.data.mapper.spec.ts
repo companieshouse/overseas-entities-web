8bc9e3031d236034fae42fd56082d9c45a177f7f
JoshFen95 <jfennell@companieshouse.gov.uk>
2023-01-17 13:59:11 +0000

    fix formatting

--- test/utils/trust/common.trust.data.mapper.spec.ts
+++ test/utils/trust/common.trust.data.mapper.spec.ts
@@ -30,7 +30,6 @@ describe('Common Trust Data Mapper to Page Service', () => {
     });
 
     test('mapCommonTrustDataToPage should return object with mapped data', () => {
-
       expect(mapCommonTrustDataToPage(mockAppData, mockTrust1.trust_id)).toEqual({
         trustId: mockTrust1.trust_id,
         trustName: mockTrust1.trust_name,

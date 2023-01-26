dabdde1cbdac705eed8c17601b4dd496bf61e9ba
corybudd-ch <117734784+corybudd-ch@users.noreply.github.com>
2023-01-26 10:00:00 +0000

    Changing to use router.route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -253,8 +253,11 @@ router.get(config.OVERSEAS_ENTITY_PAYMENT_WITH_TRANSACTION_URL, authentication, 
 
 router.get(config.OVERSEAS_ENTITY_UPDATE_DETAILS_URL, authentication, overseasEntityUpdateDetails.get);
 router.post(config.OVERSEAS_ENTITY_UPDATE_DETAILS_URL, authentication, ...validator.entity, checkValidations, overseasEntityUpdateDetails.post);
-router.get(config.WHO_IS_MAKING_UPDATE_URL, authentication, whoIsMakingUpdate.get);
-router.post(config.WHO_IS_MAKING_UPDATE_URL, authentication, ...validator.whoIsMakingFiling, checkValidations, whoIsMakingUpdate.post);
+
+router.route(config.WHO_IS_MAKING_UPDATE_URL)
+  .all(authentication)
+  .get(whoIsMakingUpdate.get)
+  .post(...validator.whoIsMakingFiling, checkValidations, whoIsMakingUpdate.post);
 
 router.route(config.OVERSEAS_ENTITY_REVIEW_URL)
   .all(authentication)

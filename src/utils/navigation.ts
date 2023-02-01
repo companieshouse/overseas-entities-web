04febfb2709043132accdacddace7d797394aecc
corybudd-ch <117734784+corybudd-ch@users.noreply.github.com>
2023-02-01 15:49:21 +0000

    update nav now UAR-102 has been merged

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -57,7 +57,7 @@ export const NAVIGATION: Navigation = {
   [config.WHO_IS_MAKING_UPDATE_URL]: {
     currentPage: config.WHO_IS_MAKING_UPDATE_PAGE,
     previousPage: () => config.CONFIRM_OVERSEAS_ENTITY_DETAILS_PAGE,
-    nextPage: [config.OVERSEAS_ENTITY_DUE_DILIGENCE_PAGE] // TO DO: UAR-102 needs added
+    nextPage: [config.UPDATE_DUE_DILIGENCE_PAGE, config.UPDATE_DUE_DILIGENCE_OVERSEAS_ENTITY_PAGE]
   },
   [config.UPDATE_DUE_DILIGENCE_OVERSEAS_ENTITY_URL]: {
     currentPage: config.OVERSEAS_ENTITY_DUE_DILIGENCE_PAGE,

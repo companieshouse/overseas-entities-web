793880060c7a31828ce33ce8f802629f8c597e06
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-02-03 14:20:55 +0000

    uar-106 reset model

--- src/model/update.reset.ts
+++ src/model/update.reset.ts
@@ -3,7 +3,27 @@ import { ApplicationData } from "./application.model";
 import { Update } from "./update.type.model";
 
 export const resetEntityUpdate = (appData: ApplicationData): Update => {
+  // Entity data will be filled in by caller so not reset.
   appData.update = {};
+  appData.presenter = {};
+  appData.who_is_registering = undefined;
+  appData.due_diligence = {};
+  appData.overseas_entity_due_diligence = {};
+  appData.payment = undefined;
+  // Save and resume will address overseas_entity_id, transaction_id but reset for now:
+  appData.overseas_entity_id = undefined;
+  appData.transaction_id = undefined;
+  // Any other entity dependent models need to be reset here too.
+  // Not yet covered by update but let's reset:
+  appData.trusts = undefined;
+  appData.beneficial_owners_statement = undefined;
+  appData.beneficial_owners_individual = undefined;
+  appData.beneficial_owners_corporate = undefined;
+  appData.beneficial_owners_government_or_public_authority = undefined;
+  appData.managing_officers_individual = undefined;
+  appData.managing_officers_corporate = undefined;
+  // Don't reset is_secure_register as pre-entity.
+  // Don't reset has_sold_land as registration only.
   return appData.update;
 };
 
--- test/model/update.reset.spec.ts
+++ test/model/update.reset.spec.ts
@@ -1,7 +1,8 @@
 import { ApplicationData, resetEntityUpdate } from "../../src/model";
 import { describe, expect, test } from '@jest/globals';
+import { WhoIsRegisteringType } from "../../src/model/who.is.making.filing.model";
 
-describe("reet update model", () => {
+describe("reset update model", () => {
 
   test("does reset update in empty model", () => {
     const appData: ApplicationData = {
@@ -21,4 +22,89 @@ describe("reet update model", () => {
     expect(update).toEqual({});
     expect(appData.update).toEqual({});
   });
+
+  test("does reset presenter in empty model", () => {
+    const appData: ApplicationData = {
+    };
+    resetEntityUpdate(appData);
+    expect(appData.presenter).toEqual({});
+  });
+
+  test("does reset presenter in model", () => {
+    const appData: ApplicationData = {
+      presenter: {
+        full_name: "John Doe",
+        email: "john.doe@test.com"
+      }
+    };
+    resetEntityUpdate(appData);
+    expect(appData.presenter).toEqual({});
+  });
+
+  test("does reset due diligence in empty model", () => {
+    const appData: ApplicationData = {
+    };
+    resetEntityUpdate(appData);
+    expect(appData.due_diligence).toEqual({});
+  });
+
+  test("does reset due diligence in model", () => {
+    const appData: ApplicationData = {
+      due_diligence: {
+        name: "Alice Keys"
+      }
+    };
+    resetEntityUpdate(appData);
+    expect(appData.due_diligence).toEqual({});
+  });
+
+  test("does reset overseas entity due diligence in empty model", () => {
+    const appData: ApplicationData = {
+    };
+    resetEntityUpdate(appData);
+    expect(appData.overseas_entity_due_diligence).toEqual({});
+  });
+
+  test("does reset overseas entity due diligence in model", () => {
+    const appData: ApplicationData = {
+      due_diligence: {
+        name: "Alice Keys"
+      }
+    };
+    resetEntityUpdate(appData);
+    expect(appData.overseas_entity_due_diligence).toEqual({});
+  });
+
+  test("does reset who is registering in empty model", () => {
+    const appData: ApplicationData = {
+    };
+    resetEntityUpdate(appData);
+    expect(appData.who_is_registering).toBeUndefined();
+  });
+
+  test("does reset who is registering in model", () => {
+    const appData: ApplicationData = {
+      who_is_registering: WhoIsRegisteringType.AGENT
+    };
+    resetEntityUpdate(appData);
+    expect(appData.who_is_registering).toBeUndefined();
+  });
+
+  test("does reset in model", () => {
+    const appData: ApplicationData = {
+      who_is_registering: WhoIsRegisteringType.AGENT
+    };
+    resetEntityUpdate(appData);
+    expect(appData.payment).toBeUndefined();
+    expect(appData.overseas_entity_id).toBeUndefined();
+    expect(appData.transaction_id).toBeUndefined();
+    expect(appData.trusts).toBeUndefined();
+    expect(appData.beneficial_owners_statement).toBeUndefined();
+    expect(appData.beneficial_owners_individual).toBeUndefined();
+    expect(appData.beneficial_owners_corporate).toBeUndefined();
+    expect(appData.beneficial_owners_government_or_public_authority).toBeUndefined();
+    expect(appData.managing_officers_individual).toBeUndefined();
+    expect(appData.managing_officers_corporate).toBeUndefined();
+  });
 });
+

f532b1d9abf39aed55cb9acef7530cef81f533f4
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-02-03 12:48:42 +0000

    uar-107 rely on validation

--- src/controllers/update/company.profile.controller.ts
+++ src/controllers/update/company.profile.controller.ts
@@ -3,19 +3,21 @@ import { ApplicationData } from "../../model";
 import { logger } from "../../utils/logger";
 import * as config from "../../config";
 import { getApplicationData } from "../../utils/application.data";
+import { Update } from "model/update.type.model";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
     logger.debugRequest(req, `GET ${config.CONFIRM_OVERSEAS_ENTITY_DETAILS_PAGE}`);
 
     const appData: ApplicationData = getApplicationData(req.session);
+    const update = appData.update as Update;
 
     return res.render(config.CONFIRM_OVERSEAS_ENTITY_DETAILS_PAGE, {
       backLinkUrl: config.OVERSEAS_ENTITY_QUERY_URL,
       updateUrl: config.UPDATE_OVERSEAS_ENTITY_CONFIRM_URL,
       templateName: config.CONFIRM_OVERSEAS_ENTITY_DETAILS_PAGE,
       appData,
-      registrationDate: appData.update?.date_of_creation
+      registrationDate: update.date_of_creation
     });
   }  catch (errors) {
     logger.errorRequest(req, errors);

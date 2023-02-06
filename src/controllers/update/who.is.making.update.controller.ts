cdee589579755c2fb8705dc029167696ec7f95f6
corybudd-ch <117734784+corybudd-ch@users.noreply.github.com>
2023-02-06 13:48:06 +0000

    UAR-104: due diligence update journey - overseas entity filing update (#629)
    * WIP: new controller & route, copy existing view for now
    
    * add template that register and update pages share
    
    * move/rename directory for shared-templates
    
    * adding GET tests
    
    * add navigation logic for UAR-101, first agent details page
    
    * WIP: new controller & route, copy existing view for now
    
    * add template that register and update pages share
    
    * move/rename directory for shared-templates
    
    * adding GET tests
    
    * add navigation logic for UAR-101, first agent details page
    
    * add navigation check
    
    * add navigation, add POST tests, update POST's redirect URL
    
    * refactored overseas entity due deligence controller
    
    * used common due deligence page to remove sonar duplicated lines fail
    
    * add hasUpdatePresenter check to whoIsMakingUpdate route, adding separate view for due diligence OE
    
    * spelling fixes, removing unnecessary/empty lines, rename common-code folder to checks, update imports
    
    * remove empty line
    
    * update who is filing common code to take agentUrl and oeUrl as params
    
    * update nav now UAR-102 has been merged
    
    * Update nav checks redirects to SECURE_UPDATE_FILTER_URL
    
    * fix to use new UPDATE page not existing register
    
    * remove whitespace/code styling updates
    
    ---------
    
    Co-authored-by: olaych <oolatunji@companieshouse.gov.uk>

--- src/controllers/update/who.is.making.update.controller.ts
+++ src/controllers/update/who.is.making.update.controller.ts
@@ -1,47 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 
-import { logger } from "../../utils/logger";
 import {
   WHO_IS_MAKING_UPDATE_PAGE,
   OVERSEAS_ENTITY_PRESENTER_URL,
   UPDATE_DUE_DILIGENCE_URL,
-  OVERSEAS_ENTITY_REVIEW_PAGE
+  UPDATE_DUE_DILIGENCE_OVERSEAS_ENTITY_URL
 } from "../../config";
-import { ApplicationData } from "../../model";
-import { getApplicationData, setExtraData } from "../../utils/application.data";
-import { WhoIsRegisteringKey,  WhoIsRegisteringType } from "../../model/who.is.making.filing.model";
+import { getWhoIsFiling, postWhoIsFiling } from "../../utils/who.is.making.filing";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    logger.debugRequest(req, `${req.method} ${req.route.path}`);
-    const appData: ApplicationData = getApplicationData(req.session);
-
-    return res.render(WHO_IS_MAKING_UPDATE_PAGE, {
-      backLinkUrl: OVERSEAS_ENTITY_PRESENTER_URL,
-      templateName: WHO_IS_MAKING_UPDATE_PAGE,
-      [WhoIsRegisteringKey]: appData[WhoIsRegisteringKey]
-    });
-  } catch (error) {
-    logger.errorRequest(req, error);
-    next(error);
-  }
+  getWhoIsFiling(req, res, next, WHO_IS_MAKING_UPDATE_PAGE, OVERSEAS_ENTITY_PRESENTER_URL);
 };
 
 export const post = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    logger.debugRequest(req, `${req.method} ${req.route.path}`);
-    const whoIsUpdating = req.body[WhoIsRegisteringKey];
-
-    setExtraData(req.session, { ...getApplicationData(req.session), [WhoIsRegisteringKey]: whoIsUpdating });
-
-    if ( whoIsUpdating === WhoIsRegisteringType.AGENT ){
-      return res.redirect(UPDATE_DUE_DILIGENCE_URL);
-    } else {
-      // TO DO: actual re-direct to be implemented for UAR-104
-      return res.redirect(OVERSEAS_ENTITY_REVIEW_PAGE);
-    }
-  } catch (error) {
-    logger.errorRequest(req, error);
-    next(error);
-  }
+  postWhoIsFiling(req, res, next, UPDATE_DUE_DILIGENCE_URL, UPDATE_DUE_DILIGENCE_OVERSEAS_ENTITY_URL);
 };

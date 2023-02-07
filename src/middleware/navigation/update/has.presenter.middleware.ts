7232905418b5a813ffeb4691f26d8b0f2b7333ab
corybudd-ch <117734784+corybudd-ch@users.noreply.github.com>
2023-02-07 08:38:15 +0000

    UAR-104: Fix for nav check, hasPresenter reinstated, hasUpdatePresenter new code (#648)
    * Fix for UAR-104, register hasPresenter reinstated, hasUpdatePresenter new/separate code
    
    * Create spec for has update presenter middleware
    
    * add check condition test

--- src/middleware/navigation/update/has.presenter.middleware.ts
+++ src/middleware/navigation/update/has.presenter.middleware.ts
@@ -1,8 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 
-import { checkIfPresenterDataExists } from '../checks/has.presenter';
+import { logger } from '../../../utils/logger';
+import { getApplicationData } from "../../../utils/application.data";
+import { checkUpdatePresenterEntered, NavigationErrorMessage } from '.././check.condition';
 import { SECURE_UPDATE_FILTER_URL } from '../../../config';
 
 export const hasUpdatePresenter = (req: Request, res: Response, next: NextFunction): void => {
-  checkIfPresenterDataExists(req, res, next, SECURE_UPDATE_FILTER_URL);
+  try {
+    if (!checkUpdatePresenterEntered(getApplicationData(req.session))) {
+      logger.infoRequest(req, NavigationErrorMessage);
+      return res.redirect(SECURE_UPDATE_FILTER_URL);
+    }
+    next();
+  } catch (err) {
+    next(err);
+  }
 };

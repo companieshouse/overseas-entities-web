6fcbfee8c506f795e26bc1ca62bbf85414b15396
mattch1 <mculpin@companieshouse.gov.uk>
2023-02-09 08:33:06 +0000

    ROE-1724 Remove duplicated register error message at top of page

--- src/validation/fields/public-register.validation.ts
+++ src/validation/fields/public-register.validation.ts
@@ -15,8 +15,7 @@ export const entity_public_register_validations = [
     .custom((_, { req }) => checkPublicRegisterJurisdiction(req.body["public_register_name"], req.body["public_register_jurisdiction"])),
   body("public_register_jurisdiction")
     .custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.PUBLIC_REGISTER_JURISDICTION, value) )
-    .custom((value, { req }) => checkInvalidCharactersIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.PUBLIC_REGISTER_JURISDICTION_INVALID_CHARACTERS, value) )
-    .custom((_, { req }) => checkPublicRegisterJurisdiction(req.body["public_register_name"], req.body["public_register_jurisdiction"])),
+    .custom((value, { req }) => checkInvalidCharactersIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.PUBLIC_REGISTER_JURISDICTION_INVALID_CHARACTERS, value) ),
   body("registration_number")
     .custom((value, { req }) => checkFieldIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.PUBLIC_REGISTER_NUMBER, value) )
     .custom((value, { req }) => checkMaxFieldIfRadioButtonSelected(req.body.is_on_register_in_country_formed_in === '1', ErrorMessages.MAX_ENTITY_PUBLIC_REGISTER_NUMBER_LENGTH, 32, value) )

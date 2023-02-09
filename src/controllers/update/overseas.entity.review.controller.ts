50fb80693d82aee8ffc0eb2c4fe2af18c2444b04
cdrainCH <cdrain@companieshouse.gov.uk>
2023-02-09 16:05:51 +0000

    reverted next page from oe review back to BO type page

--- src/controllers/update/overseas.entity.review.controller.ts
+++ src/controllers/update/overseas.entity.review.controller.ts
@@ -31,8 +31,8 @@ export const get = (req: Request, res: Response, next: NextFunction) => {
 export const post = (req: Request, res: Response, next: NextFunction) => {
   try {
     logger.debugRequest(req, `POST ${config.OVERSEAS_ENTITY_REVIEW_PAGE}`);
-    // return res.redirect(config.UPDATE_BENEFICIAL_OWNER_TYPE_PAGE);
-    return res.redirect(config.UPDATE_MANAGING_OFFICER_TYPE_PAGE);
+    return res.redirect(config.UPDATE_BENEFICIAL_OWNER_TYPE_PAGE);
+    // return res.redirect(config.UPDATE_MANAGING_OFFICER_TYPE_PAGE);
   } catch (errors) {
     logger.errorRequest(req, errors);
     next(errors);

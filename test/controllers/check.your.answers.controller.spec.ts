ac6961ad2b90dffec53ea09b97976159494042e9
JoshFen95 <jfennell@companieshouse.gov.uk>
2023-02-24 09:03:45 +0000

    roe-1566: fix test

--- test/controllers/check.your.answers.controller.spec.ts
+++ test/controllers/check.your.answers.controller.spec.ts
@@ -460,7 +460,7 @@ describe("GET tests", () => {
   });
 
   test(`renders the ${CHECK_YOUR_ANSWERS_PAGE} page with trust data and feature flag on`, async () => {
-    mockIsActiveFeature.mockReturnValueOnce(true); // FEATURE_FLAG_ENABLE_TRUSTS_WEB flag
+    mockIsActiveFeature.mockReturnValue(true); // FEATURE_FLAG_ENABLE_TRUSTS_WEB flag
 
     const mockAppData = {
       ...APPLICATION_DATA_MOCK,
@@ -476,12 +476,11 @@ describe("GET tests", () => {
 
     expect(resp.status).toEqual(200);
     expect(resp.text).not.toContain(BENEFICIAL_OWNER_TYPE_LINK); // back button
-    // expect(resp.text).toContain(TRUST_INFORMATION_LINK); // back button
     expect(resp.text).toContain(`${TRUST_ENTRY_URL + "/" + TRUST_WITH_ID.trust_id + ADD_TRUST_URL}`); // back button
     expect(resp.text).toContain(CHECK_YOUR_ANSWERS_PAGE_TRUST_TITLE);
     expect(resp.text).toContain(`${TRUST_DETAILS_URL}/${TRUST_WITH_ID.trust_id}`);
     expect(resp.text).toContain(TRUST_WITH_ID.trust_name);
-    // expect(resp.text).toMatch(/31\s+December\s+1999/m);
+    expect(resp.text).toMatch(/31\s+December\s+1999/m);
   });
 
   test(`renders the ${CHECK_YOUR_ANSWERS_PAGE} page with no trust data`, async () => {

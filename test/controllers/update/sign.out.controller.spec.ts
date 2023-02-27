ffea0e02ad4a56c858e4fc104a4e0e8519439993
corybudd-ch <117734784+corybudd-ch@users.noreply.github.com>
2023-02-27 11:45:40 +0000

    add 2 remaining tests and mock serviceAvailability to bypass feature flag check

--- test/controllers/update/sign.out.controller.spec.ts
+++ test/controllers/update/sign.out.controller.spec.ts
@@ -1,8 +1,8 @@
-import { ErrorMessages } from "../../../src/validation/error.messages";
-
 jest.mock("ioredis");
 jest.mock("../../../src/utils/logger");
+jest.mock('../../../src/middleware/service.availability.middleware');
 
+import { ErrorMessages } from "../../../src/validation/error.messages";
 import { beforeEach, expect, jest, test, describe } from "@jest/globals";
 import request from "supertest";
 import { createAndLogErrorRequest, logger } from '../../../src/utils/logger';
@@ -11,12 +11,20 @@ import app from "../../../src/app";
 
 import {
   ANY_MESSAGE_ERROR,
+  PAGE_NOT_FOUND_TEXT,
   SIGN_OUT_HINT_TEXT,
   SIGN_OUT_PAGE_TITLE
 } from "../../__mocks__/text.mock";
 
+import { serviceAvailabilityMiddleware } from "../../../src/middleware/service.availability.middleware";
+import { NextFunction, Request, Response } from "express";
+
 const mockLoggerDebugRequest = logger.debugRequest as jest.Mock;
 const mockCreateAndLogErrorRequest = createAndLogErrorRequest as jest.Mock;
+
+const mockServiceAvailabilityMiddleware = serviceAvailabilityMiddleware as jest.Mock;
+mockServiceAvailabilityMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
+
 const previousPage = `${config.UPDATE_AN_OVERSEAS_ENTITY_URL}${config.SECURE_UPDATE_FILTER_PAGE}`;
 
 describe("SIGN OUT controller", () => {
@@ -56,6 +64,28 @@ describe("SIGN OUT controller", () => {
       expect(resp.status).toEqual(302);
       expect(mockLoggerDebugRequest).toHaveBeenCalledTimes(1);
       expect(mockCreateAndLogErrorRequest).not.toHaveBeenCalled();
+    });
+
+    test(`redirects to ${config.SECURE_UPDATE_FILTER_PAGE}, the previous page when no is selected`, async () => {
+      const resp = await request(app)
+        .post(config.UPDATE_SIGN_OUT_URL)
+        .send({ sign_out: 'no', previousPage });
+
+      expect(resp.status).toEqual(302);
+      expect(resp.header.location).toEqual(previousPage);
+      expect(mockLoggerDebugRequest).toHaveBeenCalledTimes(1);
+      expect(mockCreateAndLogErrorRequest).not.toHaveBeenCalled();
+    });
+
+    test(`should rejecting redirect, throw an error and render not found page`, async () => {
+      const mockPreviousPage = "wrong/path";
+      const resp = await request(app)
+        .post(config.UPDATE_SIGN_OUT_URL)
+        .send({ sign_out: 'yes', previousPage: mockPreviousPage });
+
+      expect(resp.status).toEqual(404);
+      expect(resp.text).toContain(PAGE_NOT_FOUND_TEXT);
+      expect(mockCreateAndLogErrorRequest).toHaveBeenCalledTimes(1);
     });
 
     test("catch error when posting the page with no selection", async () => {

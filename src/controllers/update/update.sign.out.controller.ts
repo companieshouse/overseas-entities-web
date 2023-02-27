c612b270c8fb916b299f2d9185dbfe10c33efd89
garywco <gcoulter@companieshouse.gov.uk>
2023-02-27 14:39:44 +0000

    UAR-266: Removing blank lines

--- src/controllers/update/update.sign.out.controller.ts
+++ src/controllers/update/update.sign.out.controller.ts
@@ -1,12 +1,10 @@
 import { NextFunction, Request, Response } from "express";
-
 import { createAndLogErrorRequest, logger } from "../../utils/logger";
 import * as config from "../../config";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
     logger.debugRequest(req, `GET ${config.UPDATE_SIGN_OUT_URL}`);
-
     return res.render(config.UPDATE_SIGN_OUT_PAGE, {
       previousPage: `${config.UPDATE_AN_OVERSEAS_ENTITY_URL}${req.query["page"]}`,
       url: config.UPDATE_AN_OVERSEAS_ENTITY_URL
@@ -21,16 +19,13 @@ export const post = (req: Request, res: Response, next: NextFunction) => {
   try {
     logger.debugRequest(req, `POST ${config.UPDATE_SIGN_OUT_PAGE}`);
     const previousPage = req.body["previousPage"];
-
     if (!previousPage.startsWith(config.UPDATE_AN_OVERSEAS_ENTITY_URL)){
       throw createAndLogErrorRequest(req, `${previousPage} page is not part of the journey!`);
     }
-
     if (req.body["sign_out"] === 'yes') {
     // This url may change depending on decision made on ticket UAR-267
       return res.redirect(config.UPDATE_ACCOUNTS_SIGN_OUT_URL);
     }
-
     return res.redirect(previousPage);
   } catch (error) {
     logger.errorRequest(req, error);

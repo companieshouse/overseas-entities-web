743bd53d7ec90f7886705096bbcb343364260348
okoye-companieshouse <121234308+okoye-companieshouse@users.noreply.github.com>
2023-03-06 14:56:01 +0000

    roe-1548 fix to date errors (#718)
    * roe-1548 fix to date errors
    
    * roe-1548 writing tests
    
    * roe-1548 adding date test for today
    
    * roe-1548 simplifying tests
    
    * roe-1548 simplifying tests
    
    * roe-1548 correcting failed test
    
    * roe-1548 addressing PR recommendation
    
    * roe-1548 rename functions with appropriate names
    
    * roe-1548 rename functions with appropriate names

--- src/validation/fields/helper/date.validation.helper.ts
+++ src/validation/fields/helper/date.validation.helper.ts
@@ -6,10 +6,12 @@ export const dateValidations = (dateContext: dateContext, min: number, max: numb
   return [
     body(dateContext.day.name)
       .if(body(dateContext.year.name).isLength({ min: min, max: max }))
-      .custom((value, { req }) => dateContext.day.callBack(dateContext.day.errMsg[0], req.body[dateContext.day.name], req.body[dateContext.month.name], req.body[dateContext.year.name])),
+      .custom((value, { req }) => dateContext.day.callBack(dateContext.day.errMsg[0], req.body[dateContext.day.name], req.body[dateContext.month.name], req.body[dateContext.year.name]))
+      .isLength({ min: 1, max: 2 }).withMessage(ErrorMessages.DAY_LENGTH),
     body(dateContext.month.name)
       .if(body(dateContext.year.name).isLength({ min: min, max: max }))
-      .custom((value, { req }) => dateContext.month.callBack(dateContext.month.errMsg[0], req.body[dateContext.day.name], req.body[dateContext.month.name], req.body[dateContext.year.name])),
+      .custom((value, { req }) => dateContext.month.callBack(dateContext.month.errMsg[0], req.body[dateContext.day.name], req.body[dateContext.month.name], req.body[dateContext.year.name]))
+      .isLength({ min: 1, max: 2 }).withMessage(ErrorMessages.MONTH_LENGTH),
     body(dateContext.year.name)
       .custom((value, { req }) => dateContext.year.callBack(dateContext.year.errMsg[0], dateContext.day.errMsg[1], req.body[dateContext.day.name], req.body[dateContext.month.name], req.body[dateContext.year.name])),
     body(dateContext.dateInput.name)
@@ -23,11 +25,13 @@ export const conditionalDateValidations = (dateContextWithCondition: dateContext
     body(dateContextWithCondition.day.name)
       .if(body(dateContextWithCondition.condition.elementName).equals(dateContextWithCondition.condition.expectedValue))
       .if(body(dateContextWithCondition.year.name).isLength({ min: min, max: max }))
-      .custom((value, { req }) => dateContextWithCondition.day.callBack(dateContextWithCondition.day.errMsg[0], req.body[dateContextWithCondition.day.name], req.body[dateContextWithCondition.month.name], req.body[dateContextWithCondition.year.name])),
+      .custom((value, { req }) => dateContextWithCondition.day.callBack(dateContextWithCondition.day.errMsg[0], req.body[dateContextWithCondition.day.name], req.body[dateContextWithCondition.month.name], req.body[dateContextWithCondition.year.name]))
+      .isLength({ min: 1, max: 2 }).withMessage(ErrorMessages.DAY_LENGTH),
     body(dateContextWithCondition.month.name)
       .if(body(dateContextWithCondition.condition.elementName).equals(dateContextWithCondition.condition.expectedValue))
       .if(body(dateContextWithCondition.year.name).isLength({ min: min, max: max }))
-      .custom((value, { req }) => dateContextWithCondition.month.callBack(dateContextWithCondition.month.errMsg[0], req.body[dateContextWithCondition.day.name], req.body[dateContextWithCondition.month.name], req.body[dateContextWithCondition.year.name])),
+      .custom((value, { req }) => dateContextWithCondition.month.callBack(dateContextWithCondition.month.errMsg[0], req.body[dateContextWithCondition.day.name], req.body[dateContextWithCondition.month.name], req.body[dateContextWithCondition.year.name]))
+      .isLength({ min: 1, max: 2 }).withMessage(ErrorMessages.MONTH_LENGTH),
     body(dateContextWithCondition.year.name)
       .if(body(dateContextWithCondition.condition.elementName).equals(dateContextWithCondition.condition.expectedValue))
       .custom((value, { req }) => dateContextWithCondition.year.callBack(dateContextWithCondition.year.errMsg[0], dateContextWithCondition.day.errMsg[1], req.body[dateContextWithCondition.day.name], req.body[dateContextWithCondition.month.name], req.body[dateContextWithCondition.year.name])),
@@ -38,10 +42,10 @@ export const conditionalDateValidations = (dateContextWithCondition: dateContext
 };
 
 export type dateContext = {
-  day: {name: string, errMsg: ErrorMessages[], callBack: (err: ErrorMessages, dayValue: string, monthValue: string, yearValue: string) => boolean }
-  month: {name: string, errMsg: ErrorMessages[], callBack: (err: ErrorMessages, dayValue: string, monthValue: string, yearValue: string) => boolean },
-  year: {name: string, errMsg: ErrorMessages[], callBack: (err1: ErrorMessages, err2: ErrorMessages, dayValue: string, monthValue: string, yearValue: string) => boolean },
-  dateInput: {name: string, errMsg: ErrorMessages[], callBack: (dayValue: string, monthValue: string, yearValue: string) => boolean },
+  day: {name: string, errMsg: ErrorMessages[], callBack: (err: ErrorMessages, dayValue: string, monthValue: string, yearValue: string) => boolean}
+  month: {name: string, errMsg: ErrorMessages[], callBack: (err: ErrorMessages, dayValue: string, monthValue: string, yearValue: string) => boolean},
+  year: {name: string, errMsg: ErrorMessages[], callBack: (err1: ErrorMessages, err2: ErrorMessages, dayValue: string, monthValue: string, yearValue: string) => boolean},
+  dateInput: {name: string, errMsg: ErrorMessages[], callBack: (dayValue: string, monthValue: string, yearValue: string) => boolean},
 };
 
 export type dateContextWithCondition = dateContext & {condition: {elementName: string, expectedValue: string}};
--- test/validation/date.validations.spec.ts
+++ test/validation/date.validations.spec.ts
@@ -1,5 +1,5 @@
 import { RoleWithinTrustType } from '../../src/model/role.within.trust.type.model';
-import { checkDateFieldDay, checkDateFieldMonth, checkDateFieldYear, checkStartDate } from '../../src/validation/custom.validation';
+import { checkAllBirthDateFieldsForErrors, checkDateFieldsForErrors, checkBirthDate, checkDate, checkDateFieldDay, checkDateFieldMonth, checkDateFieldYear, checkStartDate } from '../../src/validation/custom.validation';
 import { ErrorMessages } from '../../src/validation/error.messages';
 import { dateValidations, dateContext, conditionalDateValidations, dateContextWithCondition } from '../../src/validation/fields/helper/date.validation.helper';
 
@@ -7,6 +7,7 @@ const mockIsLength = jest.fn();
 const mockIf = jest.fn();
 const mockCustom = jest.fn();
 const mockEquals = jest.fn();
+const mockWithMessage = jest.fn().mockReturnValue("Hello World");
 
 jest.mock('express-validator', () => ({
   body: jest.fn().mockImplementation(() => ({
@@ -14,6 +15,7 @@ jest.mock('express-validator', () => ({
     if: mockIf.mockReturnThis(),
     custom: mockCustom.mockReturnThis(),
     equals: mockEquals.mockReturnThis(),
+    withMessage: mockWithMessage,
   })),
 }));
 
@@ -24,6 +26,7 @@ describe('Test to validate date validator', () => {
     mockIf.mockRestore();
     mockCustom.mockRestore();
     mockEquals.mockRestore();
+    mockWithMessage.mockRestore();
   });
 
   test('should test dateValidations', () => {
@@ -53,9 +56,11 @@ describe('Test to validate date validator', () => {
 
     const sut = dateValidations(mockDateValidationsContext, 4, 4);
     expect(sut.length).toEqual(4);
-    expect(mockIsLength).toBeCalledTimes(2);
+    expect(mockIsLength).toBeCalledTimes(4);
     expect(mockIf).toBeCalledTimes(2);
     expect(mockCustom).toBeCalledTimes(4);
+    expect(mockWithMessage).toBeCalledTimes(2);
+    expect(mockEquals).toBeCalledTimes(0);
   });
 
   test('should test conditionalDateValidations', () => {
@@ -86,8 +91,95 @@ describe('Test to validate date validator', () => {
 
     const sut = conditionalDateValidations(mockDateValidationsContext, 4, 4);
     expect(sut.length).toEqual(4);
-    expect(mockIsLength).toBeCalledTimes(2);
+    expect(mockIsLength).toBeCalledTimes(4);
     expect(mockIf).toBeCalledTimes(6);
     expect(mockCustom).toBeCalledTimes(4);
+    expect(mockWithMessage).toBeCalledTimes(2);
+    expect(mockEquals).toBeCalledTimes(4);
+  });
+});
+
+describe("test date method", () => {
+  const today = new Date();
+  const day = today.getDate().toString();
+  const month = (today.getMonth() + 1).toString();
+  const year = today.getFullYear().toString();
+
+  const errMsgCheckAllDateFieldsPresent: ErrorMessages[] = [ErrorMessages.ENTER_DATE,
+    ErrorMessages.MONTH_AND_YEAR,
+    ErrorMessages.DAY_AND_YEAR,
+    ErrorMessages.DAY_AND_MONTH,
+    ErrorMessages.DAY,
+    ErrorMessages.MONTH];
+
+  const errMsgcheckAllBirthDateFieldsPresent: ErrorMessages[] = [ErrorMessages.ENTER_DATE_OF_BIRTH,
+    ErrorMessages.MONTH_AND_YEAR_OF_BIRTH,
+    ErrorMessages.DAY_AND_YEAR_OF_BIRTH,
+    ErrorMessages.DAY_AND_MONTH_OF_BIRTH,
+    ErrorMessages.DAY_OF_BIRTH,
+    ErrorMessages.MONTH_OF_BIRTH];
+
+  const testDateFieldCheck = (err: ErrorMessages[]) => [
+    ["", "", "", err[0]],
+    ["02", "", "", err[1]],
+    ["", "02", "", err[2]],
+    ["", "", "2009", err[3]],
+    ["", "10", "2009", err[4]],
+    ["10", "", "2009", err[5]]
+  ];
+
+  const errMsgcheckDate: ErrorMessages[] = [
+    ErrorMessages.ENTER_DATE,
+    ErrorMessages.MONTH_AND_YEAR,
+    ErrorMessages.DAY_AND_YEAR,
+    ErrorMessages.DAY_AND_MONTH,
+    ErrorMessages.INVALID_DATE,
+    ErrorMessages.INVALID_DATE,
+    ErrorMessages.YEAR_LENGTH,
+    ErrorMessages.INVALID_DATE,
+    ErrorMessages.DATE_NOT_IN_PAST_OR_TODAY,
+    ErrorMessages.DATE_NOT_IN_PAST_OR_TODAY
+  ];
+
+  const errMsgcheckBirthDate: ErrorMessages[] = [
+    ErrorMessages.ENTER_DATE_OF_BIRTH,
+    ErrorMessages.MONTH_AND_YEAR_OF_BIRTH,
+    ErrorMessages.DAY_AND_YEAR_OF_BIRTH,
+    ErrorMessages.DAY_AND_MONTH_OF_BIRTH,
+    ErrorMessages.INVALID_DATE_OF_BIRTH,
+    ErrorMessages.INVALID_DATE_OF_BIRTH,
+    ErrorMessages.DATE_OF_BIRTH_YEAR_LENGTH,
+    ErrorMessages.INVALID_DATE_OF_BIRTH,
+    ErrorMessages.DATE_OF_BIRTH_NOT_IN_PAST,
+    ErrorMessages.DATE_OF_BIRTH_NOT_IN_PAST
+  ];
+
+  const testDateCheck = (err: ErrorMessages[]) => [
+    ["", "", "", err[0]],
+    ["02", "", "", err[1]],
+    ["", "02", "", err[2]],
+    ["", "", "2009", err[3]],
+    ["0", "10", "2009", err[4]],
+    ["10", "0", "2009", err[5]],
+    ["10", "10", "209", err[6]],
+    ["10", "a", "2009", err[7]],
+    ["10", "10", "9999", err[8]],
+    [day, month, year, err[9]]
+  ];
+
+  test.each(testDateFieldCheck(errMsgCheckAllDateFieldsPresent))("should throw appropriate date errors for checkAllDateFieldsPresent", ( _day, _month, _year, _err ) => {
+    expect(() => checkDateFieldsForErrors(_day, _month, _year)).toThrow(_err);
+  });
+
+  test.each(testDateFieldCheck(errMsgcheckAllBirthDateFieldsPresent))("should throw appropriate date errors for checkAllBirthDateFieldsPresent", (_day, _month, _year, _err ) => {
+    expect(() => checkAllBirthDateFieldsForErrors(_day, _month, _year)).toThrow(_err);
+  });
+
+  test.each(testDateCheck(errMsgcheckDate))("should throw appropriate date errors for checkDate", (_day, _month, _year, _err) => {
+    expect(() => checkDate(_day, _month, _year)).toThrow(_err);
+  });
+
+  test.each(testDateCheck(errMsgcheckBirthDate))("should throw appropriate date errors for checkBirthDate", (_day, _month, _year, _err) => {
+    expect(() => checkBirthDate(_day, _month, _year)).toThrow(_err);
   });
 });

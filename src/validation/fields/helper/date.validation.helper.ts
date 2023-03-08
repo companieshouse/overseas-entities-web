d8eca76a9921c4c0c3f580bb5af1ca0a6ebbcc19
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2023-03-08 16:33:08 +0000

    roe-1548 simplifying validation

--- src/validation/fields/helper/date.validation.helper.ts
+++ src/validation/fields/helper/date.validation.helper.ts
@@ -4,12 +4,7 @@ import { ErrorMessages } from "../../../validation/error.messages";
 // NOSONAR
 export const dateValidations = (dateContext: dateContext) => {
   return [
-    body(dateContext.day.name)
-      .custom((value, { req }) => dateContext.day.callBack(dateContext.day.errMsg[0], req.body[dateContext.day.name], req.body[dateContext.month.name], req.body[dateContext.year.name])),
-    body(dateContext.month.name)
-      .custom((value, { req }) => dateContext.month.callBack(dateContext.month.errMsg[0], req.body[dateContext.day.name], req.body[dateContext.month.name], req.body[dateContext.year.name])),
-    body(dateContext.year.name)
-      .custom((value, { req }) => dateContext.year.callBack(dateContext.year.errMsg[0], dateContext.day.errMsg[1], req.body[dateContext.day.name], req.body[dateContext.month.name], req.body[dateContext.year.name])),
+
     body(dateContext.dateInput.name)
       .custom((value, { req }) => dateContext.dateInput.callBack(req.body[dateContext.day.name], req.body[dateContext.month.name], req.body[dateContext.year.name])),
   ];
@@ -18,15 +13,6 @@ export const dateValidations = (dateContext: dateContext) => {
 // NOSONAR
 export const conditionalDateValidations = (dateContextWithCondition: dateContextWithCondition) => {
   return [
-    body(dateContextWithCondition.day.name)
-      .if(body(dateContextWithCondition.condition.elementName).equals(dateContextWithCondition.condition.expectedValue))
-      .custom((value, { req }) => dateContextWithCondition.day.callBack(dateContextWithCondition.day.errMsg[0], req.body[dateContextWithCondition.day.name], req.body[dateContextWithCondition.month.name], req.body[dateContextWithCondition.year.name])),
-    body(dateContextWithCondition.month.name)
-      .if(body(dateContextWithCondition.condition.elementName).equals(dateContextWithCondition.condition.expectedValue))
-      .custom((value, { req }) => dateContextWithCondition.month.callBack(dateContextWithCondition.month.errMsg[0], req.body[dateContextWithCondition.day.name], req.body[dateContextWithCondition.month.name], req.body[dateContextWithCondition.year.name])),
-    body(dateContextWithCondition.year.name)
-      .if(body(dateContextWithCondition.condition.elementName).equals(dateContextWithCondition.condition.expectedValue))
-      .custom((value, { req }) => dateContextWithCondition.year.callBack(dateContextWithCondition.year.errMsg[0], dateContextWithCondition.day.errMsg[1], req.body[dateContextWithCondition.day.name], req.body[dateContextWithCondition.month.name], req.body[dateContextWithCondition.year.name])),
     body(dateContextWithCondition.dateInput.name)
       .if(body(dateContextWithCondition.condition.elementName).equals(dateContextWithCondition.condition.expectedValue))
       .custom((value, { req }) => dateContextWithCondition.dateInput.callBack(req.body[dateContextWithCondition.day.name], req.body[dateContextWithCondition.month.name], req.body[dateContextWithCondition.year.name])),

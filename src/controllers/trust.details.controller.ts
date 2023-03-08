06e50f1e41ebc081022ee41c284b59f62a228b95
SteveHicksCH <shicks@companieshouse.gov.uk>
2023-03-08 16:49:25 +0000

    change code to retain trustees

--- src/controllers/trust.details.controller.ts
+++ src/controllers/trust.details.controller.ts
@@ -1,11 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import * as config from '../config';
 import { logger } from '../utils/logger';
-import { getBoIndividualAssignableToTrust, getBoOtherAssignableToTrust } from '../utils/trusts';
+import { saveTrustInApp, getBoIndividualAssignableToTrust, getBoOtherAssignableToTrust, getTrustByIdFromApp } from '../utils/trusts';
 import { getApplicationData, setExtraData } from '../utils/application.data';
 import * as mapperDetails from '../utils/trust/details.mapper';
 import * as mapperBo from '../utils/trust/beneficial.owner.mapper';
-import { saveTrustInApp } from '../utils/trusts';
 import { ApplicationData } from '../model/application.model';
 import * as PageModel from '../model/trust.page.model';
 import { BeneficialOwnerIndividualKey } from '../model/beneficial.owner.individual.model';
@@ -159,8 +158,12 @@ const post = async (
       details.trust_id = mapperDetails.generateTrustId(appData);
     }
 
-    //  update trust details in application data at session
-    appData = saveTrustInApp(appData, details);
+    //  if present, get existing trust from session (as it might have attached trustees)
+    const trust = getTrustByIdFromApp(appData, details.trust_id);
+    Object.keys(details).forEach(key => trust[key] = details[key]);
+
+    //  update trust  in application data at session
+    appData = saveTrustInApp(appData, trust);
 
     //  update trusts in beneficial owners
     const selectedBoIds = req.body?.beneficialOwnersIds ?? [];

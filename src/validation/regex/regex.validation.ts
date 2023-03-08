80ef8a72262ff1c088b85087395248fe4d6bc082
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2023-03-08 12:27:01 +0000

    roe-1667 making check case insensitive

--- src/validation/regex/regex.validation.ts
+++ src/validation/regex/regex.validation.ts
@@ -10,4 +10,4 @@ export const VALID_EMAIL_FORMAT = /^.+@.+\..+$/;
 export const VALID_OE_NUMBER_FORMAT = /^OE\d{6}$/;
 
 // Valid UK post code
-export const VALID_POSTCODE = /^[A-Za-z]{1,2}[0-9R][0-9A-Z]?[\s]{0,1}[0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}$/;
+export const VALID_POSTCODE = /^[A-Z]{1,2}[0-9R][0-9A-Z]?[\s]{0,1}[0-9][ABD-HJLNP-UW-Z]{2}$/i;

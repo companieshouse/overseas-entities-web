d44521f9bf93b3ac5386d2877be50b08dbca8b6f
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2023-03-14 09:57:38 +0000

    roe-1574 rebase main

--- test/validation/date.validations.spec.ts
+++ test/validation/date.validations.spec.ts
@@ -1,6 +1,10 @@
 import { RoleWithinTrustType } from '../../src/model/role.within.trust.type.model';
-import { checkBirthDate, checkDateInterestedPerson, checkHistoricalBOEndDate, checkHistoricalBOStartDate, checkMoreThanOneDateOfBirthFieldIsNotMissing, checkStartDate,
-  checkTrustDate } from '../../src/validation/custom.validation';
+import { checkBirthDate,
+  checkDateIP,
+  checkDayFieldForErrors,
+  checkHistoricalBOEndDate, checkHistoricalBOStartDate, checkMonthFieldForErrors, checkMoreThanOneDateOfBirthFieldIsNotMissing, checkStartDate,
+  checkTrustDate,
+  checkYearFieldForErrors } from '../../src/validation/custom.validation';
 import { ErrorMessages } from '../../src/validation/error.messages';
 import { dateValidations, dateContext, conditionalDateValidations, dateContextWithCondition } from '../../src/validation/fields/helper/date.validation.helper';
 
@@ -31,15 +35,33 @@ describe('Test to validate date validator', () => {
         name: fieldNames[3],
         callBack: checkStartDate,
       },
-      dayInputName: fieldNames[0],
-      monthInputName: fieldNames[1],
-      yearInputName: fieldNames[2],
+      dayInput: {
+        name: fieldNames[0],
+        errors: {
+          noDayError: ErrorMessages.DAY_OF_BIRTH,
+          wrongDayLength: ErrorMessages.DATE_OF_BIRTH_DAY_LENGTH,
+        }
+      },
+      monthInput: {
+        name: fieldNames[1],
+        errors: {
+          noMonthError: ErrorMessages.MONTH_OF_BIRTH,
+          wrongMonthLength: ErrorMessages.DATE_OF_BIRTH_MONTH_LENGTH,
+        }
+      },
+      yearInput: {
+        name: fieldNames[2],
+        errors: {
+          noYearError: ErrorMessages.YEAR_OF_BIRTH,
+          wrongYearLength: ErrorMessages.DATE_OF_BIRTH_YEAR_LENGTH
+        }
+      },
     };
 
     const sut = dateValidations(mockDateValidationsContext);
-    expect(sut.length).toEqual(1);
+    expect(sut.length).toEqual(4);
     expect(mockIf).toBeCalledTimes(0);
-    expect(mockCustom).toBeCalledTimes(1);
+    expect(mockCustom).toBeCalledTimes(4);
     expect(mockEquals).toBeCalledTimes(0);
   });
 
@@ -50,9 +72,27 @@ describe('Test to validate date validator', () => {
         name: fieldNames[3],
         callBack: checkStartDate,
       },
-      dayInputName: fieldNames[0],
-      monthInputName: fieldNames[1],
-      yearInputName: fieldNames[2],
+      dayInput: {
+        name: fieldNames[0],
+        errors: {
+          noDayError: ErrorMessages.DAY_OF_BIRTH,
+          wrongDayLength: ErrorMessages.DATE_OF_BIRTH_DAY_LENGTH,
+        }
+      },
+      monthInput: {
+        name: fieldNames[1],
+        errors: {
+          noMonthError: ErrorMessages.MONTH_OF_BIRTH,
+          wrongMonthLength: ErrorMessages.DATE_OF_BIRTH_MONTH_LENGTH,
+        }
+      },
+      yearInput: {
+        name: fieldNames[2],
+        errors: {
+          noYearError: ErrorMessages.YEAR_OF_BIRTH,
+          wrongYearLength: ErrorMessages.DATE_OF_BIRTH_YEAR_LENGTH
+        }
+      },
       condition: {
         elementName: "type",
         expectedValue: RoleWithinTrustType.INTERESTED_PERSON
@@ -60,28 +100,24 @@ describe('Test to validate date validator', () => {
     };
 
     const sut = conditionalDateValidations(mockDateValidationsContext);
-    expect(sut.length).toEqual(1);
-    expect(mockIf).toBeCalledTimes(1);
-    expect(mockCustom).toBeCalledTimes(1);
-    expect(mockEquals).toBeCalledTimes(1);
+    expect(sut.length).toEqual(4);
+    expect(mockIf).toBeCalledTimes(4);
+    expect(mockCustom).toBeCalledTimes(4);
+    expect(mockEquals).toBeCalledTimes(4);
   });
 });
 
 describe("test date method", () => {
 
-  const errMsgCheckDateInterestedPerson: ErrorMessages[] = [
+  const errMsgcheckDate: ErrorMessages[] = [
     ErrorMessages.ENTER_DATE_INTERESTED_PERSON,
     ErrorMessages.MONTH_AND_YEAR,
     ErrorMessages.DAY_AND_YEAR,
     ErrorMessages.DAY_AND_MONTH,
     ErrorMessages.INVALID_DATE,
     ErrorMessages.INVALID_DATE,
-    ErrorMessages.YEAR_LENGTH,
     ErrorMessages.INVALID_DATE,
     ErrorMessages.DATE_NOT_IN_THE_PAST_INTERESTED_PERSON,
-    ErrorMessages.YEAR,
-    ErrorMessages.DAY,
-    ErrorMessages.MONTH
   ];
 
   const errMsgcheckBirthDate: ErrorMessages[] = [
@@ -91,12 +127,8 @@ describe("test date method", () => {
     ErrorMessages.DAY_AND_MONTH_OF_BIRTH,
     ErrorMessages.INVALID_DATE_OF_BIRTH,
     ErrorMessages.INVALID_DATE_OF_BIRTH,
-    ErrorMessages.DATE_OF_BIRTH_YEAR_LENGTH,
     ErrorMessages.INVALID_DATE_OF_BIRTH,
     ErrorMessages.DATE_OF_BIRTH_NOT_IN_PAST,
-    ErrorMessages.YEAR_OF_BIRTH,
-    ErrorMessages.DAY_OF_BIRTH,
-    ErrorMessages.MONTH_OF_BIRTH
   ];
 
   const errMsgcheckTrustDate: ErrorMessages[] = [
@@ -106,12 +138,8 @@ describe("test date method", () => {
     ErrorMessages.DAY_AND_MONTH_OF_TRUST,
     ErrorMessages.INVALID_DATE_OF_TRUST,
     ErrorMessages.INVALID_DATE_OF_TRUST,
-    ErrorMessages.YEAR_LENGTH_OF_TRUST,
     ErrorMessages.INVALID_DATE_OF_TRUST,
     ErrorMessages.DATE_NOT_IN_PAST_OR_TODAY_OF_TRUST,
-    ErrorMessages.YEAR_OF_TRUST,
-    ErrorMessages.DAY_OF_TRUST,
-    ErrorMessages.MONTH_OF_TRUST
   ];
 
   const errMsgHistoricalBOStartDate: ErrorMessages[] = [
@@ -121,12 +149,8 @@ describe("test date method", () => {
     ErrorMessages.START_DAY_AND_MONTH_HISTORICAL_BO,
     ErrorMessages.INVALID_START_DATE_HISTORICAL_BO,
     ErrorMessages.INVALID_START_DATE_HISTORICAL_BO,
-    ErrorMessages.START_YEAR_LENGTH_HISTORICAL_BO,
     ErrorMessages.INVALID_START_DATE_HISTORICAL_BO,
     ErrorMessages.START_DATE_NOT_IN_PAST_OR_TODAY_HISTORICAL_BO,
-    ErrorMessages.START_YEAR_HISTORICAL_BO,
-    ErrorMessages.START_DAY_HISTORICAL_BO,
-    ErrorMessages.START_MONTH_HISTORICAL_BO
   ];
 
   const errMsgHistoricalBOEndDate: ErrorMessages[] = [
@@ -136,12 +160,8 @@ describe("test date method", () => {
     ErrorMessages.END_DAY_AND_MONTH_HISTORICAL_BO,
     ErrorMessages.INVALID_END_DATE_HISTORICAL_BO,
     ErrorMessages.INVALID_END_DATE_HISTORICAL_BO,
-    ErrorMessages.END_YEAR_LENGTH_HISTORICAL_BO,
     ErrorMessages.INVALID_END_DATE_HISTORICAL_BO,
     ErrorMessages.END_DATE_NOT_IN_PAST_OR_TODAY_HISTORICAL_BO,
-    ErrorMessages.END_YEAR_HISTORICAL_BO,
-    ErrorMessages.END_DAY_HISTORICAL_BO,
-    ErrorMessages.END_MONTH_HISTORICAL_BO
   ];
 
   const testDateCheck = (err: ErrorMessages[]) => [
@@ -151,16 +171,12 @@ describe("test date method", () => {
     ["", "", "2009", err[3]],
     ["0", "10", "2009", err[4]],
     ["10", "0", "2009", err[5]],
-    ["10", "10", "209", err[6]],
-    ["10", "a", "2009", err[7]],
-    ["10", "10", "9999", err[8]],
-    ["10", "10", "", err[9]],
-    ["", "10", "9999", err[10]],
-    ["10", "", "9999", err[11]],
+    ["10", "a", "2009", err[6]],
+    ["10", "10", "9999", err[7]],
   ];
 
-  test.each(testDateCheck(errMsgCheckDateInterestedPerson))("should throw appropriate date errors for checkDate", (_day, _month, _year, _err) => {
-    expect(() => checkDateInterestedPerson(_day, _month, _year)).toThrow(_err);
+  test.each(testDateCheck(errMsgcheckDate))("should throw appropriate date errors for checkDateIP", (_day, _month, _year, _err) => {
+    expect(() => checkDateIP(_day, _month, _year)).toThrow(_err);
   });
 
   test.each(testDateCheck(errMsgcheckBirthDate))("should throw appropriate date errors for checkBirthDate", (_day, _month, _year, _err) => {
@@ -169,10 +185,6 @@ describe("test date method", () => {
 
   test.each(testDateCheck(errMsgcheckTrustDate))("should throw appropriate date errors for checkTrustDate", (_day, _month, _year, _err) => {
     expect(() => checkTrustDate(_day, _month, _year)).toThrow(_err);
-  });
-
-  test.each(testDateCheck(errMsgHistoricalBOStartDate))("should throw appropriate date errors for checkHistoricalBOStartDate", (_day, _month, _year, _err) => {
-    expect(() => checkHistoricalBOStartDate(_day, _month, _year)).toThrow(_err);
   });
 
   test.each(testDateCheck(errMsgHistoricalBOStartDate))("should throw appropriate date errors for checkHistoricalBOStartDate", (_day, _month, _year, _err) => {
@@ -189,5 +201,67 @@ describe("test date method", () => {
     [["", "02", ""], ErrorMessages.DAY_AND_YEAR_OF_BIRTH]
   ])("test more than one date of birth field not missing", (date, err) => {
     expect (() => checkMoreThanOneDateOfBirthFieldIsNotMissing(...date)).toThrow(err);
+  });
+});
+
+describe("test day,month and year error checkers", () => {
+  const errors = {
+    noDayError: ErrorMessages.DAY,
+    noMonthError: ErrorMessages.MONTH,
+    noYearError: ErrorMessages.YEAR,
+    wrongDayLength: ErrorMessages.DAY_LENGTH,
+    wrongMonthLength: ErrorMessages.MONTH_LENGTH,
+    wrongYearLength: ErrorMessages.YEAR_LENGTH,
+  };
+  test("test day absent", () => {
+    expect(() => checkDayFieldForErrors({
+      noDayError: errors.noDayError,
+      wrongDayLength: errors.wrongDayLength }, "")).toThrowError(errors.noDayError);
+
+    expect(() => checkDayFieldForErrors({
+      noDayError: errors.noDayError,
+      wrongDayLength: errors.wrongDayLength }, "123")).toThrowError(errors.wrongDayLength);
+
+    expect(() => checkDayFieldForErrors({
+      noDayError: errors.noDayError,
+      wrongDayLength: errors.wrongDayLength }, "13")).toBeTruthy();
+
+    expect(() => checkDayFieldForErrors({
+      noDayError: errors.noDayError,
+      wrongDayLength: errors.wrongDayLength }, "3")).toBeTruthy();
+  });
+  test("test month absent", () => {
+    expect(() => checkMonthFieldForErrors({
+      noMonthError: errors.noMonthError,
+      wrongMonthLength: errors.wrongMonthLength }, "")).toThrowError(errors.noMonthError);
+
+    expect(() => checkMonthFieldForErrors({
+      noMonthError: errors.noMonthError,
+      wrongMonthLength: errors.wrongMonthLength }, "321")).toThrowError(errors.wrongMonthLength);
+
+    expect(() => checkMonthFieldForErrors({
+      noMonthError: errors.noMonthError,
+      wrongMonthLength: errors.wrongMonthLength }, "1")).toBeTruthy();
+
+    expect(() => checkMonthFieldForErrors({
+      noMonthError: errors.noMonthError,
+      wrongMonthLength: errors.wrongMonthLength }, "12")).toBeTruthy();
+  });
+  test("test year absent", () => {
+    expect(() => checkYearFieldForErrors({
+      noYearError: errors.noYearError,
+      wrongYearLength: errors.wrongYearLength }, "")).toThrowError(errors.noYearError);
+
+    expect(() => checkYearFieldForErrors({
+      noYearError: errors.noYearError,
+      wrongYearLength: errors.wrongYearLength }, "20033")).toThrowError(errors.wrongYearLength);
+
+    expect(() => checkYearFieldForErrors({
+      noYearError: errors.noYearError,
+      wrongYearLength: errors.wrongYearLength }, "200")).toThrowError(errors.wrongYearLength);
+
+    expect(() => checkYearFieldForErrors({
+      noYearError: errors.noYearError,
+      wrongYearLength: errors.wrongYearLength }, "2033")).toBeTruthy();
   });
 });

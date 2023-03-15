d133610bd94a836fa8695c02d460bebbb0a22658
AndreKramer7654 <117173564+AndreKramer7654@users.noreply.github.com>
2023-03-15 11:59:03 +0000

    uar-127 fix bomo fetch (#759)
    * uar-127 fix bomo fetch
    
    * uar-127 fix camel/snake case

--- src/utils/update/beneficial_owners_managing_officers_data_fetch.ts
+++ src/utils/update/beneficial_owners_managing_officers_data_fetch.ts
@@ -1,13 +1,10 @@
 import { ApplicationData } from "../../model";
-import { ManagingOfficerKey } from "../../model/managing.officer.model";
-import { ManagingOfficerCorporateKey } from "../../model/managing.officer.corporate.model";
-import { BeneficialOwnerOtherKey } from "../../model/beneficial.owner.other.model";
-import { BeneficialOwnerGovKey } from "../../model/beneficial.owner.gov.model";
-import { BeneficialOwnerIndividualKey } from "../../model/beneficial.owner.individual.model";
 
-export const hasFetchedBoAndMoData = (appData: ApplicationData) =>
-  appData[BeneficialOwnerOtherKey] !== undefined ||
-    appData[BeneficialOwnerGovKey] !== undefined ||
-    appData[BeneficialOwnerIndividualKey] !== undefined ||
-    appData[ManagingOfficerCorporateKey] !== undefined ||
-    appData[ManagingOfficerKey] !== undefined;
+export const hasFetchedBoAndMoData = (appData: ApplicationData) => appData?.update?.bo_mo_data ?? false;
+
+export const setFetchedBoMoData = (appData: ApplicationData) => {
+  if (!appData.update) {
+    appData.update = {};
+  }
+  appData.update.bo_mo_data = true;
+};

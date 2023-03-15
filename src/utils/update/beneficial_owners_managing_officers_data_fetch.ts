bf7c4e82e9923775ea75d40e33df913ebe807c20
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-03-15 11:36:04 +0000

    uar-127 fix camel/snake case

--- src/utils/update/beneficial_owners_managing_officers_data_fetch.ts
+++ src/utils/update/beneficial_owners_managing_officers_data_fetch.ts
@@ -1,10 +1,10 @@
 import { ApplicationData } from "../../model";
 
-export const hasFetchedBoAndMoData = (appData: ApplicationData) => appData?.update?.fetchedBoMoData ?? false;
+export const hasFetchedBoAndMoData = (appData: ApplicationData) => appData?.update?.bo_mo_data ?? false;
 
 export const setFetchedBoMoData = (appData: ApplicationData) => {
   if (!appData.update) {
     appData.update = {};
   }
-  appData.update.fetchedBoMoData = true;
+  appData.update.bo_mo_data = true;
 };

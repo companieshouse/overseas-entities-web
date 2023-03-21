8ad48fb35cbba552f093d2cb8c11026589f1cb4d
JoshFen95 <jfennell@companieshouse.gov.uk>
2023-03-21 10:33:13 +0000

    roe-2060: start to add mapToPage
    - start implementing map to page method
    - add method call in controller

--- src/utils/trust/historical.beneficial.owner.mapper.ts
+++ src/utils/trust/historical.beneficial.owner.mapper.ts
@@ -3,6 +3,8 @@ import { TrusteeType } from '../../model/trustee.type.model';
 import * as Trust from '../../model/trust.model';
 import * as Page from '../../model/trust.page.model';
 import { yesNoResponse } from '../../model/data.types.model';
+import { getFormerTrustee } from '../../utils/trusts';
+import { ApplicationData } from 'model';
 
 const mapBeneficialOwnerToSession = (
   formData: Page.TrustHistoricalBeneficialOwnerForm,
@@ -33,12 +35,63 @@ const mapBeneficialOwnerToSession = (
   };
 };
 
+const mapFormerTrusteeFromSessionToPage = (
+  appData: ApplicationData,
+  trustId: string,
+  trusteeId: string,
+): Page.TrustHistoricalBeneficialOwnerForm => {
+  const trustee = getFormerTrustee(appData, trustId, trusteeId);
+  // const ff = trustee as Trust.TrustHistoricalBeneficialOwnerLegal;
+  // console.log(trustee.surname);
+  // if(trustee instanceof Trust.TrustHistoricalBeneficialOwner)
+
+  const data = {
+    boId: trustee.id,
+    startDateDay: trustee.notified_date_day,
+    startDateMonth: trustee.notified_date_month,
+    startDateYear: trustee.notified_date_year,
+    endDateDay: trustee.ceased_date_day,
+    endDateMonth: trustee.ceased_date_month,
+    endDateYear: trustee.ceased_date_year,
+  };
+  console.log(trustee.corporate_indicator);
+  if (trustee.corporate_indicator === yesNoResponse.Yes && 'corporate_name' in trustee) {
+    console.log("here");
+    console.log(trustee.corporate_name);
+
+    return {
+      ...data,
+      type: TrusteeType.LEGAL_ENTITY,
+      corporate_name: trustee.corporate_name
+    };
+  }
+
+  if ('forename' in trustee && 'surname' in trustee) {
+    return {
+      ...data,
+      type: TrusteeType.INDIVIDUAL,
+      firstName: trustee.forename,
+      lastName: trustee.surname,
+    };
+  }
+
+  return data as Page.TrustHistoricalBeneficialOwnerForm;
+};
+
+// function isFish(TrustHistoricalBeneficialOwner: Trust.TrustHistoricalBeneficialOwnerLegal | Bird): pet is Fish {
+//   return (pet as Fish).swim !== undefined;
+
 //  other
 const generateBoId = (): string => {
   return uuidv4();
 };
 
+// const isNecklace = (b: Trust.TrustHistoricalBeneficialOwner): b is Trust.TrustHistoricalBeneficialOwnerIndividual => {
+//   return (b as Trust.TrustHistoricalBeneficialOwnerIndividual).forename !== undefined;
+// }
+
 export {
   mapBeneficialOwnerToSession,
+  mapFormerTrusteeFromSessionToPage,
   generateBoId,
 };

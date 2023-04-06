8a397fc97bf57e25a11b764be0a78d2dbc3ccf59
SteveHicksCH <shicks@companieshouse.gov.uk>
2023-04-06 18:06:00 +0100

    pa-roe-2127: Add test for missing mandatory fields

--- test/controllers/trust.individual.beneficial.owner.controller.int.spec.ts
+++ test/controllers/trust.individual.beneficial.owner.controller.int.spec.ts
@@ -43,42 +43,87 @@ describe('Trust Individual Beneficial Owner Controller Integration Tests', () =>
     (hasTrustWithId as jest.Mock).mockImplementation((_, __, next: NextFunction) => next());
   });
 
-  test('renders the ${TRUST_INDIVIDUAL_BENEFICIAL_OWNER_PAGE} page with MAX error messages', async () => {
+  describe("POST tests", () => {
 
-    const mockTrust = {} as Trust;
-    (getTrustByIdFromApp as jest.Mock).mockReturnValue(mockTrust);
+    test('renders the ${TRUST_INDIVIDUAL_BENEFICIAL_OWNER_PAGE} page with missing mandatory field messages', async () => {
 
-    const individualTrusteeAboveMaximumFieldLengths: IndividualTrusteesFormCommon = {
-      roleWithinTrust: RoleWithinTrustType.BENEFICIARY,
-      forename: maxLengthMocks.MAX_50 + "1",
-      surname: maxLengthMocks.MAX_50 + "1",
-      dateOfBirthDay: "19",
-      dateOfBirthMonth: "03",
-      dateOfBirthYear: "1976",
-      nationality: maxLengthMocks.NO_MAX,
-      ...RESIDENTIAL_ADDRESS_WITH_MAX_LENGTH_FIELDS_MOCK,
-      ...SERVICE_ADDRESS_WITH_MAX_LENGTH_FIELDS_MOCK,
-      is_service_address_same_as_usual_residential_address: yesNoResponse.No,
-      dateBecameIPDay: "11",
-      dateBecameIPMonth: "11",
-      dateBecameIPYear: "1987",
-    };
+      const mockTrust = {} as Trust;
+      (getTrustByIdFromApp as jest.Mock).mockReturnValue(mockTrust);
 
-    const resp =
+      const individualTrusteeMissingMandatoryFields = {
+        roleWithinTrust: RoleWithinTrustType.INTERESTED_PERSON,
+        forename: "",
+        surname: "",
+        dateOfBirthDay: "",
+        dateOfBirthMonth: "",
+        dateOfBirthYear: "",
+        nationality: "",
+        usual_residential_address_property_name_number: "",
+        usual_residential_address_line_1: "",
+        usual_residential_address_town: "",
+        usual_residential_address_country: "",
+        is_service_address_same_as_usual_residential_address: "",
+        dateBecameIPDay: "",
+        dateBecameIPMonth: "",
+        dateBecameIPYear: "",
+      };
+
+      const resp =
+        await request(app)
+          .post(pageUrl)
+          .send(individualTrusteeMissingMandatoryFields);
+
+      expect(resp.status).toEqual(constants.HTTP_STATUS_OK);
+      expect(mockSaveAndContinue).not.toHaveBeenCalled();
+      expect(resp.text).toContain(ErrorMessages.FIRST_NAME_INDIVIDUAL_BO);
+      expect(resp.text).toContain(ErrorMessages.LAST_NAME_INDIVIDUAL_BO);
+      expect(resp.text).toContain(ErrorMessages.ENTER_DATE_OF_BIRTH_INDIVIDUAL_BO);
+      expect(resp.text).toContain(ErrorMessages.ENTER_DATE_INTERESTED_PERSON_INDIVIDUAL_BO);
+      expect(resp.text).toContain(ErrorMessages.NATIONALITY_INDIVIDUAL_BO);
+      expect(resp.text).toContain(ErrorMessages.SELECT_IF_SERVICE_ADDRESS_SAME_AS_USER_RESIDENTIAL_ADDRESS_INDIVIDUAL_BO);
+      expect(resp.text).toContain(ErrorMessages.PROPERTY_NAME_OR_NUMBER_INDIVIDUAL_BO);
+      expect(resp.text).toContain(ErrorMessages.CITY_OR_TOWN_INDIVIDUAL_BO);
+      expect(resp.text).toContain(ErrorMessages.COUNTRY_INDIVIDUAL_BO);
+    });
+
+    test('renders the ${TRUST_INDIVIDUAL_BENEFICIAL_OWNER_PAGE} page with MAX error messages', async () => {
+
+      const mockTrust = {} as Trust;
+      (getTrustByIdFromApp as jest.Mock).mockReturnValue(mockTrust);
+
+      const individualTrusteeAboveMaximumFieldLengths: IndividualTrusteesFormCommon = {
+        roleWithinTrust: RoleWithinTrustType.BENEFICIARY,
+        forename: maxLengthMocks.MAX_50 + "1",
+        surname: maxLengthMocks.MAX_50 + "1",
+        dateOfBirthDay: "19",
+        dateOfBirthMonth: "03",
+        dateOfBirthYear: "1976",
+        nationality: maxLengthMocks.NO_MAX,
+        ...RESIDENTIAL_ADDRESS_WITH_MAX_LENGTH_FIELDS_MOCK,
+        ...SERVICE_ADDRESS_WITH_MAX_LENGTH_FIELDS_MOCK,
+        is_service_address_same_as_usual_residential_address: yesNoResponse.No,
+        dateBecameIPDay: "11",
+        dateBecameIPMonth: "11",
+        dateBecameIPYear: "1987",
+      };
+
+      const resp =
         await request(app)
           .post(pageUrl)
           .send(individualTrusteeAboveMaximumFieldLengths);
 
-    expect(resp.status).toEqual(constants.HTTP_STATUS_OK);
-    expect(mockSaveAndContinue).not.toHaveBeenCalled();
-    expect(resp.text).toContain(ErrorMessages.MAX_FIRST_NAME_LENGTH);
-    expect(resp.text).toContain(ErrorMessages.MAX_LAST_NAME_LENGTH_50);
-    expect(resp.text).toContain(ErrorMessages.MAX_PROPERTY_NAME_OR_NUMBER_LENGTH);
-    expect(resp.text).toContain(ErrorMessages.MAX_ADDRESS_LINE1_LENGTH);
-    expect(resp.text).toContain(ErrorMessages.MAX_ADDRESS_LINE2_LENGTH);
-    expect(resp.text).toContain(ErrorMessages.MAX_CITY_OR_TOWN_LENGTH);
-    expect(resp.text).toContain(ErrorMessages.MAX_COUNTY_LENGTH);
-    expect(resp.text).toContain(ErrorMessages.MAX_POSTCODE_LENGTH);
+      expect(resp.status).toEqual(constants.HTTP_STATUS_OK);
+      expect(mockSaveAndContinue).not.toHaveBeenCalled();
+      expect(resp.text).toContain(ErrorMessages.MAX_FIRST_NAME_LENGTH);
+      expect(resp.text).toContain(ErrorMessages.MAX_LAST_NAME_LENGTH_50);
+      expect(resp.text).toContain(ErrorMessages.MAX_PROPERTY_NAME_OR_NUMBER_LENGTH);
+      expect(resp.text).toContain(ErrorMessages.MAX_ADDRESS_LINE1_LENGTH);
+      expect(resp.text).toContain(ErrorMessages.MAX_ADDRESS_LINE2_LENGTH);
+      expect(resp.text).toContain(ErrorMessages.MAX_CITY_OR_TOWN_LENGTH);
+      expect(resp.text).toContain(ErrorMessages.MAX_COUNTY_LENGTH);
+      expect(resp.text).toContain(ErrorMessages.MAX_POSTCODE_LENGTH);
+    });
+
   });
 
 });

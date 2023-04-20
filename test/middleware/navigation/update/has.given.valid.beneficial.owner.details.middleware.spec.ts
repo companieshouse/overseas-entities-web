c39d7d4c4d6c0f2548ec1c8feeeba57c517e4765
corybudd-ch <117734784+corybudd-ch@users.noreply.github.com>
2023-04-20 18:01:03 +0100

    fix nav middleware tests by passing in params

--- test/middleware/navigation/update/has.given.valid.beneficial.owner.details.middleware.spec.ts
+++ test/middleware/navigation/update/has.given.valid.beneficial.owner.details.middleware.spec.ts
@@ -4,18 +4,29 @@ jest.mock('../../../../src/middleware/navigation/check.condition');
 
 import { describe, expect, test, beforeEach, jest } from '@jest/globals';
 import { Request, Response } from 'express';
+import { Params } from "express-serve-static-core";
 
 import { logger } from "../../../../src/utils/logger";
-// import { SECURE_UPDATE_FILTER_PAGE, SECURE_UPDATE_FILTER_URL } from '../../../../src/config';
+import {
+  PARAM_BENEFICIAL_OWNER_INDIVIDUAL,
+  SECURE_UPDATE_FILTER_PAGE,
+  SECURE_UPDATE_FILTER_URL
+} from '../../../../src/config';
 import { ANY_MESSAGE_ERROR } from '../../../__mocks__/text.mock';
 
-import { checkHasGivenValidBOData } from '../../../../src/middleware/navigation/check.condition';
+import { NavigationErrorMessage, checkHasGivenValidBOData } from '../../../../src/middleware/navigation/check.condition';
 import { hasGivenValidBODetails } from '../../../../src/middleware/navigation/update/has.given.valid.beneficial.owner.details.middleware';
+import { BO_IND_ID } from '../../../__mocks__/session.mock';
 
 const mockCheckHasGivenValidBOData = checkHasGivenValidBOData as jest.Mock;
 const mockLoggerInfoRequest = logger.infoRequest as jest.Mock;
 
-const req = {} as Request;
+const req = {
+  params: {
+    id: BO_IND_ID,
+    beneficialOwnerType: PARAM_BENEFICIAL_OWNER_INDIVIDUAL,
+  } as Params,
+} as Request;
 const res = { redirect: jest.fn() as any } as Response;
 const next = jest.fn();
 
@@ -25,18 +36,18 @@ describe("has.given.valid.beneficial.owner.details navigation middleware tests",
     jest.clearAllMocks();
   });
 
-  // test(`should redirect to ${SECURE_UPDATE_FILTER_PAGE} page and log message error ${NavigationErrorMessage} if check fails`, () => {
-  //   mockCheckHasGivenValidBOData.mockImplementationOnce( () => { return false; });
-  //   hasGivenValidBODetails(req, res, next);
+  test(`should redirect to ${SECURE_UPDATE_FILTER_PAGE} page and log message error ${NavigationErrorMessage} if check fails`, () => {
+    mockCheckHasGivenValidBOData.mockImplementationOnce( () => { return false; });
+    hasGivenValidBODetails(req, res, next);
 
-  //   expect(next).not.toHaveBeenCalled;
+    expect(next).not.toBeCalled();
 
-  //   expect(mockLoggerInfoRequest).toHaveBeenCalledTimes(1);
-  //   expect(mockLoggerInfoRequest).toHaveBeenCalledWith(req, NavigationErrorMessage);
+    expect(mockLoggerInfoRequest).toHaveBeenCalledTimes(1);
+    expect(mockLoggerInfoRequest).toHaveBeenCalledWith(req, NavigationErrorMessage);
 
-  //   expect(res.redirect).toHaveBeenCalledTimes(1);
-  //   expect(res.redirect).toHaveBeenCalledWith(SECURE_UPDATE_FILTER_URL);
-  // });
+    expect(res.redirect).toHaveBeenCalledTimes(1);
+    expect(res.redirect).toHaveBeenCalledWith(SECURE_UPDATE_FILTER_URL);
+  });
 
   test(`should not redirect and pass to the next middleware`, () => {
     mockCheckHasGivenValidBOData.mockImplementationOnce( () => { return true; });

c952b82b25261faa32b2046efcf494a37d16bab1
olaych <oolatunji@companieshouse.gov.uk>
2023-04-28 18:29:27 +0100

    added padding function

--- src/controllers/update/update.review.beneficial.owner.individual.ts
+++ src/controllers/update/update.review.beneficial.owner.individual.ts
@@ -8,6 +8,7 @@ import { setBeneficialOwnerData } from "../../utils/beneficial.owner.individual"
 import { v4 as uuidv4 } from "uuid";
 import { saveAndContinue } from "../../utils/save.and.continue";
 import { Session } from "@companieshouse/node-session-handler";
+import { InputDate } from "model/data.types.model";
 
 export const get = (req: Request, res: Response) => {
   logger.debugRequest(req, `${req.method} ${req.route.path}`);
@@ -43,13 +44,11 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
 
     if (boiIndex !== undefined && appData.beneficial_owners_individual && appData.beneficial_owners_individual[Number(boiIndex)].id === req.body["id"]){
       const boId = appData.beneficial_owners_individual[Number(boiIndex)].id;
-      const dob = appData.beneficial_owners_individual[Number(boiIndex)].date_of_birth;
+      const dob = appData.beneficial_owners_individual[Number(boiIndex)].date_of_birth as InputDate;
 
       removeFromApplicationData(req, BeneficialOwnerIndividualKey, boId);
 
-      req.body["date_of_birth-day"] = String(dob?.day).length > 1 ? dob?.day : String(dob?.day).padStart(2, '0');
-      req.body["date_of_birth-month"] = String(dob?.month).length > 1 ? dob?.month : String(dob?.month).padStart(2, '0');
-      req.body["date_of_birth-year"] = dob?.year;
+      setReviewedDateOfBirth(req, dob);
 
       const session = req.session as Session;
 
@@ -65,3 +64,15 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+export const setReviewedDateOfBirth = (req: Request, dob: InputDate) => {
+  req.body["date_of_birth-day"] = padWithZero(dob?.day, 2, "0");
+  req.body["date_of_birth-month"] = padWithZero(dob?.month, 2, "0");
+  req.body["date_of_birth-year"] = padWithZero(dob?.year, 2, "0");
+};
+
+const padWithZero = (input: string, maxLength: number, fillString: string): string => {
+  if (input.length > 1){
+    return input;
+  }
+  return String(input).padStart(maxLength, fillString);
+};

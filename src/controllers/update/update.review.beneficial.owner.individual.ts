1b354af4f6e0cafe48d20d62e1b5df42b1043e9c
cdrainCH <cdrain@companieshouse.gov.uk>
2023-04-28 15:47:36 +0100

    populating data after change link

--- src/controllers/update/update.review.beneficial.owner.individual.ts
+++ src/controllers/update/update.review.beneficial.owner.individual.ts
@@ -1,6 +1,6 @@
 import { REVIEW_BENEFICIAL_OWNER_INDEX_PARAM, UPDATE_BENEFICIAL_OWNER_BO_MO_REVIEW_URL, UPDATE_BENEFICIAL_OWNER_TYPE_URL, UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE, UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_URL } from "../../config";
 import { NextFunction, Request, Response } from "express";
-import { getApplicationData, removeFromApplicationData, setApplicationData } from "../../utils/application.data";
+import { getApplicationData, removeFromApplicationData, setApplicationData, getFromApplicationData, mapDataObjectToFields } from "../../utils/application.data";
 import { logger } from "../../utils/logger";
 import { BeneficialOwnerIndividualKey } from "../../model/beneficial.owner.individual.model";
 import { ApplicationData, ApplicationDataType } from "../../model";
@@ -8,6 +8,8 @@ import { setBeneficialOwnerData } from "../../utils/beneficial.owner.individual"
 import { v4 as uuidv4 } from "uuid";
 import { saveAndContinue } from "../../utils/save.and.continue";
 import { Session } from "@companieshouse/node-session-handler";
+import { CeasedDateKey, CeasedDateKeys } from "model/date.model";
+import { EntityNumberKey, InputDateKeys } from "model/data.types.model";
 
 export const get = (req: Request, res: Response) => {
   logger.debugRequest(req, `${req.method} ${req.route.path}`);
@@ -15,16 +17,41 @@ export const get = (req: Request, res: Response) => {
   const index = req.query.index;
 
   let dataToReview = {};
+  let id = "";
   if (appData?.beneficial_owners_individual){
     dataToReview = appData?.beneficial_owners_individual[Number(index)];
+    id = appData?.beneficial_owners_individual[Number(index)].id;
   }
+
+  const data = getFromApplicationData(req, BeneficialOwnerIndividualKey, id, true);
+
   const backLinkUrl = getBackLinkUrl(appData, index);
-  return res.render(UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE, {
+
+  const templateOptions = {
     backLinkUrl,
     templateName: UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE,
     ...dataToReview,
+    ...data,
     isOwnersReview: true
-  });
+  };
+
+  // extra data needed for update journey
+  if (EntityNumberKey in appData && appData[EntityNumberKey] !== null) {
+    templateOptions["is_still_bo"] = (Object.keys(data["ceased_date"]).length === 0) ? 1 : 0;
+    templateOptions[EntityNumberKey] = appData[EntityNumberKey];
+    templateOptions["ceased_date"] = (data) ? mapDataObjectToFields(data[CeasedDateKey], CeasedDateKeys, InputDateKeys) : {};
+  }
+
+  return res.render(UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE, templateOptions);
+
+  // console.log(JSON.stringify(appData.beneficial_owners_individual));
+
+  // return res.render(UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE, {
+  //   backLinkUrl,
+  //   templateName: UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE,
+  //   ...dataToReview,
+  //   isOwnersReview: true
+  // });
 };
 
 const getBackLinkUrl = (appData: ApplicationData, pageIndex) => {
@@ -43,6 +70,7 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
 
     if (boiIndex !== undefined && appData.beneficial_owners_individual && appData.beneficial_owners_individual[Number(boiIndex)].id === req.body["id"]){
       const boId = appData.beneficial_owners_individual[Number(boiIndex)].id;
+      const dateOfBirth = appData.beneficial_owners_individual[Number(boiIndex)].date_of_birth;
       removeFromApplicationData(req, BeneficialOwnerIndividualKey, boId);
 
       const session = req.session as Session;
@@ -50,6 +78,10 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
       const data: ApplicationDataType = setBeneficialOwnerData(req.body, uuidv4());
 
       setApplicationData(req.session, data, BeneficialOwnerIndividualKey);
+
+      appData.beneficial_owners_individual[Number(boiIndex)].date_of_birth = dateOfBirth;
+      // appData.beneficial_owners_individual[Number(boiIndex)].ceased_date = {day: "27", month: "4", year: "2023"}
+
       await saveAndContinue(req, session, false);
 
       res.redirect(UPDATE_BENEFICIAL_OWNER_TYPE_URL);
@@ -58,4 +90,3 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
     next(error);
   }
 };
-
--- src/utils/beneficial.owner.individual.ts
+++ src/utils/beneficial.owner.individual.ts
@@ -34,6 +34,8 @@ import {
   UsualResidentialAddressKeys,
 } from "../model/address.model";
 import {
+  CeasedDateKey,
+  CeasedDateKeys,
   DateOfBirthKey,
   DateOfBirthKeys,
   StartDateKey,
@@ -148,6 +150,13 @@ export const setBeneficialOwnerData = (reqBody: any, id: string): ApplicationDat
     : {};
   data[DateOfBirthKey] = mapFieldsToDataObject(reqBody, DateOfBirthKeys, InputDateKeys);
   data[StartDateKey] = mapFieldsToDataObject(reqBody, StartDateKeys, InputDateKeys);
+
+  // not present in register journey
+  if ("is_still_bo" in reqBody) {
+    data[CeasedDateKey] = (reqBody["is_still_bo"] === '0')
+      ? mapFieldsToDataObject(reqBody, CeasedDateKeys, InputDateKeys)
+      : {};
+  }
 
   // It needs concatenations because if in the check boxes we select only one option
   // nunjucks returns just a string and with concat we will return an array.

8d55d59666aa2d3843a21ab0ae4c4c476daa3e46
cdrainCH <cdrain@companieshouse.gov.uk>
2023-05-02 13:59:01 +0100

    change link repopulates ceased date field

--- src/controllers/update/update.review.beneficial.owner.individual.ts
+++ src/controllers/update/update.review.beneficial.owner.individual.ts
@@ -1,6 +1,11 @@
-import { REVIEW_BENEFICIAL_OWNER_INDEX_PARAM, UPDATE_BENEFICIAL_OWNER_BO_MO_REVIEW_URL, UPDATE_BENEFICIAL_OWNER_TYPE_URL, UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE, UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_URL } from "../../config";
+import {
+  REVIEW_BENEFICIAL_OWNER_INDEX_PARAM,
+  UPDATE_BENEFICIAL_OWNER_BO_MO_REVIEW_URL,
+  UPDATE_BENEFICIAL_OWNER_TYPE_URL,
+  UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE,
+  UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_URL } from "../../config";
 import { NextFunction, Request, Response } from "express";
-import { getApplicationData, removeFromApplicationData, setApplicationData, getFromApplicationData, mapDataObjectToFields } from "../../utils/application.data";
+import { getApplicationData, removeFromApplicationData, setApplicationData } from "../../utils/application.data";
 import { logger } from "../../utils/logger";
 import { BeneficialOwnerIndividualKey } from "../../model/beneficial.owner.individual.model";
 import { ApplicationData, ApplicationDataType } from "../../model";
@@ -8,8 +13,7 @@ import { setBeneficialOwnerData } from "../../utils/beneficial.owner.individual"
 import { v4 as uuidv4 } from "uuid";
 import { saveAndContinue } from "../../utils/save.and.continue";
 import { Session } from "@companieshouse/node-session-handler";
-import { CeasedDateKey, CeasedDateKeys } from "../../model/date.model";
-import { EntityNumberKey, InputDateKeys } from "../../model/data.types.model";
+import { addCeasedDateToTemplateOptions } from "../../utils/update/ceased_date_util";
 
 export const get = (req: Request, res: Response) => {
   logger.debugRequest(req, `${req.method} ${req.route.path}`);
@@ -17,13 +21,13 @@ export const get = (req: Request, res: Response) => {
   const index = req.query.index;
 
   let dataToReview = {};
-  let id = "";
   if (appData?.beneficial_owners_individual){
     dataToReview = appData?.beneficial_owners_individual[Number(index)];
-    id = appData?.beneficial_owners_individual[Number(index)].id;
   }
 
-  const data = getFromApplicationData(req, BeneficialOwnerIndividualKey, id, true);
+  if (!dataToReview["ceased_date"]) {
+    dataToReview["ceased_date"] = {};
+  }
 
   const backLinkUrl = getBackLinkUrl(appData, index);
 
@@ -31,27 +35,11 @@ export const get = (req: Request, res: Response) => {
     backLinkUrl,
     templateName: UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE,
     ...dataToReview,
-    ...data,
     isOwnersReview: true
   };
 
-  // extra data needed for update journey
-  if (EntityNumberKey in appData && appData[EntityNumberKey] !== null) {
-    templateOptions["is_still_bo"] = (Object.keys(data["ceased_date"]).length === 0) ? 1 : 0;
-    templateOptions[EntityNumberKey] = appData[EntityNumberKey];
-    templateOptions["ceased_date"] = (data) ? mapDataObjectToFields(data[CeasedDateKey], CeasedDateKeys, InputDateKeys) : {};
-  }
+  return res.render(UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE, addCeasedDateToTemplateOptions(templateOptions, appData, dataToReview));
 
-  return res.render(UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE, templateOptions);
-
-  // console.log(JSON.stringify(appData.beneficial_owners_individual));
-
-  // return res.render(UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE, {
-  //   backLinkUrl,
-  //   templateName: UPDATE_REVIEW_BENEFICIAL_OWNER_INDIVIDUAL_PAGE,
-  //   ...dataToReview,
-  //   isOwnersReview: true
-  // });
 };
 
 const getBackLinkUrl = (appData: ApplicationData, pageIndex) => {
@@ -80,7 +68,6 @@ export const post = async (req: Request, res: Response, next: NextFunction) => {
       setApplicationData(req.session, data, BeneficialOwnerIndividualKey);
 
       appData.beneficial_owners_individual[Number(boiIndex)].date_of_birth = dateOfBirth;
-      // appData.beneficial_owners_individual[Number(boiIndex)].ceased_date = {day: "27", month: "4", year: "2023"}
 
       await saveAndContinue(req, session, false);
 

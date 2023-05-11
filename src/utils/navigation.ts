9da021282a69ff676e7d94b98e9b487141785c24
cdrainCH <cdrain@companieshouse.gov.uk>
2023-05-11 13:07:17 +0100

    fixed error in navigation, added tests

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -71,7 +71,7 @@ export const NAVIGATION: Navigation = {
   },
   [config.UPDATE_REVIEW_OVERSEAS_ENTITY_INFORMATION_URL]: {
     currentPage: config.UPDATE_REVIEW_OVERSEAS_ENTITY_INFORMATION_PAGE,
-    previousPage: () => [config.UPDATE_DUE_DILIGENCE_PAGE, config.UPDATE_DUE_DILIGENCE_OVERSEAS_ENTITY_PAGE],
+    previousPage: () => config.WHO_IS_MAKING_UPDATE_PAGE,
     nextPage: [config.OVERSEAS_ENTITY_REVIEW_URL]
   },
   [config.OVERSEAS_ENTITY_REVIEW_URL]: {

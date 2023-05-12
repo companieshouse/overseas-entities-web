868d3138203583c4ed51b59b50159f3f8985c40e
cdrainCH <120725571+cdrainCH@users.noreply.github.com>
2023-05-12 11:29:13 +0100

    Fix/uar 392 youre about to review overseas entity information page (#882)
    * changed button to continue, back link goes to agent details page
    
    * fixed back link redirection, fixed title
    
    * removed log, fixed navigation, tidied code in controller
    
    * fixed error in navigation, added tests
    
    * revert navigation file to match main
    
    * removing unnecessary code

--- src/controllers/update/update.review.overseas.entity.information.controller.ts
+++ src/controllers/update/update.review.overseas.entity.information.controller.ts
@@ -4,6 +4,7 @@ import * as config from "../../config";
 import { ApplicationData } from "model";
 import { getApplicationData } from "../../utils/application.data";
 import { Session } from "@companieshouse/node-session-handler";
+import { DueDiligenceKey } from "../../model/due.diligence.model";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -14,7 +15,7 @@ export const get = (req: Request, res: Response, next: NextFunction) => {
 
     return res.render(config.UPDATE_REVIEW_OVERSEAS_ENTITY_INFORMATION_PAGE, {
       templateName: config.UPDATE_REVIEW_OVERSEAS_ENTITY_INFORMATION_PAGE,
-      backLinkUrl: config.WHO_IS_MAKING_UPDATE_URL,
+      backLinkUrl: getBackLinkUrl(appData),
       appData
     });
   } catch (errors) {
@@ -31,4 +32,19 @@ export const post = (req: Request, res: Response, next: NextFunction) => {
     logger.errorRequest(req, errors);
     next(errors);
   }
+};
+
+const getBackLinkUrl = (appData: ApplicationData) => {
+  let backLinkUrl;
+
+  const agentDueDiligence = appData[DueDiligenceKey] && Object.keys(appData[DueDiligenceKey]).length > 0;
+
+  if (agentDueDiligence) {
+    backLinkUrl = config.UPDATE_DUE_DILIGENCE_URL;
+  } else {
+    backLinkUrl = config.UPDATE_DUE_DILIGENCE_OVERSEAS_ENTITY_URL;
+  }
+
+  return backLinkUrl;
+
 };

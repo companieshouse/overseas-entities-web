1d62f9b800e8289064040c5a171fa4cd6117b990
corybudd-ch <117734784+corybudd-ch@users.noreply.github.com>
2023-06-14 10:46:27 +0100

    update back button logic for submit by paper page

--- src/controllers/update/update.trusts.submit.by.paper.controller.ts
+++ src/controllers/update/update.trusts.submit.by.paper.controller.ts
@@ -1,11 +1,19 @@
 import { logger } from "../../utils/logger";
 import * as config from "../../config";
 import { Request, Response } from "express";
+import { getApplicationData } from "../../utils/application.data";
+import { ApplicationData } from "../../model";
+import { Session } from "@companieshouse/node-session-handler";
 
 export const get = (req: Request, res: Response) => {
   logger.debugRequest(req, `${req.method} ${req.route.path}`);
+
+  const appData: ApplicationData = getApplicationData(req.session as Session);
+
+  const backLinkUrl = appData?.entity ? config.UPDATE_OVERSEAS_ENTITY_CONFIRM_URL : config.UPDATE_ANY_TRUSTS_INVOLVED_URL;
+
   return res.render(config.UPDATE_TRUSTS_SUBMIT_BY_PAPER_PAGE, {
-    backLinkUrl: config.UPDATE_ANY_TRUSTS_INVOLVED_URL,
+    backLinkUrl,
     templateName: config.UPDATE_TRUSTS_SUBMIT_BY_PAPER_PAGE
   });
 };

dd49b4302dea85ac64519b797d344197f4e72825
conuirchgov <133892214+conuirchgov@users.noreply.github.com>
2023-06-16 10:47:37 +0100

    UAR-616: Request private OE data in update journey (#916)

---
+++ src/service/private.overseas.entity.details.ts
@@ -0,0 +1,36 @@
+import { Session } from "@companieshouse/node-session-handler";
+import { Request } from "express";
+import { makeApiCallWithRetry } from "./retry.handler.service";
+import { createAndLogErrorRequest, logger } from "../utils/logger";
+import { OverseasEntityExtraDetails } from "@companieshouse/api-sdk-node/dist/services/overseas-entities/types";
+import { ApplicationData } from "../model/application.model";
+
+export const hasRetrievedPrivateOeDetails = (appData: ApplicationData): boolean =>
+  !!appData.entity?.email;
+
+export const getPrivateOeDetails = async (
+  req: Request,
+  oeNumber: string,
+): Promise<OverseasEntityExtraDetails | undefined> => {
+  const response = await makeApiCallWithRetry(
+    "overseasEntity",
+    "getOverseasEntityDetails",
+    req,
+    req.session as Session,
+    oeNumber,
+  );
+
+  if (response.httpStatusCode !== 200 && response.httpStatusCode !== 404) {
+    const errorMsg = `Something went wrong fetching company details = ${JSON.stringify(response)}`;
+    throw createAndLogErrorRequest(req, errorMsg);
+  }
+
+  if (response.httpStatusCode === 404) {
+    logger.debugRequest(req, `No company data found for ${oeNumber}`);
+    return undefined;
+  }
+
+  const infoMsg = `OE NUMBER ID: ${oeNumber}`;
+  logger.debugRequest(req, `Overseas Entity Data Retrieved - ${infoMsg}`);
+  return response.resource;
+};

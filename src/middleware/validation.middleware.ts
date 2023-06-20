2399821d7802b091e6ce927db14cc2239b5ebff3
bmcgreevyCH <bmcgreevy@companieshouse.gov.uk>
2023-06-20 15:27:47 +0100

    Update View with current Bos Mos and remove private fields for Individual BO

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -49,7 +49,6 @@ export function checkValidations(req: Request, res: Response, next: NextFunction
       const id = req.params[ID];
       const appData: ApplicationData = getApplicationData(req.session);
       const entityName = appData?.[EntityNameKey];
-      console.log(`error is ${JSON.stringify(errors)}`);
       return res.render(NAVIGATION[routePath].currentPage, {
         backLinkUrl: NAVIGATION[routePath].previousPage(appData),
         templateName: NAVIGATION[routePath].currentPage,

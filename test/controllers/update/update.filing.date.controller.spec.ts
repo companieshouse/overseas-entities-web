3bd6beaabb3e6fe1a567d6eb698333c6719bddb3
corybudd-ch <117734784+corybudd-ch@users.noreply.github.com>
2023-06-26 11:04:58 +0100

    add test to check for error summary heading

--- test/controllers/update/update.filing.date.controller.spec.ts
+++ test/controllers/update/update.filing.date.controller.spec.ts
@@ -36,6 +36,7 @@ import {
 import {
   ANY_MESSAGE_ERROR,
   BACK_LINK_FOR_UPDATE_FILING_DATE,
+  ERROR_LIST,
   FOUND_REDIRECT_TO,
   PAGE_TITLE_ERROR,
   SERVICE_UNAVAILABLE,
@@ -208,6 +209,7 @@ describe("Update Filing Date controller", () => {
 
       expect(resp.status).toEqual(200);
       expect(resp.text).toContain(ErrorMessages.ENTER_DATE);
+      expect(resp.text).toContain(ERROR_LIST);
     });
 
     test(`catch error on POST action for ${config.UPDATE_FILING_DATE_URL} page`, async () => {

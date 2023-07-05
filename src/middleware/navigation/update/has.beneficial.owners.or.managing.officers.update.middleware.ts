6f6721814ddddcca3143171b8770bebbd6a2a00c
olaych <118522865+olaych@users.noreply.github.com>
2023-07-05 11:46:36 +0100

    fixes payment redirection (#955)
    * fixes payment redirection
    
    * added redirect on failure to include no change
    
    * added no change redirect on payment failure
    
    * implemented review comment
    
    * implemented review comment

--- src/middleware/navigation/update/has.beneficial.owners.or.managing.officers.update.middleware.ts
+++ src/middleware/navigation/update/has.beneficial.owners.or.managing.officers.update.middleware.ts
@@ -4,10 +4,12 @@ import { logger } from '../../../utils/logger';
 import { SECURE_UPDATE_FILTER_URL } from '../../../config';
 import { getApplicationData } from "../../../utils/application.data";
 import { checkBOsOrMOsDetailsEnteredUpdate, NavigationErrorMessage } from '../check.condition';
+import { ApplicationData } from 'model';
 
 export const hasBOsOrMOsUpdate = (req: Request, res: Response, next: NextFunction): void => {
   try {
-    if (!checkBOsOrMOsDetailsEnteredUpdate(getApplicationData(req.session))) {
+    const appData: ApplicationData = getApplicationData(req.session);
+    if (!(appData.update?.no_change || checkBOsOrMOsDetailsEnteredUpdate(appData))) {
       logger.infoRequest(req, NavigationErrorMessage);
       return res.redirect(SECURE_UPDATE_FILTER_URL);
     }

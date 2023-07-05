9f4e9a16daacc54e721f9b81d57bf3edbba473e8
olaych <oolatunji@companieshouse.gov.uk>
2023-07-05 10:39:57 +0100

    implemented review comment

--- src/middleware/navigation/update/has.beneficial.owners.or.managing.officers.update.middleware.ts
+++ src/middleware/navigation/update/has.beneficial.owners.or.managing.officers.update.middleware.ts
@@ -9,7 +9,8 @@ import { ApplicationData } from 'model';
 export const hasBOsOrMOsUpdate = (req: Request, res: Response, next: NextFunction): void => {
   try {
     const appData: ApplicationData = getApplicationData(req.session);
-    if (!checkBOsOrMOsDetailsEnteredUpdate(appData) && !appData.update?.no_change) {
+    // if (!checkBOsOrMOsDetailsEnteredUpdate(appData) && !appData.update?.no_change) {
+    if (!(appData.update?.no_change || checkBOsOrMOsDetailsEnteredUpdate(appData))) {
       logger.infoRequest(req, NavigationErrorMessage);
       return res.redirect(SECURE_UPDATE_FILTER_URL);
     }

2af322123b716415b65f5ce6a32ac6e40910cb02
conuirchgov <133892214+conuirchgov@users.noreply.github.com>
2023-07-06 15:38:03 +0100

    UAR-826: Update trusts skeleton journey added to web (#957)

--- src/controllers/update/check.your.answers.controller.ts
+++ src/controllers/update/check.your.answers.controller.ts
@@ -1,13 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+import * as config from "../../config";
+import { isActiveFeature } from "../../utils/feature.flag";
 
-import {
-  UPDATE_BENEFICIAL_OWNER_TYPE_URL,
-  UPDATE_CHECK_YOUR_ANSWERS_PAGE,
-} from "../../config";
 import { getDataForReview, postDataForReview } from "../../utils/check.your.answers";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
-  getDataForReview(req, res, next, UPDATE_CHECK_YOUR_ANSWERS_PAGE, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
+  const backLinkUrl = isActiveFeature(config.FEATURE_FLAG_ENABLE_UPDATE_TRUSTS)
+    ? config.UPDATE_TRUSTS_ASSOCIATED_WITH_THE_OVERSEAS_ENTITY_URL
+    : config.UPDATE_BENEFICIAL_OWNER_TYPE_URL;
+
+  getDataForReview(req, res, next, config.UPDATE_CHECK_YOUR_ANSWERS_PAGE, backLinkUrl);
 };
 
 export const post = (req: Request, res: Response, next: NextFunction) => {

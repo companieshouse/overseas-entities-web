2a0e85faf527c3463d56e2cddb7a5be8f679e175
Joshua Higginson <jhigginson@companieshouse.gov.uk>
2023-07-12 16:14:11 +0100

    UAR-264: Trusts associated with OE page

--- src/controllers/update/update.trusts.associated.with.entity.controller.ts
+++ src/controllers/update/update.trusts.associated.with.entity.controller.ts
@@ -1,26 +1,10 @@
 import { NextFunction, Request, Response } from "express";
-import { logger } from "../../utils/logger";
-import * as config from "../../config";
+import { getTrusts, postTrusts } from "../../utils/add.trust";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    logger.debugRequest(req, `${req.method} ${req.route.path}`);
-
-    return res.render(config.UPDATE_TRUSTS_ASSOCIATED_WITH_THE_OVERSEAS_ENTITY_PAGE, {
-      backLinkUrl: config.UPDATE_TRUSTS_INDIVIDUALS_OR_ENTITIES_INVOLVED_URL,
-      templateName: config.UPDATE_TRUSTS_ASSOCIATED_WITH_THE_OVERSEAS_ENTITY_PAGE
-    });
-  } catch (error) {
-    next(error);
-  }
+  getTrusts(req, res, next, true);
 };
 
 export const post = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    logger.debugRequest(req, `${req.method} ${req.route.path}`);
-
-    return res.redirect(config.UPDATE_CHECK_YOUR_ANSWERS_URL);
-  } catch (error) {
-    next(error);
-  }
+  postTrusts(req, res, next, true);
 };

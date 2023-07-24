12cc8b44b2405dded5418f64476826da7ea640b2
mattch1 <mculpin@companieshouse.gov.uk>
2023-07-24 09:58:31 +0100

    ROE-2309 Restore bad save and continue test

--- test/utils/save.and.continue.spec.ts
+++ test/utils/save.and.continue.spec.ts
@@ -1,3 +1,4 @@
+jest.mock("../../src/utils/feature.flag" );
 jest.mock("../../src/service/overseas.entities.service" );
 
 import { beforeEach, describe, expect, test, jest } from '@jest/globals';
@@ -6,8 +7,10 @@ import { Session } from '@companieshouse/node-session-handler';
 
 import { saveAndContinue } from '../../src/utils/save.and.continue';
 import { updateOverseasEntity } from "../../src/service/overseas.entities.service";
+import { isActiveFeature } from "../../src/utils/feature.flag";
 import { getSessionRequestWithExtraData } from '../__mocks__/session.mock';
 
+const mockIsActiveFeature = isActiveFeature as jest.Mock;
 const mockUpdateOverseasEntity = updateOverseasEntity as jest.Mock;
 
 const session = getSessionRequestWithExtraData();
@@ -19,8 +22,20 @@ describe('saveAndContinue test suite', () => {
     jest.clearAllMocks();
   });
 
-  test('should call updateOverseasEntity if feature flag is true', async () => {
+  test('should call updateOverseasEntity if is registration not update journey', async () => {
     await saveAndContinue(req, req.session as Session, true);
     expect(mockUpdateOverseasEntity).toBeCalledWith(req, req.session);
+  });
+
+  test('should not call updateOverseasEntity if not registration and feature flag is false', async () => {
+    mockIsActiveFeature.mockReturnValue(false);
+    await saveAndContinue(req, req.session as Session, false);
+    expect(mockUpdateOverseasEntity).not.toHaveBeenCalled();
+  });
+
+  test('should call updateOverseasEntity if not registration and feature flag is true', async () => {
+    mockIsActiveFeature.mockReturnValue(true);
+    await saveAndContinue(req, req.session as Session, false);
+    expect(mockUpdateOverseasEntity).toHaveBeenCalled();
   });
 });

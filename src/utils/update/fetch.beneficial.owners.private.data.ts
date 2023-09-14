baf7c4a6967ba59dd7dafa68ca14431f0b5bdaec
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-09-14 09:08:02 +0100

    uar-619 fix code smell

--- src/utils/update/fetch.beneficial.owners.private.data.ts
+++ src/utils/update/fetch.beneficial.owners.private.data.ts
@@ -32,21 +32,20 @@ export const fetchBeneficialOwnersPrivateData = async (appData: ApplicationData,
 };
 
 const mapBeneficialOwnersPrivateData = (boPrivateData, appData: ApplicationData) => {
-  if (boPrivateData !== undefined && boPrivateData.length > 0) {
-    appData.update?.review_beneficial_owners_individual?.forEach(beneficialOwner => {
-      if (beneficialOwner.ch_reference) {
-        beneficialOwner.usual_residential_address = mapBoPrivateAddress(boPrivateData, beneficialOwner.ch_reference, false);
-      }
-    });
-    appData.update?.review_beneficial_owners_government_or_public_authority?.forEach(beneficialOwner => {
-      if (beneficialOwner.ch_reference) {
-        beneficialOwner.principal_address = mapBoPrivateAddress(boPrivateData, beneficialOwner.ch_reference, true);
-      }
-    });
-    appData.update?.review_beneficial_owners_corporate?.forEach(beneficialOwner => {
-      if (beneficialOwner.ch_reference) {
-        beneficialOwner.principal_address = mapBoPrivateAddress(boPrivateData, beneficialOwner.ch_reference, true);
-      }
-    });
+  if (boPrivateData === undefined || boPrivateData.length === 0) {
+    return;
   }
+  const mapUsualResidentialAddress = beneficialOwner => {
+    if (beneficialOwner.ch_reference) {
+      beneficialOwner.usual_residential_address = mapBoPrivateAddress(boPrivateData, beneficialOwner.ch_reference, false);
+    }
+  };
+  const mapPrincipalAddress = beneficialOwner => {
+    if (beneficialOwner.ch_reference) {
+      beneficialOwner.principal_address = mapBoPrivateAddress(boPrivateData, beneficialOwner.ch_reference, true);
+    }
+  };
+  appData.update?.review_beneficial_owners_individual?.forEach(mapUsualResidentialAddress);
+  appData.update?.review_beneficial_owners_government_or_public_authority?.forEach(mapPrincipalAddress);
+  appData.update?.review_beneficial_owners_corporate?.forEach(mapPrincipalAddress);
 };

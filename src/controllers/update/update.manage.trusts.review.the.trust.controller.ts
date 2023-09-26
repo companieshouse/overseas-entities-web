bf3ab181dd7f9edadc3924a6fac202990fd16bf3
cdrainCH <cdrain@companieshouse.gov.uk>
2023-09-26 14:56:51 +0100

    fixed validation, bo boxes checked

--- src/controllers/update/update.manage.trusts.review.the.trust.controller.ts
+++ src/controllers/update/update.manage.trusts.review.the.trust.controller.ts
@@ -11,12 +11,18 @@ export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
     logger.debugRequest(req, `${req.method} ${req.route.path}`);
 
+    const isReview = true;
     const appData = getApplicationData(req.session);
     const trustInReview = appData.update?.review_trusts?.find(trust => trust.review?.in_review);
     let trustId = "";
     if (trustInReview?.trust_id) {
       trustId = trustInReview?.trust_id;
     }
+
+    if (appData.beneficial_owners_individual) {
+      appData.beneficial_owners_individual[0].trust_ids = ["1"];
+    }
+
     const formData = mapperDetails.mapDetailToPage(appData, trustId, true);
 
     const boAvailableForTrust = [
@@ -25,20 +31,6 @@ export const get = (req: Request, res: Response, next: NextFunction) => {
       ...getBoOtherAssignableToTrust(appData)
         .map(mapperBo.mapBoOtherToPage),
     ];
-
-    if (appData.update?.review_trusts && trustInReview) {
-      const historicalBOs = trustInReview.HISTORICAL_BO;
-      const individualBos = trustInReview.INDIVIDUALS;
-      const otherBos = trustInReview.CORPORATES;
-      const trustBos = [
-        ...getTrustBoTypeIds(historicalBOs),
-        ...getTrustBoTypeIds(individualBos),
-        ...getTrustBoTypeIds(otherBos)
-      ];
-      formData.beneficialOwnersIds = trustBos;
-    }
-
-    const isReview = true;
 
     return res.render(UPDATE_MANAGE_TRUSTS_REVIEW_THE_TRUST_PAGE, {
       templateName: UPDATE_MANAGE_TRUSTS_REVIEW_THE_TRUST_PAGE,
@@ -56,19 +48,4 @@ export const get = (req: Request, res: Response, next: NextFunction) => {
 
 export const post = (req: Request, res: Response, next: NextFunction) => {
   postTrustDetails(req, res, next, true, true);
-
-  // vvv  Conuir's Addition
-  // take submitted form, update appData as appropriate, and save.
-  // return res.redirect(UPDATE_MANAGE_TRUSTS_ORCHESTRATOR_URL);
-};
-
-const getTrustBoTypeIds = (boType) => {
-  const boIds: string[] = [];
-  for (const bo of boType) {
-    if (bo.id) {
-      const id = bo.id;
-      boIds.push(id);
-    }
-  }
-  return boIds;
 };

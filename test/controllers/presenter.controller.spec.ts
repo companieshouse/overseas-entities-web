808acff4406553494d9dc426b496f815fbb31480
mwestacott <mwestacott2@companieshouse.gov.uk>
2023-09-29 12:38:02 +0100

    Add new (copied) tests for the controller POST end-point

--- test/controllers/presenter.controller.spec.ts
+++ test/controllers/presenter.controller.spec.ts
@@ -3,7 +3,8 @@ jest.mock('../../src/middleware/authentication.middleware');
 jest.mock('../../src/utils/application.data');
 jest.mock('../../src/utils/save.and.continue');
 jest.mock('../../src/middleware/navigation/has.overseas.name.middleware');
-// jest.mock('../../src/utils/feature.flag');
+jest.mock('../../src/utils/feature.flag');
+jest.mock('../../src/middleware/service.availability.middleware');
 
 import { describe, expect, test, jest, beforeEach } from '@jest/globals';
 import { NextFunction, Request, Response } from "express";
@@ -14,6 +15,7 @@ import {
   PRESENTER_URL,
   LANDING_URL,
   WHO_IS_MAKING_FILING_PAGE,
+  WHO_IS_MAKING_FILING_WITH_PARAMS_URL,
   WHO_IS_MAKING_FILING_URL,
   LANDING_PAGE_URL,
   PRESENTER_WITH_PARAMS_URL,
@@ -45,7 +47,8 @@ import {
 } from '../__mocks__/validation.mock';
 import { hasOverseasName } from "../../src/middleware/navigation/has.overseas.name.middleware";
 import { saveAndContinue } from "../../src/utils/save.and.continue";
-// import { isActiveFeature } from "../../src/utils/feature.flag";
+import { isActiveFeature } from "../../src/utils/feature.flag";
+import { serviceAvailabilityMiddleware } from "../../src/middleware/service.availability.middleware";
 
 const mockSaveAndContinue = saveAndContinue as jest.Mock;
 
@@ -61,13 +64,17 @@ mockAuthenticationMiddleware.mockImplementation((req: Request, res: Response, ne
 
 const mockPrepareData = prepareData as jest.Mock;
 
-// const mockIsActiveFeature = isActiveFeature as jest.Mock;
+const mockIsActiveFeature = isActiveFeature as jest.Mock;
+
+const mockServiceAvailabilityMiddleware = serviceAvailabilityMiddleware as jest.Mock;
+mockServiceAvailabilityMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
 
 describe("PRESENTER controller", () => {
 
   beforeEach(() => {
     jest.clearAllMocks();
     mockSetApplicationData.mockReset();
+    mockIsActiveFeature.mockReset();
     process.env.FEATURE_FLAG_ENABLE_REDIS_REMOVAL_27092023 = "false";
   });
 
@@ -240,13 +247,140 @@ describe("PRESENTER controller", () => {
   });
 
   describe("POST with url params tests", () => {
-    // test(`redirect to the ${WHO_IS_MAKING_FILING_PAGE} page after a successful post from presenter page with url params`, async () => {
-    //   mockIsActiveFeature.mockReturnValueOnce(false);
-    //   const resp = await request(app).post(PRESENTER_WITH_PARAMS_URL).send(PRESENTER_OBJECT_MOCK);
+    test(`redirect to the ${WHO_IS_MAKING_FILING_PAGE} page after a successful post from presenter page with url params`, async () => {
+      mockIsActiveFeature.mockReturnValueOnce(true); // For FEATURE_FLAG_ENABLE_ROE_UPDATE
 
-    //   expect(resp.status).toEqual(302);
-    //   expect(resp.text).toContain(`${WHO_IS_MAKING_FILING_WITH_PARAMS_URL}`);
-    //   expect(mockSaveAndContinue).toHaveBeenCalledTimes(1);
-    // });
+      const resp = await request(app).post(PRESENTER_WITH_PARAMS_URL).send(PRESENTER_OBJECT_MOCK);
+
+      expect(resp.status).toEqual(302);
+      expect(resp.text).toContain(`${WHO_IS_MAKING_FILING_WITH_PARAMS_URL}`);
+      expect(mockSaveAndContinue).toHaveBeenCalledTimes(1);
+    });
+
+    test(`redirect to the ${WHO_IS_MAKING_FILING_PAGE} page after a successful post from presenter page with special characters`, async () => {
+      mockIsActiveFeature.mockReturnValueOnce(true); // For FEATURE_FLAG_ENABLE_ROE_UPDATE
+
+      const resp = await request(app).post(PRESENTER_WITH_PARAMS_URL).send(PRESENTER_WITH_SPECIAL_CHARACTERS_FIELDS_MOCK);
+
+      expect(resp.status).toEqual(302);
+      expect(resp.text).toContain(`${WHO_IS_MAKING_FILING_WITH_PARAMS_URL}`);
+      expect(mockSaveAndContinue).toHaveBeenCalledTimes(1);
+    });
+
+    test("renders the current page with error message", async () => {
+      mockIsActiveFeature.mockReturnValueOnce(true); // For FEATURE_FLAG_ENABLE_ROE_UPDATE
+
+      const resp = await request(app).post(PRESENTER_WITH_PARAMS_URL);
+
+      expect(resp.status).toEqual(200);
+      expect(resp.text).toContain(PRESENTER_PAGE_TITLE);
+      expect(resp.text).toContain(ErrorMessages.FULL_NAME);
+      expect(resp.text).toContain(ErrorMessages.EMAIL);
+      expect(resp.text).not.toContain(ErrorMessages.MAX_EMAIL_LENGTH);
+      expect(resp.text).not.toContain(ErrorMessages.EMAIL_INVALID_FORMAT);
+      expect(resp.text).toContain(LANDING_URL);
+      expect(mockSaveAndContinue).not.toHaveBeenCalled();
+    });
+
+    test(`POST empty object and check for error in page title`, async () => {
+      const resp = await request(app).post(PRESENTER_WITH_PARAMS_URL);
+      expect(resp.status).toEqual(200);
+      expect(resp.text).toContain(PAGE_TITLE_ERROR);
+    });
+
+    test("renders the current page with MAX error messages", async () => {
+      const resp = await request(app)
+        .post(PRESENTER_WITH_PARAMS_URL)
+        .send(PRESENTER_WITH_MAX_LENGTH_FIELDS_MOCK);
+
+      expect(resp.status).toEqual(200);
+      expect(resp.text).toContain(PRESENTER_PAGE_TITLE);
+      expect(resp.text).toContain(ErrorMessages.MAX_FULL_NAME_LENGTH);
+      expect(resp.text).toContain(ErrorMessages.MAX_EMAIL_LENGTH);
+      expect(resp.text).not.toContain(ErrorMessages.EMAIL_INVALID_FORMAT);
+      expect(resp.text).not.toContain(ErrorMessages.FULL_NAME);
+    });
+
+    test("renders the current page with INVALID_CHARACTERS error message for full name and email", async () => {
+      const resp = await request(app)
+        .post(PRESENTER_WITH_PARAMS_URL)
+        .send(PRESENTER_WITH_INVALID_CHARACTERS_FIELDS_MOCK);
+
+      expect(resp.status).toEqual(200);
+      expect(resp.text).toContain(PRESENTER_PAGE_TITLE);
+      expect(resp.text).toContain(ErrorMessages.FULL_NAME_INVALID_CHARACTERS);
+      expect(resp.text).toContain(ErrorMessages.EMAIL_INVALID_FORMAT);
+      expect(resp.text).not.toContain(ErrorMessages.MAX_EMAIL_LENGTH);
+    });
+
+    test("renders the next page and no errors are reported if email has leading and trailing spaces", async () => {
+      mockIsActiveFeature.mockReturnValueOnce(true); // For FEATURE_FLAG_ENABLE_ROE_UPDATE
+
+      const resp = await request(app)
+        .post(PRESENTER_WITH_PARAMS_URL)
+        .send(PRESENTER_OBJECT_MOCK_WITH_EMAIL_CONTAINING_LEADING_AND_TRAILING_SPACES);
+
+      expect(resp.status).toEqual(302);
+      expect(resp.text).toContain(`${WHO_IS_MAKING_FILING_WITH_PARAMS_URL}`);
+
+      // Additionally check that email address is trimmed before it's saved in the session
+      const data: ApplicationDataType = mockPrepareData.mock.calls[0][0];
+      expect(data["email"]).toEqual(EMAIL_ADDRESS);
+    });
+
+    test("catch error when post data from presenter page", async () => {
+      mockSetApplicationData.mockImplementationOnce( () => { throw new Error(ANY_MESSAGE_ERROR); });
+      const resp = await request(app).post(PRESENTER_WITH_PARAMS_URL).send(PRESENTER_OBJECT_MOCK);
+
+      expect(resp.status).toEqual(500);
+      expect(resp.text).toContain(SERVICE_UNAVAILABLE);
+    });
+
+    test("Test email is valid with long email address", async () => {
+      mockIsActiveFeature.mockReturnValueOnce(true); // For FEATURE_FLAG_ENABLE_ROE_UPDATE
+
+      const presenter = {
+        ...PRESENTER_OBJECT_MOCK,
+        email: "vsocarroll@QQQQQQQT123465798U123456789V123456789W123456789X123456789Y123456.companieshouse.gov.uk" };
+      const resp = await request(app)
+        .post(PRESENTER_WITH_PARAMS_URL)
+        .send(presenter);
+      expect(resp.status).toEqual(302);
+      expect(resp.text).toContain(`${WHO_IS_MAKING_FILING_WITH_PARAMS_URL}`);
+      expect(resp.text).not.toContain(ErrorMessages.MAX_EMAIL_LENGTH);
+      expect(resp.text).not.toContain(ErrorMessages.EMAIL_INVALID_FORMAT);
+    });
+
+    test("Test email is valid with long email name and address", async () => {
+      mockIsActiveFeature.mockReturnValueOnce(true); // For FEATURE_FLAG_ENABLE_ROE_UPDATE
+
+      const presenter = {
+        ...PRESENTER_OBJECT_MOCK,
+        email: "socarrollA123456789B132456798C123456798D123456789@T123465798U123456789V123456789W123456789X123456789Y123456.companieshouse.gov.uk" };
+      const resp = await request(app)
+        .post(PRESENTER_WITH_PARAMS_URL)
+        .send(presenter);
+      expect(resp.status).toEqual(302);
+      expect(resp.text).toContain(`${WHO_IS_MAKING_FILING_WITH_PARAMS_URL}`);
+      expect(resp.text).not.toContain(ErrorMessages.MAX_EMAIL_LENGTH);
+      expect(resp.text).not.toContain(ErrorMessages.EMAIL_INVALID_FORMAT);
+    });
+
+    test("Test email is valid with very long email name and address", async () => {
+      mockIsActiveFeature.mockReturnValueOnce(true); // For FEATURE_FLAG_ENABLE_ROE_UPDATE
+
+      const presenter = {
+        ...PRESENTER_OBJECT_MOCK,
+        email: "socarrollA123456789B132456798C123456798D123456789E123456789F123XX@T123465798U123456789V123456789W123456789X123456789Y123456.companieshouse.gov.uk" };
+      const resp = await request(app)
+        .post(PRESENTER_WITH_PARAMS_URL)
+        .send(presenter);
+      expect(resp.status).toEqual(302);
+      expect(resp.text).toContain(`${WHO_IS_MAKING_FILING_WITH_PARAMS_URL}`);
+      expect(resp.text).not.toContain(ErrorMessages.EMAIL);
+      expect(resp.text).not.toContain(ErrorMessages.MAX_EMAIL_LENGTH);
+      expect(resp.text).not.toContain(ErrorMessages.EMAIL_INVALID_FORMAT);
+    });
+
   });
 });

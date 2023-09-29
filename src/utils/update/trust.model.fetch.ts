ec7639e6cbdb8282be5d0d88854e7b2d6351bb34
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-09-29 15:13:58 +0100

    uar-315 fetch trustees & trust/bo links

--- src/utils/update/trust.model.fetch.ts
+++ src/utils/update/trust.model.fetch.ts
@@ -1,7 +1,12 @@
 import { ApplicationData } from "../../model";
-import { getTrustData } from "../../../src/service/trust.data.service";
+import {
+  getTrustData,
+  getIndividualTrustees,
+  getCorporateTrustees,
+  getTrustLinks
+} from "../../../src/service/trust.data.service";
 import { logger } from "../../utils/logger";
-import { TrustData } from "@companieshouse/api-sdk-node/dist/services/overseas-entities/types";
+import { CorporateTrusteeData, IndividualTrusteeData, TrustData } from "@companieshouse/api-sdk-node/dist/services/overseas-entities/types";
 import { Request } from "express";
 import { Trust } from "../../model/trust.model";
 import { mapInputDate } from "./mapper.utils";
@@ -11,6 +16,7 @@ export const retrieveTrustData = async (req: Request, appData: ApplicationData) 
     initialiseTrustUpdateAppData(appData);
     if (appData.update) {
       await retrieveTrusts(req, appData);
+      await retrieveTrustLinks(req, appData);
       appData.update.trust_data_fetched = true;
     }
   }
@@ -42,13 +48,18 @@ const retrieveTrusts = async (req: Request, appData: ApplicationData) => {
     return;
   }
 
-  for (const trust of trusts) {
-    logger.info("Loaded trust " + trust.trustId);
-    mapTrustData(trust, appData);
+  for (const trustData of trusts) {
+    logger.info("Loaded trust " + trustData.trustId);
+
+    const trust = mapTrustData(trustData, appData);
+
+    fetchAndMapIndivdualTrustees(req, transactionId, overseasEntityId, trust);
+
+    fetchAndMapCorporateTrustees(req, transactionId, overseasEntityId, trust);
   }
 };
 
-const mapTrustData = (trustData: TrustData, appData: ApplicationData) => {
+export const mapTrustData = (trustData: TrustData, appData: ApplicationData) => {
   const dateOfBirth = mapInputDate(trustData.creationDate);
   const trust: Trust = {
     trust_id: trustData.trustId,
@@ -60,4 +71,75 @@ const mapTrustData = (trustData: TrustData, appData: ApplicationData) => {
     unable_to_obtain_all_trust_info: trustData.unableToObtainAllTrustInfo ? "Yes" : "No",
   };
   appData.update?.review_trusts?.push(trust);
+  return trust;
+};
+
+const fetchAndMapIndivdualTrustees = async (
+  req: Request,
+  transactionId: string,
+  overseasEntityId: string,
+  trust: Trust
+) => {
+  const individualTrustees = await getIndividualTrustees(req, transactionId, overseasEntityId, trust.trust_id);
+
+  if (individualTrustees === undefined || individualTrustees.length === 0) {
+    logger.info(`No individual trustees found for overseas entity ${overseasEntityId} and trust ${trust.trust_id}`);
+    return;
+  }
+
+  for (const trustee of individualTrustees) {
+    logger.info("Loaded individual trustee " + trustee.trusteeId);
+    mapIndividualTrusteeData(trustee, trust);
+  }
+};
+
+const mapIndividualTrusteeData = (trustee: IndividualTrusteeData, trust: Trust) => {
+  logger.debug("Mapping individual trustee " + trustee.trusteeId + " for trust " + trust.trust_id);
+  // TODO: map individual trustee data
+};
+
+const fetchAndMapCorporateTrustees = async (
+  req: Request,
+  transactionId: string,
+  overseasEntityId: string,
+  trust: Trust
+) => {
+  const corporateTrustees = await getCorporateTrustees(req, transactionId, overseasEntityId, trust.trust_id);
+
+  if (corporateTrustees === undefined || corporateTrustees.length === 0) {
+    logger.info(`No corporate trustees found for overseas entity ${overseasEntityId} and trust ${trust.trust_id}`);
+    return;
+  }
+
+  for (const trustee of corporateTrustees) {
+    logger.info("Loaded corporate trustee " + trustee.trusteeId);
+    mapCorporateTrusteeData(trustee, trust);
+  }
+};
+
+const mapCorporateTrusteeData = (trustee: CorporateTrusteeData, trust: Trust) => {
+  logger.debug("Mapping corporate trustee " + trustee.trusteeId + " for trust " + trust.trust_id);
+  // TODO: map corporate trustee data
+};
+
+export const retrieveTrustLinks = async (req: Request, appData: ApplicationData) => {
+  const overseasEntityId = appData.overseas_entity_id;
+  const transactionId = appData.transaction_id;
+
+  if (transactionId === undefined || overseasEntityId === undefined) {
+    logger.errorRequest(req, "Trust links could not be retrieved as transactionId or overseasEntityId is undefined");
+    return;
+  }
+
+  const trustLinks = await getTrustLinks(req, transactionId, overseasEntityId);
+
+  if (trustLinks === undefined || trustLinks.length === 0) {
+    logger.info(`No trust links found for overseas entity ${overseasEntityId}`);
+    return;
+  }
+
+  for (const trustLink of trustLinks) {
+    logger.info("Loaded trust link " + trustLink.trustId + " for coprporate appointment " + trustLink.corporateBodyAppointmentId);
+    // TODO: map trust link data
+  }
 };
--- test/utils/update/trust.model.fetch.spec.ts
+++ test/utils/update/trust.model.fetch.spec.ts
@@ -1,4 +1,4 @@
-import { retrieveTrustData } from "../../../src/utils/update/trust.model.fetch";
+import { retrieveTrustData, mapTrustData } from "../../../src/utils/update/trust.model.fetch";
 import { describe, expect, jest, test } from '@jest/globals';
 import { Request } from "express";
 import { getTrustData } from '../../../src/service/trust.data.service';
@@ -6,6 +6,7 @@ import { logger } from '../../../src/utils/logger';
 import { Trust } from "../../../src/model/trust.model";
 import { FETCH_TRUST_DATA_MOCK } from "./mocks";
 import { FETCH_TRUST_APPLICATION_DATA_MOCK } from "../../__mocks__/session.mock";
+import { TrustData } from "@companieshouse/api-sdk-node/dist/services/overseas-entities/types";
 
 jest.mock('../../../src/service/trust.data.service');
 jest.mock('../../../src/utils/logger');
@@ -29,7 +30,7 @@ describe("Test fetching and mapping of Trust data", () => {
     await retrieveTrustData(req, appData);
 
     expect(mockGetTrustData).toBeCalledTimes(1);
-    expect(mockLoggerInfo).toBeCalledTimes(2);
+    expect(mockLoggerInfo).toBeCalledTimes(7);
     expect(appData.update?.review_trusts).toHaveLength(2);
     expect((appData.update?.review_trusts ?? [])[0]).toEqual({
       ch_reference: "12345678",
@@ -58,7 +59,7 @@ describe("Test fetching and mapping of Trust data", () => {
     await retrieveTrustData(req, appData);
 
     expect(mockGetTrustData).toBeCalledTimes(1);
-    expect(mockLoggerInfo).toBeCalledTimes(1);
+    expect(mockLoggerInfo).toBeCalledTimes(2);
   });
 
   test("should fetch and not map trust data data when trusts are empty", async () => {
@@ -68,7 +69,7 @@ describe("Test fetching and mapping of Trust data", () => {
     await retrieveTrustData(req, appData);
 
     expect(mockGetTrustData).toBeCalledTimes(1);
-    expect(mockLoggerInfo).toBeCalledTimes(1);
+    expect(mockLoggerInfo).toBeCalledTimes(2);
   });
 
   test("should not fetch and map trust data data when transactionId is undefined", async () => {
@@ -77,7 +78,7 @@ describe("Test fetching and mapping of Trust data", () => {
     await retrieveTrustData(req, appData);
 
     expect(mockGetTrustData).not.toBeCalled();
-    expect(mockLoggerError).toBeCalledTimes(1);
+    expect(mockLoggerError).toBeCalledTimes(2);
   });
 
   test("should not fetch and map trust data data when overseasEntityId is undefined", async () => {
@@ -86,7 +87,43 @@ describe("Test fetching and mapping of Trust data", () => {
     await retrieveTrustData(req, appData);
 
     expect(mockGetTrustData).not.toBeCalled();
-    expect(mockLoggerError).toBeCalledTimes(1);
+    expect(mockLoggerError).toBeCalledTimes(2);
   });
 
+  test("mapTrustData without date of birth", () => {
+    const appData = { ...FETCH_TRUST_APPLICATION_DATA_MOCK, update: { review_trusts: [] } };
+    const trustData = { ...FETCH_TRUST_DATA_MOCK[0], creationDate: undefined } as unknown as TrustData;
+    const trust: Trust = {
+      trust_id: trustData.trustId,
+      ch_reference: trustData.trustId,
+      trust_name: trustData.trustName,
+      creation_date_day: "",
+      creation_date_month: "",
+      creation_date_year: "",
+      unable_to_obtain_all_trust_info: trustData.unableToObtainAllTrustInfo ? "Yes" : "No",
+    };
+
+    mapTrustData(trustData, appData);
+
+    expect(appData.update?.review_trusts).toHaveLength(1);
+    expect((appData.update?.review_trusts ?? [])[0]).toEqual(trust);
+  });
+
+  test("mapTrustData should not push result if no review list", () => {
+    const appData = { ...FETCH_TRUST_APPLICATION_DATA_MOCK, update: { review_trusts: undefined } };
+    const trustData = { ...FETCH_TRUST_DATA_MOCK[0] };
+
+    mapTrustData(trustData, appData);
+
+    expect(appData.update?.review_trusts).toEqual(undefined);
+  });
+
+  test("mapTrustData should not push result if no update app data", () => {
+    const appData = { ...FETCH_TRUST_APPLICATION_DATA_MOCK, update: undefined };
+    const trustData = { ...FETCH_TRUST_DATA_MOCK[0] };
+
+    mapTrustData(trustData, appData);
+
+    expect(appData.update).toEqual(undefined);
+  });
 });

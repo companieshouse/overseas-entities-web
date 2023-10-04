cad18990293301ca1baa9dda8b5229d8b89bd4dd
makhtar-ch <makhtar@companieshouse.gov.uk>
2023-10-04 16:38:05 +0100

    remove commented out code and blank spaces

--- src/controllers/beneficial.owner.statements.controller.ts
+++ src/controllers/beneficial.owner.statements.controller.ts
@@ -1,19 +1,11 @@
-// import {
-//  FEATURE_FLAG_ENABLE_REDIS_REMOVAL,
-// } from "../config";
-
 import { NextFunction, Request, Response } from "express";
 
 import { getBeneficialOwnerStatements, postBeneficialOwnerStatements } from "../utils/beneficial.owner.statements";
-// import { isActiveFeature } from "../utils/feature.flag";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
   getBeneficialOwnerStatements(req, res, next, true);
 };
 
 export const post = (req: Request, res: Response, next: NextFunction) => {
-//  let nextPageUrl = BENEFICIAL_OWNER_TYPE_URL;
-
   postBeneficialOwnerStatements(req, res, next, true);
-
 };

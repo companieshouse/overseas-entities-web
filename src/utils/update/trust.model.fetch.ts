6e41a7b88025c76e5fe20a0dfffbb7d6fb69af22
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-10-17 16:36:20 +0100

    uar-1101 use ch_references instead of trust ids for trustee fetch

--- src/utils/update/trust.model.fetch.ts
+++ src/utils/update/trust.model.fetch.ts
@@ -97,10 +97,10 @@ const fetchAndMapIndivdualTrustees = async (
   overseasEntityId: string,
   trust: Trust
 ) => {
-  const individualTrustees = await getIndividualTrustees(req, transactionId, overseasEntityId, trust.trust_id);
+  const individualTrustees = await getIndividualTrustees(req, transactionId, overseasEntityId, trust.ch_reference ?? trust.trust_id);
 
   if (individualTrustees === undefined || individualTrustees.length === 0) {
-    logger.info(`No individual trustees found for overseas entity ${overseasEntityId} and trust ${trust.trust_id}`);
+    logger.info(`No individual trustees found for overseas entity ${overseasEntityId} and trust ${trust.ch_reference ?? trust.trust_id}`);
     return;
   }
 
@@ -171,10 +171,10 @@ const fetchAndMapCorporateTrustees = async (
   overseasEntityId: string,
   trust: Trust
 ) => {
-  const corporateTrustees = await getCorporateTrustees(req, transactionId, overseasEntityId, trust.trust_id);
+  const corporateTrustees = await getCorporateTrustees(req, transactionId, overseasEntityId, trust.ch_reference ?? trust.trust_id);
 
   if (corporateTrustees === undefined || corporateTrustees.length === 0) {
-    logger.info(`No corporate trustees found for overseas entity ${overseasEntityId} and trust ${trust.trust_id}`);
+    logger.info(`No corporate trustees found for overseas entity ${overseasEntityId} and trust ${trust.ch_reference ?? trust.trust_id}`);
     return;
   }
 

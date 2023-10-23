68960182c178804a685b0aba76142d07b9b4cc5d
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-10-23 12:37:32 +0100

    roe-2397 fix for manage trusts too

--- src/utils/trusts.ts
+++ src/utils/trusts.ts
@@ -55,7 +55,7 @@ const checkEntityReviewRequiresTrusts = (appData: ApplicationData | undefined): 
 const getTrustLandingUrl = (appData: ApplicationData): string => {
   if (containsTrustData(getTrustArray(appData))) {
     // Once naviation changes are agreed the following will change
-    if (appData.entity_number !== undefined) {
+    if (appData.entity_number) {
       return UPDATE_TRUSTS_ASSOCIATED_WITH_THE_OVERSEAS_ENTITY_URL;
     } else {
       return `${TRUST_ENTRY_URL + ADD_TRUST_URL}`;

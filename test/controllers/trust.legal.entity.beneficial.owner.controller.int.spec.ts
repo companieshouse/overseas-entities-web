ab578d9970cf84210c563647d6905be16a07d6e4
jkayyala <jkayyala@companieshouse.gov.uk>
2023-11-06 14:21:46 +0000

    Fix tests

--- test/controllers/trust.legal.entity.beneficial.owner.controller.int.spec.ts
+++ test/controllers/trust.legal.entity.beneficial.owner.controller.int.spec.ts
@@ -164,14 +164,14 @@ describe("Legal entity beneficial owner integration tests", () => {
       expect(decodedHTML).not.toContain(ErrorMessages.NAME_REGISTRATION_JURISDICTION_LEGAL_ENTITY_BO);
     });
 
-  test(`renders the ${TRUST_LEGAL_ENTITY_BENEFICIAL_OWNER_PAGE} page without errors interested person date`, async () => {
+    test(`renders the ${TRUST_LEGAL_ENTITY_BENEFICIAL_OWNER_PAGE} page without errors interested person date`, async () => {
     // Arrange
-    const mockTrust = <Trust>{};
-    (getTrustByIdFromApp as jest.Mock).mockReturnValue(mockTrust);
-    legalEntityWithMissingFields.roleWithinTrust = RoleWithinTrustType.INTERESTED_PERSON;
-    legalEntityWithMissingFields.interestedPersonStartDateDay = "2";
-    legalEntityWithMissingFields.interestedPersonStartDateMonth = "8";
-    legalEntityWithMissingFields.interestedPersonStartDateYear = "2012";
+      const mockTrust = <Trust>{};
+      (getTrustByIdFromApp as jest.Mock).mockReturnValue(mockTrust);
+      legalEntityWithMissingFields.roleWithinTrust = RoleWithinTrustType.INTERESTED_PERSON;
+      legalEntityWithMissingFields.interestedPersonStartDateDay = "2";
+      legalEntityWithMissingFields.interestedPersonStartDateMonth = "8";
+      legalEntityWithMissingFields.interestedPersonStartDateYear = "2012";
 
       // Act
       const resp = await request(app).post(pageUrl).send(legalEntityWithMissingFields);
@@ -310,8 +310,8 @@ describe("Legal entity beneficial owner integration tests", () => {
       const mockTrust = <Trust>{};
       (getTrustByIdFromApp as jest.Mock).mockReturnValue(mockTrust);
       legalEntityWithMissingFields.roleWithinTrust = RoleWithinTrustType.INTERESTED_PERSON;
-      legalEntityWithMissingFields.interestedPersonStartDateDay = "02";
-      legalEntityWithMissingFields.interestedPersonStartDateMonth = "08";
+      legalEntityWithMissingFields.interestedPersonStartDateDay = "2";
+      legalEntityWithMissingFields.interestedPersonStartDateMonth = "8";
       legalEntityWithMissingFields.interestedPersonStartDateYear = "2012";
 
       // Act

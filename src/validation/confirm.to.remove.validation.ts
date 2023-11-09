37646234854f47bb863229f10423eb9e27b965d8
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-11-09 12:15:04 +0000

    uar-1283 use dynamic tile for form / static for page

--- src/validation/confirm.to.remove.validation.ts
+++ src/validation/confirm.to.remove.validation.ts
@@ -2,9 +2,9 @@ import { body } from "express-validator";
 import { DoYouWantToRemoveKey } from "../model/data.types.model";
 
 export const confirmToRemove = [
-  body(DoYouWantToRemoveKey).custom((value, { req }) => {
+  body(DoYouWantToRemoveKey).custom((value) => {
     if (value === undefined) {
-      throw new Error("Are you sure you want to remove " + req.body['boMoName'] + "?");
+      throw new Error("Are you sure you want to remove confirm has no value");
     }
 
     return true;

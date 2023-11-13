7438f2e3cbe413eb0cc728d92d5819e2be5baf64
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-11-13 13:50:06 +0000

    uar-1283 better error message

--- src/validation/confirm.to.remove.validation.ts
+++ src/validation/confirm.to.remove.validation.ts
@@ -4,7 +4,7 @@ import { DoYouWantToRemoveKey } from "../model/data.types.model";
 export const confirmToRemove = [
   body(DoYouWantToRemoveKey).custom((value) => {
     if (value === undefined) {
-      throw new Error("Are you sure you want to remove confirm has no value");
+      throw new Error("Are you sure you want to remove confirmation has no beneficial owner or managing officer");
     }
 
     return true;

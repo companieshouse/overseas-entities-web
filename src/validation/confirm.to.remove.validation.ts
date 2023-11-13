6ff0b1450285229ce7cb8039c29fc62c58d584df
AndreKramer7654 <akramer@companieshouse.gov.uk>
2023-11-13 17:14:59 +0000

    uar-1282 fix error message

--- src/validation/confirm.to.remove.validation.ts
+++ src/validation/confirm.to.remove.validation.ts
@@ -4,7 +4,7 @@ import { DoYouWantToRemoveKey } from "../model/data.types.model";
 export const confirmToRemove = [
   body(DoYouWantToRemoveKey).custom((value) => {
     if (value === undefined) {
-      throw new Error("Are you sure you want to remove beneficial owner or managing officer");
+      throw new Error("Are you sure you want to remove beneficial owner or managing officer?");
     }
 
     return true;

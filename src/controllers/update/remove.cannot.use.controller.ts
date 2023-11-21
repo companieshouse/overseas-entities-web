ea6168d4c33ea55d2b18741b8f88b56e2bf5105b
mattch1 <mculpin@companieshouse.gov.uk>
2023-11-21 08:34:32 +0000

    Backlink needs to go to remove not update sold land page

--- src/controllers/update/remove.cannot.use.controller.ts
+++ src/controllers/update/remove.cannot.use.controller.ts
@@ -3,7 +3,8 @@ import * as config from "../../config";
 
 export const get = (req: Request, res: Response) => {
   return res.render(config.REMOVE_CANNOT_USE_PAGE, {
-    backLinkUrl: config.SOLD_LAND_FILTER_URL,
+    journey: config.JourneyType.remove,
+    backLinkUrl: `${config.REMOVE_SOLD_ALL_LAND_FILTER_PAGE}${config.JOURNEY_REMOVE_QUERY_PARAM}`,
     templateName: config.CANNOT_USE_PAGE
   });
 };

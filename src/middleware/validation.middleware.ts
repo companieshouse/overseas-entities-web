f91a4308bb28fb81c1672e9e6058128fd70572ff
markpit <mpitten@companieshouse.gov.uk>
2023-11-21 10:54:37 +0000

    UAR-1161 Update remove comments in validation middleware

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -58,6 +58,7 @@ export function checkValidations(req: Request, res: Response, next: NextFunction
       }
       const entityNumber = appData?.[EntityNumberKey];
 
+      // The journey property may already be part of the page form data/body so get it from there and override it if we are on a remove journey
       // Then when we pass it back into the template, make sure it is below/after the req.body fields so it overrides the req.body value
       let journey = req.body["journey"];
       if (isRemoveJourney(req)){

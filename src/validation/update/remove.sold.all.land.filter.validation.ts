ef394684e66af7c8e0dc733f5cfe867f261d936f
mwestacott <mwestacott2@companieshouse.gov.uk>
2023-11-22 09:40:09 +0000

    UAR-1072 Change to trigger Sonar build

--- src/validation/update/remove.sold.all.land.filter.validation.ts
+++ src/validation/update/remove.sold.all.land.filter.validation.ts
@@ -3,5 +3,5 @@ import { body } from "express-validator";
 import { ErrorMessages } from "../error.messages";
 
 export const removeSoldAllLandFilter = [
-  body("disposed_all_land").not().isEmpty().withMessage(ErrorMessages.SELECT_IF_REMOVE_SOLD_ALL_LAND_FILTER),
+  body("disposed_all_land").not().isEmpty().withMessage(ErrorMessages.SELECT_IF_REMOVE_SOLD_ALL_LAND_FILTER)
 ];

49d8ea8bbf9291239044fda2e15eaddc924dfc38
dlloyd1-ch <dlloyd1@companieshouse.gov.uk>
2023-11-23 13:33:37 +0000

    Initial commit for navigating securtiy filter page for remove

--- src/controllers/update/update.use.paper.controller.ts
+++ src/controllers/update/update.use.paper.controller.ts
@@ -1,10 +1,24 @@
 import * as config from "../../config";
 import { Request, Response } from "express";
 import { logger } from "../../utils/logger";
+import { isRemoveJourney } from "../../utils/url";
 
 export const get = (req: Request, res: Response) => {
   logger.debugRequest(req, `${req.method} ${req.route.path}`);
+  // TODO needs to b a version of this for Remove journey.
   const applyWithPaperFormHeading: string = "You'll need to file an update using the paper form";
+
+  if (isRemoveJourney(req)){
+    return res.render(config.USE_PAPER_PAGE, {
+      backLinkUrl: config.SECURE_UPDATE_FILTER_URL,
+      templateName: config.USE_PAPER_PAGE,
+      applyWithPaperFormHeading,
+      journey: config.JourneyType.remove,
+      pageParams: {
+        isRegistration: false
+      }
+    });
+  }
   return res.render(config.USE_PAPER_PAGE, {
     backLinkUrl: config.SECURE_UPDATE_FILTER_URL,
     templateName: config.USE_PAPER_PAGE,

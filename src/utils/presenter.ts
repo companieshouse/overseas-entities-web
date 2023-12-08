fba3dfffd2eaa40a68ff41fffda89c312f593de0
mwestacott <mwestacott2@companieshouse.gov.uk>
2023-12-08 11:13:12 +0000

    UAR-1326 Add a new 'is_remove' field to the OE session data
    * Also sent to API
    * Transaction and OE submission created on 'presenter' screen if
    remove journey

--- src/utils/presenter.ts
+++ src/utils/presenter.ts
@@ -2,9 +2,13 @@ import { NextFunction, Request, Response } from "express";
 import { Session } from "@companieshouse/node-session-handler";
 import { ApplicationData } from "../model";
 import { PresenterKey, PresenterKeys } from "../model/presenter.model";
+import { IsRemoveKey, OverseasEntityKey, Transactionkey } from '../model/data.types.model';
 import { getApplicationData, prepareData, setApplicationData } from "./application.data";
 import { logger } from "./logger";
 import { saveAndContinue } from "./save.and.continue";
+import { isRemoveJourney } from "../utils/url";
+import { postTransaction } from "../service/transaction.service";
+import { createOverseasEntity } from "../service/overseas.entities.service";
 
 export const getPresenterPage = (req: Request, res: Response, next: NextFunction, templateName: string, backLinkUrl: string): void => {
   try {
@@ -29,6 +33,17 @@ export const postPresenterPage = async (req: Request, res: Response, next: NextF
     logger.debugRequest(req, `${req.method} ${req.route.path}`);
 
     const session = req.session as Session;
+
+    if (isRemoveJourney(req)) {
+      const appData: ApplicationData = getApplicationData(session);
+      if (!appData[Transactionkey]) {
+        const transactionID = await postTransaction(req, session);
+        appData[Transactionkey] = transactionID;
+        appData[IsRemoveKey] = true;
+        appData[OverseasEntityKey] = await createOverseasEntity(req, session, transactionID, true);
+      }
+    }
+
     const data = prepareData(req.body, PresenterKeys);
     setApplicationData(session, data, PresenterKey);
 

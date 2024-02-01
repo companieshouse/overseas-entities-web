b5ab02593733358ad3dbcffe63673389d633a38f
mwestacott <mwestacott2@companieshouse.gov.uk>
2024-02-01 14:20:42 +0000

    UAR-1288 Show correct confirmation details for a Remove journey
    * Delivered and working for the 'no change' journey
    * Controller code changes should ensure only template changes
    are needed for the 'change' journey tickets
    * Tests added and updated where appropriate

--- src/controllers/update/confirmation.controller.ts
+++ src/controllers/update/confirmation.controller.ts
@@ -3,12 +3,14 @@ import { NextFunction, Request, Response } from "express";
 import { logger } from "../../utils/logger";
 import {
   CONFIRMATION_PAGE,
+  JourneyType
 } from "../../config";
 import { getLoggedInUserEmail } from "../../utils/session";
 import { deleteApplicationData, getApplicationData } from "../../utils/application.data";
 import { ApplicationData } from "../../model/application.model";
 import { Transactionkey } from "../../model/data.types.model";
 import { WhoIsRegisteringType } from "../../model/who.is.making.filing.model";
+import { isRemoveJourney } from "../../utils/url";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -17,7 +19,23 @@ export const get = (req: Request, res: Response, next: NextFunction) => {
     const appData: ApplicationData = getApplicationData(req.session);
     const referenceNumber = appData[Transactionkey];
 
+    // It's necessary to do this check (and save the result) before deleting the application data
+    const isRemove: boolean = isRemoveJourney(req);
+
     deleteApplicationData(req.session);
+
+    if (isRemove) {
+      return res.render(CONFIRMATION_PAGE, {
+        journey: JourneyType.remove,
+        isAgentRegistering: appData.who_is_registering === WhoIsRegisteringType.AGENT,
+        referenceNumber,
+        userEmail: getLoggedInUserEmail(req.session),
+        verificationCheckDays: 14,
+        isUpdate: true,
+        noChange: appData.update?.no_change,
+        templateName: CONFIRMATION_PAGE
+      });
+    }
 
     return res.render(CONFIRMATION_PAGE, {
       isAgentRegistering: appData.who_is_registering === WhoIsRegisteringType.AGENT,

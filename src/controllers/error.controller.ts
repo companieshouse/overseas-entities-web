8af30053078f2e5f70a446c0a4acb81550b8b0e2
makhtar-ch <makhtar@companieshouse.gov.uk>
2024-06-11 11:12:32 +0100

    uar-1479 csrf error handling and tests

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -1,12 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 import { logger } from "../utils/logger";
 import * as config from "../config";
+import { CsrfError } from '@companieshouse/web-security-node';
 
 const pageNotFound = (req: Request, res: Response) => {
   const isRegistration: boolean = req.path.startsWith(config.LANDING_URL);
 
   return res.status(404).render(config.NOT_FOUND_PAGE, {
     isRegistration
+  });
+};
+
+const csrfErrorHandler = (err: CsrfError | Error, req: Request, res: Response, next: NextFunction) => {
+  const isRegistration: boolean = req.path.startsWith(config.LANDING_URL);
+  // Handle non-CSRF Errors immediately
+  if (!(err instanceof CsrfError)) {
+    return next(err);
+  }
+
+  return res.status(403).render(config.ERROR_PAGE, {
+    templateName: config.ERROR_PAGE,
+    isRegistration,
+    csrfErrors: true
   });
 };
 
@@ -26,4 +41,4 @@ const errorHandler = (err: Error, req: Request, res: Response, _next: NextFuncti
   });
 };
 
-export default [pageNotFound, errorHandler];
+export default [pageNotFound, csrfErrorHandler, errorHandler];

a14d49b75629fafdc162d8e2af6396fe5ba9832e
ndeekonda-ch <ndeekonda@companieshouse.gov.uk>
2024-06-17 14:16:28 +0100

    feature/ROECCT-136-web-validation-filter-page:refractor the code

--- src/validation/relevant.period.owned.land.validation.ts
+++ src/validation/relevant.period.owned.land.validation.ts
@@ -1,7 +1,6 @@
 import { body } from "express-validator";
-import { OwnedLandKey } from "../model/relevant.period.owned.land.filter.model";
 import { ErrorMessages } from "./error.messages";
 
 export const relevantPeriodOwnedLandFilter = [
-  body(OwnedLandKey).not().isEmpty().withMessage(ErrorMessages.SELECT_RELEVANT_OWNED_LAND_FILTER),
+  body("owned_land_relevant_period").not().isEmpty().withMessage(ErrorMessages.SELECT_RELEVANT_OWNED_LAND_FILTER),
 ];

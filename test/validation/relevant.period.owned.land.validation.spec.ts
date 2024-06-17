2257b8d54aac34b909b38b15b83048421618ec14
ndeekonda-ch <ndeekonda@companieshouse.gov.uk>
2024-06-17 14:28:07 +0100

    feature/ROECCT-136-web-validation-filter-page:refractor the code

--- test/validation/relevant.period.owned.land.validation.spec.ts
+++ test/validation/relevant.period.owned.land.validation.spec.ts
@@ -1,19 +1,19 @@
 
-// const mockNot = jest.fn();
-// const mockIsEmpty = jest.fn();
-//
-// jest.mock('express-validator', () => ({
-//   body: jest.fn().mockImplementation(() => ({
-//     not: mockNot.mockReturnThis(),
-//     isEmpty: mockIsEmpty.mockReturnThis()
-//   })),
-// }));
-//
+const mockNot = jest.fn();
+const mockIsEmpty = jest.fn();
+
+jest.mock('express-validator', () => ({
+  body: jest.fn().mockImplementation(() => ({
+    not: mockNot.mockReturnThis(),
+    isEmpty: mockIsEmpty.mockReturnThis()
+  })),
+}));
+
 // import { OwnedLandKey } from "../../src/model/relevant.period.owned.land.filter.model";
 // import * as ownedLandHelper from "../../src/validation/relevant.period.owned.land.validation";
 //
 // const ownedLandKey = OwnedLandKey;
-
+//
 // describe('Overseas entity was registered owner of UK land during relevant period page validation test', () => {
 //   // Arrange
 //   afterAll(() => {

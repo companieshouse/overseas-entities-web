dd6a97a32a1346e643b07de1cb27ff328ac0f26a
ConnorOS-ch <coshaughnessy@companieshouse.gov.uk>
2024-06-19 23:44:44 +0100

    modify page template based on relevant-period query param
    add unit tests

--- src/model/beneficial.owner.type.model.ts
+++ src/model/beneficial.owner.type.model.ts
@@ -3,7 +3,8 @@ export const BeneficialOwnerTypeKey = "beneficial_owner_type";
 export enum BeneficialOwnerTypeChoice {
     individual = "individualOwner",
     otherLegal = "otherLegalOwner",
-    government = "governmentOrPublicOwner"
+    government = "governmentOrPublicOwner",
+    relevantPeriodIndividual = "relevantPeriodIndividualOwner",
 }
 
 export enum ManagingOfficerTypeChoice {

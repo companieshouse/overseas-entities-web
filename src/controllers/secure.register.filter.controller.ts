ce66ccdc47a76b4a85107d069c5f68c70c5cb628
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-06-21 08:59:30 +0100

    feat: refactor nextPageUrl and backLinkUrl for secure-register screen

--- src/controllers/secure.register.filter.controller.ts
+++ src/controllers/secure.register.filter.controller.ts
@@ -2,9 +2,15 @@ import { Request, Response, NextFunction } from "express";
 
 import * as config from "../config";
 import { getFilterPage, postFilterPage } from "../utils/secure.filter";
+import { isActiveFeature } from "../utils/feature.flag";
+import { getUrlWithParamsToPath } from "../utils/url";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
-  getFilterPage(req, res, next, config.SECURE_REGISTER_FILTER_PAGE, config.SOLD_LAND_FILTER_URL);
+  let backLinkUrl: string = config.SOLD_LAND_FILTER_URL;
+  if (isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL)) {
+    backLinkUrl = getUrlWithParamsToPath(config.SOLD_LAND_FILTER_WITH_PARAMS_URL, req);
+  }
+  getFilterPage(req, res, next, config.SECURE_REGISTER_FILTER_PAGE, backLinkUrl);
 };
 
 export const post = (req: Request, res: Response, next: NextFunction) => {

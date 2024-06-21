31078829b57ad6322133e72ab94fa665910eb0a4
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-06-21 08:57:12 +0100

    feat: refactor nextPageUrl and backLinkUrl in interrupt controller

--- src/controllers/interrupt.card.controller.ts
+++ src/controllers/interrupt.card.controller.ts
@@ -8,18 +8,20 @@ import { getUrlWithParamsToPath, transactionIdAndSubmissionIdExistInRequest } fr
 export const get = (req: Request, res: Response) => {
   logger.debugRequest(req, `GET ${config.INTERRUPT_CARD_PAGE}`);
 
-  let nextPageUrl = config.OVERSEAS_NAME_URL;
+  let nextPageUrl: string = config.OVERSEAS_NAME_URL;
+  let backLinkUrl: string = config.SECURE_REGISTER_FILTER_URL;
 
   if (isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL) && transactionIdAndSubmissionIdExistInRequest(req)){
     nextPageUrl = getUrlWithParamsToPath(config.OVERSEAS_NAME_WITH_PARAMS_URL, req);
+    backLinkUrl = getUrlWithParamsToPath(config.SECURE_REGISTER_FILTER_WITH_PARAMS_URL, req);
   }
 
   return res.render(config.INTERRUPT_CARD_PAGE, {
-    backLinkUrl: config.SECURE_REGISTER_FILTER_URL,
+    backLinkUrl,
     templateName: config.INTERRUPT_CARD_PAGE,
     pageParams: {
-      isTrustFeatureEnabled: isActiveFeature(config.FEATURE_FLAG_ENABLE_TRUSTS_WEB),
-      nextPageUrl: nextPageUrl,
+      nextPageUrl,
+      isTrustFeatureEnabled: isActiveFeature(config.FEATURE_FLAG_ENABLE_TRUSTS_WEB)
     },
   });
 };

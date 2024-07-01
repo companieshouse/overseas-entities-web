c0110e5cacb5b71cdd57d1459ae837da93c5bdec
ConnorOS-ch <coshaughnessy@companieshouse.gov.uk>
2024-07-01 11:28:40 +0100

    use relevant-period constant

--- src/controllers/update/beneficial.owner.type.controller.ts
+++ src/controllers/update/beneficial.owner.type.controller.ts
@@ -122,7 +122,7 @@ const getNextPage = (beneficialOwnerTypeChoices: BeneficialOwnerTypeChoice | Man
       case ManagingOfficerTypeChoice.individual:
         return config.UPDATE_MANAGING_OFFICER_URL;
       case BeneficialOwnerTypeChoice.relevantPeriodIndividual:
-        return config.UPDATE_BENEFICIAL_OWNER_INDIVIDUAL_URL + "?relevant-period=true";
+        return config.UPDATE_BENEFICIAL_OWNER_INDIVIDUAL_URL + config.RELEVANT_PERIOD_QUERY_PARAM;
       default:
         return config.UPDATE_BENEFICIAL_OWNER_INDIVIDUAL_URL;
   }

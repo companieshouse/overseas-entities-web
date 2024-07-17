9f2717af6e166377222ab620e34b6baffe983bb4
ndeekonda-ch <ndeekonda@companieshouse.gov.uk>
2024-07-17 17:08:17 +0100

    ROECCT-173-amend-15Jul

--- src/controllers/update/update.beneficial.owner.other.controller.ts
+++ src/controllers/update/update.beneficial.owner.other.controller.ts
@@ -1,11 +1,14 @@
 import { NextFunction, Request, Response } from "express";
-import { UPDATE_BENEFICIAL_OWNER_OTHER_PAGE, UPDATE_BENEFICIAL_OWNER_TYPE_URL } from "../../config";
+import { UPDATE_BENEFICIAL_OWNER_OTHER_PAGE, UPDATE_BENEFICIAL_OWNER_TYPE_URL, RELEVANT_PERIOD_QUERY_PARAM } from "../../config";
 import {
   getBeneficialOwnerOther,
   getBeneficialOwnerOtherById,
   postBeneficialOwnerOther,
   updateBeneficialOwnerOther
 } from "../../utils/beneficial.owner.other";
+import { checkRelevantPeriod } from "../../utils/relevant.period";
+import { ApplicationData } from "../../model";
+import { getApplicationData } from "../../utils/application.data";
 
 export const get = (req: Request, res: Response) => {
   getBeneficialOwnerOther(req, res, UPDATE_BENEFICIAL_OWNER_OTHER_PAGE, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
@@ -16,7 +19,12 @@ export const getById = (req: Request, res: Response, next: NextFunction) => {
 };
 
 export const post = (req: Request, res: Response, next: NextFunction) => {
-  postBeneficialOwnerOther(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL, false);
+  const appData: ApplicationData = getApplicationData(req.session);
+  if (checkRelevantPeriod(appData)) {
+    postBeneficialOwnerOther(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL + RELEVANT_PERIOD_QUERY_PARAM, false);
+  } else {
+    postBeneficialOwnerOther(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL, false);
+  }
 };
 
 export const update = (req: Request, res: Response, next: NextFunction) => {

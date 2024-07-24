7c12bc976249c014a7e3089934a1c4a771e7ea14
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-07-24 15:19:51 +0100

    chore: add @todo decorator to comment

--- src/controllers/secure.register.filter.controller.ts
+++ src/controllers/secure.register.filter.controller.ts
@@ -8,7 +8,7 @@ export const get = (req: Request, res: Response, next: NextFunction) => {
   getFilterPage(req, res, next, config.SECURE_REGISTER_FILTER_PAGE, config.SOLD_LAND_FILTER_URL);
 };
 
-// remember ro remove url parameters after update journey is updated for REDIS removal
+// @todo: remember to remove url parameters after update journey is updated for REDIS removal
 export const post = async (req: Request, res: Response, next: NextFunction) => {
   const nextPageUrl = isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL)
     ? config.INTERRUPT_CARD_WITH_PARAMS_URL

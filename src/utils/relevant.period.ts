3b7a7f190da650dee2b94292f707459eb56607b1
ConnorOS-ch <coshaughnessy@companieshouse.gov.uk>
2024-07-31 10:50:02 +0100

    remove uncovered lines

--- src/utils/relevant.period.ts
+++ src/utils/relevant.period.ts
@@ -4,8 +4,3 @@ export const checkRelevantPeriod = (appData: ApplicationData): boolean =>
   appData.update?.change_bo_relevant_period === "CHANGE_BO_RELEVANT_PERIOD" ||
   appData.update?.trustee_involved_relevant_period === "TRUSTEE_INVOLVED_RELEVANT_PERIOD" ||
   appData.update?.change_beneficiary_relevant_period === "CHANGE_BENEFICIARY_RELEVANT_PERIOD";
-
-export const checkRPStatementsExist = (appData: ApplicationData): boolean =>
-  appData.update?.change_bo_relevant_period !== undefined ||
-  appData.update?.trustee_involved_relevant_period !== undefined ||
-  appData.update?.change_beneficiary_relevant_period !== undefined ;

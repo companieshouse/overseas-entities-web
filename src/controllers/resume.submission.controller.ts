8e17a6f5fc161b5ee7985cd06d8a3bbf19f3b921
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-08-07 09:02:04 +0100

    feat: resume url with transactionId and submissionId

--- src/controllers/resume.submission.controller.ts
+++ src/controllers/resume.submission.controller.ts
@@ -1,7 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import * as config from "../config";
+import { isActiveFeature } from "../utils/feature.flag";
 import { getResumePage } from "./shared/common.resume.submission.controller";
+import { getUrlWithTransactionIdAndSubmissionId } from "../utils/url";
 
-export const get = (req: Request, res: Response, next: NextFunction) => {
-  getResumePage(req, res, next, config.SOLD_LAND_FILTER_URL);
+export const get = async (req: Request, res: Response, next: NextFunction) => {
+  const { transactionId, overseaEntityId } = req.params;
+  const resumeUrl: string = isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL)
+    ? getUrlWithTransactionIdAndSubmissionId(config.SOLD_LAND_FILTER_WITH_PARAMS_URL, transactionId, overseaEntityId)
+    : config.SOLD_LAND_FILTER_URL;
+  await getResumePage(req, res, next, resumeUrl);
 };

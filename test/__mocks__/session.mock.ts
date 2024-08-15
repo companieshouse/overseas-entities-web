52f816968ab0bb6f28e28050172569b778564e37
ConnorOS-ch <coshaughnessy@companieshouse.gov.uk>
2024-08-15 13:23:32 +0100

    fix unit tests

--- test/__mocks__/session.mock.ts
+++ test/__mocks__/session.mock.ts
@@ -1459,6 +1459,7 @@ export const UPDATE_OBJECT_MOCK_REVIEW_BO_OTHER_MODEL: updateType.Update = {
 
 export const UPDATE_OBJECT_MOCK_RELEVANT_PERIOD_CHANGE: updateType.Update = {
   ...UPDATE_OBJECT_MOCK,
+  owned_land_relevant_period: true,
   change_bo_relevant_period: relevantPeriodType.ChangeBoRelevantPeriodType.YES,
   trustee_involved_relevant_period: relevantPeriodType.TrusteeInvolvedRelevantPeriodType.YES,
   change_beneficiary_relevant_period: relevantPeriodType.ChangeBeneficiaryRelevantPeriodType.YES
@@ -1466,6 +1467,7 @@ export const UPDATE_OBJECT_MOCK_RELEVANT_PERIOD_CHANGE: updateType.Update = {
 
 export const UPDATE_OBJECT_MOCK_RELEVANT_PERIOD_NO_CHANGE: updateType.Update = {
   ...UPDATE_OBJECT_MOCK,
+  owned_land_relevant_period: false,
   change_bo_relevant_period: relevantPeriodType.ChangeBoRelevantPeriodType.NO,
   trustee_involved_relevant_period: relevantPeriodType.TrusteeInvolvedRelevantPeriodType.NO,
   change_beneficiary_relevant_period: relevantPeriodType.ChangeBeneficiaryRelevantPeriodType.NO

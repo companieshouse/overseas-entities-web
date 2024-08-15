187c28fd389d7003f840996f6cd53754af27f37b
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-08-15 16:00:40 +0100

    feat: fix backLink for interrupt page

--- src/controllers/interrupt.card.controller.ts
+++ src/controllers/interrupt.card.controller.ts
@@ -3,23 +3,25 @@ import { Request, Response } from "express";
 import { logger } from "../utils/logger";
 import * as config from "../config";
 import { isActiveFeature } from "../utils/feature.flag";
-import { getUrlWithParamsToPath, transactionIdAndSubmissionIdExistInRequest } from "../utils/url";
+import { getUrlWithParamsToPath } from "../utils/url";
 
 export const get = (req: Request, res: Response) => {
   logger.debugRequest(req, `GET ${config.INTERRUPT_CARD_PAGE}`);
 
   let nextPageUrl = config.OVERSEAS_NAME_URL;
+  let backLinkUrl = config.SECURE_REGISTER_FILTER_URL;
 
-  if (isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL) && transactionIdAndSubmissionIdExistInRequest(req)){
+  if (isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL)){
     nextPageUrl = getUrlWithParamsToPath(config.OVERSEAS_NAME_WITH_PARAMS_URL, req);
+    backLinkUrl = getUrlWithParamsToPath(config.SECURE_REGISTER_FILTER_WITH_PARAMS_URL, req);
   }
 
   return res.render(config.INTERRUPT_CARD_PAGE, {
-    backLinkUrl: config.SECURE_REGISTER_FILTER_URL,
+    backLinkUrl,
     templateName: config.INTERRUPT_CARD_PAGE,
     pageParams: {
-      isTrustFeatureEnabled: isActiveFeature(config.FEATURE_FLAG_ENABLE_TRUSTS_WEB),
-      nextPageUrl: nextPageUrl,
+      nextPageUrl,
+      isTrustFeatureEnabled: isActiveFeature(config.FEATURE_FLAG_ENABLE_TRUSTS_WEB)
     },
   });
 };

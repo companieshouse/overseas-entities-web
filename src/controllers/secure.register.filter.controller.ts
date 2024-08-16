838c10001189fb8dacc35ad0e4830fa1df89463a
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-08-16 11:14:44 +0100

    feat: add ids to backLinkUrl for secure-register-filter

--- src/controllers/secure.register.filter.controller.ts
+++ src/controllers/secure.register.filter.controller.ts
@@ -2,10 +2,14 @@ import { Request, Response, NextFunction } from "express";
 
 import * as config from "../config";
 import { getFilterPage, postFilterPage } from "../utils/secure.filter";
+import { getUrlWithParamsToPath } from "../utils/url";
 import { isActiveFeature } from "../utils/feature.flag";
 
 export const get = (req: Request, res: Response, next: NextFunction) => {
-  getFilterPage(req, res, next, config.SECURE_REGISTER_FILTER_PAGE, config.SOLD_LAND_FILTER_URL);
+  const backLinkUrl = isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL)
+    ? getUrlWithParamsToPath(config.SOLD_LAND_FILTER_WITH_PARAMS_URL, req)
+    : config.SOLD_LAND_FILTER_URL;
+  getFilterPage(req, res, next, config.SECURE_REGISTER_FILTER_PAGE, backLinkUrl);
 };
 
 // @todo: remember to remove url parameters after update journey is updated for REDIS removal

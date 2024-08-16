f038f1bb16e121b6083c8515d5231b373fd9dd3d
ConnorOS-ch <coshaughnessy@companieshouse.gov.uk>
2024-08-16 10:23:23 +0100

    fix is in change jorney middleware

--- src/middleware/navigation/update/is.in.change.journey.middleware.ts
+++ src/middleware/navigation/update/is.in.change.journey.middleware.ts
@@ -4,11 +4,17 @@ import { logger } from '../../../utils/logger';
 import { SECURE_UPDATE_FILTER_URL } from '../../../config';
 import { getApplicationData } from "../../../utils/application.data";
 import { NavigationErrorMessage } from '../check.condition';
+import { isActiveFeature } from "../../../utils/feature.flag";
+import * as config from "../../../config";
+import { checkRelevantPeriod } from "../../../utils/relevant.period";
 
 export const isInChangeJourney = (req: Request, res: Response, next: NextFunction): void => {
   try {
     const appData = getApplicationData(req.session);
-    const inChangeJourney = !appData.update?.no_change;
+
+    const relevantPeriodNoChange = isActiveFeature(config.FEATURE_FLAG_ENABLE_RELEVANT_PERIOD) ? !checkRelevantPeriod(appData) : true;
+
+    const inChangeJourney = !appData.update?.no_change || !relevantPeriodNoChange;
 
     if (inChangeJourney) {
       return next();

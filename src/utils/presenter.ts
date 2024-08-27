9b0a536b8076ba64e1ce46221cfc99781776c9a2
lduranteau <lduranteau@companieshouse.gov.uk>
2024-08-27 12:00:22 +0100

    asm-350 - refactor presenter.controller

--- src/utils/presenter.ts
+++ src/utils/presenter.ts
@@ -11,11 +11,11 @@ import * as config from "../config";
 import { postTransaction } from "../service/transaction.service";
 import { createOverseasEntity } from "../service/overseas.entities.service";
 
-export const getPresenterPage = (req: Request, res: Response, next: NextFunction, templateName: string, backLinkUrl: string): void => {
+export const getPresenterPage = async (req: Request, res: Response, next: NextFunction, templateName: string, backLinkUrl: string): Promise<void> => {
   try {
     logger.debugRequest(req, `${req.method} ${req.route.path}`);
 
-    const appData: ApplicationData = getApplicationData(req.session);
+    const appData: ApplicationData = await getApplicationData(req.session);
     const presenter = appData[PresenterKey];
 
     if (isRemoveJourney(req)){
@@ -45,7 +45,7 @@ export const postPresenterPage = async (req: Request, res: Response, next: NextF
     const session = req.session as Session;
 
     if (isRemoveJourney(req)) {
-      const appData: ApplicationData = getApplicationData(session);
+      const appData: ApplicationData = await getApplicationData(session);
       if (!appData[Transactionkey]) {
         const transactionID = await postTransaction(req, session);
         appData[Transactionkey] = transactionID;
@@ -56,7 +56,7 @@ export const postPresenterPage = async (req: Request, res: Response, next: NextF
     }
 
     const data = prepareData(req.body, PresenterKeys);
-    setApplicationData(session, data, PresenterKey);
+    await setApplicationData(session, data, PresenterKey);
 
     await saveAndContinue(req, session);
 

769e3f85525cf68ea00d5d673a965d02a3ced03c
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-08-27 16:16:21 +0100

    asm-350 - refactor update/due.diligence.controller.ts

--- src/controllers/update/due.diligence.controller.ts
+++ src/controllers/update/due.diligence.controller.ts
@@ -8,10 +8,10 @@ import {
 
 import { getDueDiligencePage, postDueDiligencePage } from "../../utils/due.diligence";
 
-export const get = (req: Request, res: Response, next: NextFunction) => {
-  getDueDiligencePage(req, res, next, UPDATE_DUE_DILIGENCE_PAGE, WHO_IS_MAKING_UPDATE_URL);
+export const get = async (req: Request, res: Response, next: NextFunction) => {
+  await getDueDiligencePage(req, res, next, UPDATE_DUE_DILIGENCE_PAGE, WHO_IS_MAKING_UPDATE_URL);
 };
 
-export const post = (req: Request, res: Response, next: NextFunction) => {
-  postDueDiligencePage(req, res, next, UPDATE_REVIEW_OVERSEAS_ENTITY_INFORMATION_URL);
+export const post = async (req: Request, res: Response, next: NextFunction) => {
+  await postDueDiligencePage(req, res, next, UPDATE_REVIEW_OVERSEAS_ENTITY_INFORMATION_URL);
 };

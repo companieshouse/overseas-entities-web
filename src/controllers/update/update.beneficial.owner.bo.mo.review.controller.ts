44dd83b6e008cfd539aad3602cdac7396b3b882a
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-08-27 16:36:55 +0100

    asm-350 - refactor update controllers for beneficial owner

--- src/controllers/update/update.beneficial.owner.bo.mo.review.controller.ts
+++ src/controllers/update/update.beneficial.owner.bo.mo.review.controller.ts
@@ -6,14 +6,14 @@ import { ApplicationData } from "../../model";
 import { getApplicationData } from "../../utils/application.data";
 import { checkRelevantPeriod } from "../../utils/relevant.period";
 
-export const get = (req: Request, res: Response, next: NextFunction) => {
+export const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
     logger.debugRequest(req, `${req.method} ${req.route.path}`);
 
-    const appData: ApplicationData = getApplicationData(req.session);
+    const appData: ApplicationData = await getApplicationData(req.session);
     const backLinkUrl = config.OVERSEAS_ENTITY_UPDATE_DETAILS_URL;
     return res.render(config.UPDATE_BENEFICIAL_OWNER_BO_MO_REVIEW_PAGE, {
-      backLinkUrl: backLinkUrl,
+      backLinkUrl,
       templateName: config.UPDATE_BENEFICIAL_OWNER_BO_MO_REVIEW_PAGE,
       ...appData
     });
@@ -23,10 +23,10 @@ export const get = (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export const post = (req: Request, res: Response, next: NextFunction) => {
+export const post = async (req: Request, res: Response, next: NextFunction) => {
   try {
     logger.debugRequest(req, `${req.method} ${req.route.path}`);
-    const appData: ApplicationData = getApplicationData(req.session);
+    const appData: ApplicationData = await getApplicationData(req.session);
     if (checkRelevantPeriod(appData)) {
       return res.redirect(config.UPDATE_BENEFICIAL_OWNER_TYPE_URL + config.RELEVANT_PERIOD_QUERY_PARAM);
     } else {
--- src/controllers/update/update.beneficial.owner.gov.controller.ts
+++ src/controllers/update/update.beneficial.owner.gov.controller.ts
@@ -14,24 +14,24 @@ export const get = async (req: Request, res: Response) => {
   return await getBeneficialOwnerGov(req, res, UPDATE_BENEFICIAL_OWNER_GOV_PAGE, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
 };
 
-export const getById = (req: Request, res: Response, next: NextFunction) => {
-  return getBeneficialOwnerGovById(req, res, next, UPDATE_BENEFICIAL_OWNER_GOV_PAGE, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
+export const getById = async (req: Request, res: Response, next: NextFunction) => {
+  return await getBeneficialOwnerGovById(req, res, next, UPDATE_BENEFICIAL_OWNER_GOV_PAGE, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
 };
 
-export const post = (req: Request, res: Response, next: NextFunction) => {
-  const appData: ApplicationData = getApplicationData(req.session);
+export const post = async (req: Request, res: Response, next: NextFunction) => {
+  const appData: ApplicationData = await getApplicationData(req.session);
   if (checkRelevantPeriod(appData)) {
-    postBeneficialOwnerGov(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL + RELEVANT_PERIOD_QUERY_PARAM);
+    await postBeneficialOwnerGov(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL + RELEVANT_PERIOD_QUERY_PARAM);
   } else {
-    postBeneficialOwnerGov(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
+    await postBeneficialOwnerGov(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
   }
 };
 
-export const update = (req: Request, res: Response, next: NextFunction) => {
-  const appData: ApplicationData = getApplicationData(req.session);
+export const update = async (req: Request, res: Response, next: NextFunction) => {
+  const appData: ApplicationData = await getApplicationData(req.session);
   if (checkRelevantPeriod(appData)) {
-    updateBeneficialOwnerGov(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL + RELEVANT_PERIOD_QUERY_PARAM);
+    await updateBeneficialOwnerGov(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL + RELEVANT_PERIOD_QUERY_PARAM);
   } else {
-    updateBeneficialOwnerGov(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
+    await updateBeneficialOwnerGov(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
   }
 };

82e2a77a80e648d5f154e3f350673d90b2590c20
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-08-27 16:20:18 +0100

    asm-350 - refactor update/overseas.entity.presenter.controller.ts

--- src/controllers/update/overseas.entity.presenter.controller.ts
+++ src/controllers/update/overseas.entity.presenter.controller.ts
@@ -2,10 +2,10 @@ import * as config from "../../config";
 import { NextFunction, Request, Response } from "express";
 import { getPresenterPage, postPresenterPage } from "../../utils/presenter";
 
-export const get = (req: Request, res: Response, next: NextFunction) => {
-  getPresenterPage(req, res, next, config.UPDATE_PRESENTER_PAGE, config.UPDATE_FILING_DATE_URL);
+export const get = async (req: Request, res: Response, next: NextFunction) => {
+  await getPresenterPage(req, res, next, config.UPDATE_PRESENTER_PAGE, config.UPDATE_FILING_DATE_URL);
 };
 
-export const post = (req: Request, res: Response, next: NextFunction) => {
-  postPresenterPage(req, res, next, config.UPDATE_DO_YOU_WANT_TO_MAKE_OE_CHANGE_URL);
+export const post = async (req: Request, res: Response, next: NextFunction) => {
+  await postPresenterPage(req, res, next, config.UPDATE_DO_YOU_WANT_TO_MAKE_OE_CHANGE_URL);
 };

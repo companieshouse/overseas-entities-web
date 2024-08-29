52bd6683cf96350c3d0a78ef5d6f4c018756db8f
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-08-29 14:49:36 +0100

    asm-350 - refactor update/update.overseas.entity.no.change.registrable.beneficial.owner.controller.ts

--- src/controllers/update/update.overseas.entity.no.change.registrable.beneficial.owner.controller.ts
+++ src/controllers/update/update.overseas.entity.no.change.registrable.beneficial.owner.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import { getRegistrableBeneficialOwner, postRegistrableBeneficialOwner } from '../../utils/registrable.beneficial.owner';
 
-export const get = (req: Request, res: Response, next: NextFunction) => {
-  getRegistrableBeneficialOwner(req, res, next, true);
+export const get = async (req: Request, res: Response, next: NextFunction) => {
+  await getRegistrableBeneficialOwner(req, res, next, true);
 };
 
-export const post = (req: Request, res: Response, next: NextFunction) => {
-  postRegistrableBeneficialOwner(req, res, next, true);
+export const post = async (req: Request, res: Response, next: NextFunction) => {
+  await postRegistrableBeneficialOwner(req, res, next, true);
 };

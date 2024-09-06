50c60021f763d641bad50f762eac2532da4ed6ef
markpit <mpitten@companieshouse.gov.uk>
2024-09-06 15:52:09 +0100

    UAR-1584 Update mock data

--- test/__mocks__/session.mock.ts
+++ test/__mocks__/session.mock.ts
@@ -568,6 +568,7 @@ export const UPDATE_BENEFICIAL_OWNER_INDIVIDUAL_OBJECT_MOCK: beneficialOwnerIndi
   trustees_nature_of_control_types: [NatureOfControlType.OVER_25_PERCENT_OF_VOTING_RIGHTS],
   trust_control_nature_of_control_types: [NatureOfControlType.OVER_25_PERCENT_OF_SHARES],
   non_legal_firm_members_nature_of_control_types: [NatureOfControlType.APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS],
+  non_legal_firm_control_nature_of_control_types: [NatureOfControlType.APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS],
   owner_of_land_person_nature_of_control_jurisdictions: [NatureOfControlJurisdiction.ENGLAND_AND_WALES],
   owner_of_land_other_entity_nature_of_control_jurisdictions: [NatureOfControlJurisdiction.SCOTLAND],
   is_on_sanctions_list: 1,

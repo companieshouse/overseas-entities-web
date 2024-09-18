8a55d7df905edc1d70c24a6d39c775bcc01e60af
jnicholson-ch <jnicholson@companieshouse.gov.uk>
2024-09-18 12:23:59 +0100

    ROECCT-306: Fix update information page displaying incorrect message when update data is null

--- src/utils/relevant.period.ts
+++ src/utils/relevant.period.ts
@@ -6,6 +6,6 @@ export const checkRelevantPeriod = (appData: ApplicationData): boolean =>
   appData.update?.change_beneficiary_relevant_period === "CHANGE_BENEFICIARY_RELEVANT_PERIOD";
 
 export const checkRPStatementsExist = (appData: ApplicationData): boolean =>
-  appData.update?.change_bo_relevant_period !== undefined ||
-  appData.update?.trustee_involved_relevant_period !== undefined ||
-  appData.update?.change_beneficiary_relevant_period !== undefined ;
+  !!appData.update?.change_bo_relevant_period ||
+  !!appData.update?.trustee_involved_relevant_period ||
+  !!appData.update?.change_beneficiary_relevant_period ;

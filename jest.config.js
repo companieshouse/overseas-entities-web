a2a799312563f5babaf2d580191bf6efd58e38b2
ndeekonda-ch <ndeekonda@companieshouse.gov.uk>
2024-09-18 15:23:30 +0100

    feature/ROECCT-173-bugfix-amend-urls-latest-branch

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   preset: "ts-jest",
   testEnvironment: "node",
-  testTimeout: 30000,
+  testTimeout: 37000,
   verbose: true,
   testMatch: ["**/test/**/*.spec.[jt]s"],
   globals: {

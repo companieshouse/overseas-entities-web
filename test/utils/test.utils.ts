5cbf04a3f060b76bfaf45d41417ab60ad2fb10d3
markpit <mpitten@companieshouse.gov.uk>
2024-09-19 09:53:22 +0100

    ROE-2610  regex test util change

--- test/utils/test.utils.ts
+++ test/utils/test.utils.ts
@@ -1,5 +1,5 @@
 export function wordCount(wordToCount: string, inText: string): number {
-  const regex = new RegExp(`${wordToCount}`, 'gm');
+  const regex = new RegExp(`${wordToCount}`, 'gi');
   const matches = inText.match(regex);
   const wordCount = matches ? matches.length : 0;
   return wordCount;

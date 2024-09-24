fb9191b482681bd027610bbab14285b93e8c35bc
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-09-24 10:01:06 +0100

    refactor: get ids from originalUrl from new method

--- src/middleware/navigation/sign.out.base.url.ts
+++ src/middleware/navigation/sign.out.base.url.ts
@@ -7,18 +7,23 @@ import * as config from "../../config";
 import { logger } from "../../utils/logger";
 import {
   isRegistrationJourney,
-  getUrlWithParamsToPath,
-  transactionIdAndSubmissionIdExistInRequest
+  getTransactionIdAndSubmissionIdFromOriginalUrl,
+  getUrlWithTransactionIdAndSubmissionId
 } from "../../utils/url";
 
 export const generateSignOutBaseUrl = (req: Request, res: Response, next: NextFunction) => {
-  logger.info("setting sign-our-base-url");
+  logger.info("setting sign-out-base-url");
   let signOutBaseUrl: string = config.UPDATE_AN_OVERSEAS_ENTITY_URL;
   if (isRegistrationJourney(req)) {
-    if (!transactionIdAndSubmissionIdExistInRequest(req)) {
+    const transactionIdAndSubmissionId = getTransactionIdAndSubmissionIdFromOriginalUrl(req);
+    if (!transactionIdAndSubmissionId) {
       signOutBaseUrl = config.REGISTER_AN_OVERSEAS_ENTITY_URL;
     } else {
-      signOutBaseUrl = getUrlWithParamsToPath(config.REGISTER_AN_OVERSEAS_ENTITY_WITH_PARAMS_URL, req);
+      signOutBaseUrl = getUrlWithTransactionIdAndSubmissionId(
+        config.REGISTER_AN_OVERSEAS_ENTITY_WITH_PARAMS_URL,
+        transactionIdAndSubmissionId.transactionId,
+        transactionIdAndSubmissionId.submissionId
+      );
     }
   }
   res.locals.signOutBaseUrl = signOutBaseUrl;

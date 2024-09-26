038cb1f805503a7e5635464f2fb58c3cd0ed285e
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-09-26 10:24:17 +0100

    refactor: set default journey to registration in journeyDetectionMiddleware

--- src/middleware/navigation/journey.detection.middleware.ts
+++ src/middleware/navigation/journey.detection.middleware.ts
@@ -8,12 +8,11 @@ import {
 } from "../../config";
 
 import { logger } from "../../utils/logger";
-import { isRegistrationJourney, isRemoveJourney, isUpdateJourney } from "../../utils/url";
+import { isRemoveJourney, isUpdateJourney } from "../../utils/url";
 
 export const journeyDetectionMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   const isRemove: boolean = await isRemoveJourney(req);
   const isUpdate: boolean = await isUpdateJourney(req);
-  const isRegistration: boolean = isRegistrationJourney(req);
 
   if (isRemove) {
     logger.infoRequest(req, "Marking this request/response as a Remove Journey");
@@ -21,11 +20,9 @@ export const journeyDetectionMiddleware = async (req: Request, res: Response, ne
   } else if (isUpdate) {
     logger.infoRequest(req, "Marking this request/response as a Update Journey");
     res.locals.journey = JourneyType.update;
-  } else if (isRegistration) {
+  } else {
     logger.infoRequest(req, "Marking this request/response as a Registration Journey");
     res.locals.journey = JourneyType.register;
-  } else {
-    throw new Error("Unable to detect journey type");
   }
 
   return next();

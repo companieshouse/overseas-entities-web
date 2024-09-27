fc832ce52375ecffb7a3fa9fe46c2f2933526550
jnicholson-ch <jnicholson@companieshouse.gov.uk>
2024-09-27 15:30:53 +0100

    add:unit tests for relevant period - should cover all new lines now fix:relevant period not checking for when multiple items are not selected

--- src/utils/relevant.period.ts
+++ src/utils/relevant.period.ts
@@ -6,6 +6,6 @@ export const checkRelevantPeriod = (appData: ApplicationData): boolean =>
   appData.update?.change_beneficiary_relevant_period === "CHANGE_BENEFICIARY_RELEVANT_PERIOD";
 
 export const checkRPStatementsExist = (appData: ApplicationData): boolean =>
-  !!appData.update?.change_bo_relevant_period ||
-  !!appData.update?.trustee_involved_relevant_period ||
-  !!appData.update?.change_beneficiary_relevant_period ;
+  (!!appData.update?.change_bo_relevant_period && appData.update?.change_bo_relevant_period !== "NO_CHANGE_BO_RELEVANT_PERIOD") ||
+  (!!appData.update?.trustee_involved_relevant_period && appData.update?.trustee_involved_relevant_period !== "NO_TRUSTEE_INVOLVED_RELEVANT_PERIOD") ||
+  (!!appData.update?.change_beneficiary_relevant_period && appData.update?.change_beneficiary_relevant_period !== "NO_CHANGE_BENEFICIARY_RELEVANT_PERIOD");

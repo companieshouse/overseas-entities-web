1f87daa59cc4c07882fe483fa630e7ad4c0627ef
markpit <mpitten@companieshouse.gov.uk>
2024-09-30 17:42:32 +0100

    UAR-1636 Updating PSC to BO mapper for new NOCs

--- src/utils/update/psc.to.beneficial.owner.type.mapper.ts
+++ src/utils/update/psc.to.beneficial.owner.type.mapper.ts
@@ -2,7 +2,7 @@ import { CompanyPersonWithSignificantControl } from "@companieshouse/api-sdk-nod
 import { BeneficialOwnerGov } from "../../model/beneficial.owner.gov.model";
 import { BeneficialOwnerIndividual } from "../../model/beneficial.owner.individual.model";
 import { BeneficialOwnerOther } from "../../model/beneficial.owner.other.model";
-import { NatureOfControlType, yesNoResponse } from "../../model/data.types.model";
+import { NatureOfControlJurisdiction, NatureOfControlType, yesNoResponse } from "../../model/data.types.model";
 import { mapBOMOAddress, isSameAddress, mapDateOfBirth, mapSelfLink, mapInputDate, splitNationalities, mapBOIndividualName, lowerCaseAllWordsExceptFirstLetters } from "./mapper.utils";
 import { logger } from "../../utils/logger";
 import { BeneficialOwnerPrivateData } from "@companieshouse/api-sdk-node/dist/services/overseas-entities";
@@ -71,46 +71,83 @@ export const mapPscToBeneficialOwnerGov = (psc: CompanyPersonWithSignificantCont
   return result;
 };
 
-const mapNatureOfControl = (psc: CompanyPersonWithSignificantControl, beneficialOwner: BeneficialOwnerIndividual| BeneficialOwnerOther | BeneficialOwnerGov, isBeneficialGov: boolean) => {
+const mapNatureOfControl = (psc: CompanyPersonWithSignificantControl, beneficialOwner: BeneficialOwnerIndividual | BeneficialOwnerOther | BeneficialOwnerGov, isBeneficialGov: boolean) => {
   beneficialOwner.beneficial_owner_nature_of_control_types = [];
   beneficialOwner.non_legal_firm_members_nature_of_control_types = [];
-
   if (!isBeneficialGov) {
     beneficialOwner['trustees_nature_of_control_types'] = [];
   }
+  beneficialOwner.non_legal_firm_control_nature_of_control_types = [];
+  beneficialOwner.trust_control_nature_of_control_types = [];
+  beneficialOwner.owner_of_land_person_nature_of_control_jurisdictions = [];
+  beneficialOwner.owner_of_land_other_entity_nature_of_control_jurisdictions = [];
 
   psc.naturesOfControl?.forEach(natureType => {
-    const controlKind = natureTypeMap.get(natureType);
-    if (!controlKind) {
+    const natureKind = natureTypeMap.get(natureType);
+    if (!natureKind) {
       return;
     }
-    const natureOfControlType = natureOfControlTypeMap.get(natureType);
 
+    const natureOfControlType = natureOfControlTypeMap.get(natureType);
     if (natureOfControlType) {
-      switch (controlKind) {
-          case 'BoNatureOfControl':
-            beneficialOwner.beneficial_owner_nature_of_control_types?.push(natureOfControlType);
-            break;
-          case "NonLegalNatureOfControl":
-            beneficialOwner.non_legal_firm_members_nature_of_control_types?.push(natureOfControlType);
-            break;
-          case "TrustNatureOfControl":
-            if (!isBeneficialGov){
-              beneficialOwner['trustees_nature_of_control_types'].push(natureOfControlType);
-            }
-            break;
-          default:
-            logger.error("Unexpected nature of control for BO: " + controlKind);
-            break;
-      }
+      addNatureOfControlTypeToBeneficialOwner(natureKind, beneficialOwner, natureOfControlType, isBeneficialGov);
+      return;
+    }
+
+    const natureOfControlJurisdiction = natureOfControlJurisdictionMap.get(natureType);
+    if (natureOfControlJurisdiction) {
+      addNatureOfControlJurisdictionToBeneficialOwner(natureKind, beneficialOwner, natureOfControlJurisdiction);
     }
   });
+};
+
+const addNatureOfControlTypeToBeneficialOwner = (natureKind: BoTypes, beneficialOwner: BeneficialOwnerIndividual | BeneficialOwnerOther | BeneficialOwnerGov, natureOfControlType: NatureOfControlType, isBeneficialGov: boolean) => {
+  switch (natureKind) {
+      case BoTypes.BO_NATURE_OF_CONTROL:
+        beneficialOwner.beneficial_owner_nature_of_control_types?.push(natureOfControlType);
+        break;
+      case BoTypes.NON_LEGAL_NATURE_OF_CONTROL:
+        beneficialOwner.non_legal_firm_members_nature_of_control_types?.push(natureOfControlType);
+        break;
+      case BoTypes.TRUST_NATURE_OF_CONTROL:
+        if (!isBeneficialGov) {
+          beneficialOwner['trustees_nature_of_control_types'].push(natureOfControlType);
+        }
+        break;
+      case BoTypes.CONTROL_OVER_TRUST_NATURE_OF_CONTROL:
+        beneficialOwner.trust_control_nature_of_control_types?.push(natureOfControlType);
+        break;
+      case BoTypes.CONTROL_OVER_FIRM_NATURE_OF_CONTROL:
+        beneficialOwner.non_legal_firm_control_nature_of_control_types?.push(natureOfControlType);
+        break;
+      default:
+        logger.error("Unexpected nature of control for BO: " + natureKind);
+        break;
+  }
+};
+
+const addNatureOfControlJurisdictionToBeneficialOwner = (natureKind: BoTypes, beneficialOwner: BeneficialOwnerIndividual | BeneficialOwnerOther | BeneficialOwnerGov, natureOfControlJurisdiction: NatureOfControlJurisdiction) => {
+  switch (natureKind) {
+      case BoTypes.REGISTERED_OWNER_AS_NOMINEE_PERSON_NATURE_OF_CONTROL:
+        beneficialOwner.owner_of_land_person_nature_of_control_jurisdictions?.push(natureOfControlJurisdiction);
+        break;
+      case BoTypes.REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_NATURE_OF_CONTROL:
+        beneficialOwner.owner_of_land_other_entity_nature_of_control_jurisdictions?.push(natureOfControlJurisdiction);
+        break;
+      default:
+        logger.error("Unexpected nature of control for BO: " + natureKind);
+        break;
+  }
 };
 
 enum BoTypes {
   BO_NATURE_OF_CONTROL = "BoNatureOfControl",
   TRUST_NATURE_OF_CONTROL = "TrustNatureOfControl",
   NON_LEGAL_NATURE_OF_CONTROL = "NonLegalNatureOfControl",
+  CONTROL_OVER_TRUST_NATURE_OF_CONTROL = "ControlOverTrustNatureOfControl",
+  CONTROL_OVER_FIRM_NATURE_OF_CONTROL = "ControlOverFirmNatureOfControl",
+  REGISTERED_OWNER_AS_NOMINEE_PERSON_NATURE_OF_CONTROL = "RegisteredOwnerAsNomineePersonNatureOfControl",
+  REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_NATURE_OF_CONTROL = "RegisteredOwnerAsNomineeOtherEntityNatureOfControl",
 }
 
 export enum natureOfControl {
@@ -118,14 +155,34 @@ export enum natureOfControl {
   VOTING_RIGHT_MORE_THAN_25_PERCENT_SHARE_ENTITY = 'voting-rights-more-than-25-percent-registered-overseas-entity',
   RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS = 'right-to-appoint-and-remove-directors-registered-overseas-entity',
   SIGNIFICANT_INFLUENCE_OR_CONTROL_ENTITY = 'significant-influence-or-control-registered-overseas-entity',
+
   RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS_AS_TRUST = 'right-to-appoint-and-remove-directors-as-trust-registered-overseas-entity',
   SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_TRUST = 'significant-influence-or-control-as-trust-registered-overseas-entity',
   VOTING_RIGHT_MORE_THAN_25_PERCENT_AS_TRUST ='voting-rights-more-than-25-percent-as-trust-registered-overseas-entity',
   OWNERSHIP_MORE_THAN_25_PERCENT_AS_TRUST = 'ownership-of-shares-more-than-25-percent-as-trust-registered-overseas-entity',
+
   SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_FIRM = 'significant-influence-or-control-as-firm-registered-overseas-entity',
   RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS_AS_FIRM = 'right-to-appoint-and-remove-directors-as-firm-registered-overseas-entity',
   VOTING_RIGHT_MORE_THAN_25_PERCENT_AS_FIRM = 'voting-rights-more-than-25-percent-as-firm-registered-overseas-entity',
-  OWNERSHIP_MORE_THAN_25_PERCENT_AS_FIRM = 'ownership-of-shares-more-than-25-percent-as-firm-registered-overseas-entity'
+  OWNERSHIP_MORE_THAN_25_PERCENT_AS_FIRM = 'ownership-of-shares-more-than-25-percent-as-firm-registered-overseas-entity',
+
+  OWNERSHIP_OF_SHARES_MORE_THAN_25_PERCENT_AS_CONTROL_OVER_TRUST = 'ownership-of-shares-more-than-25-percent-as-control-over-trust-registered-overseas-entity',
+  VOTING_RIGHT_MORE_THAN_25_PERCENT_AS_CONTROL_OVER_TRUST = 'voting-rights-more-than-25-percent-as-control-over-trust-registered-overseas-entity',
+  RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS_AS_CONTROL_OVER_TRUST = 'right-to-appoint-and-remove-directors-as-control-over-trust-registered-overseas-entity',
+  SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_CONTROL_OVER_TRUST = 'significant-influence-or-control-as-control-over-trust-registered-overseas-entity',
+
+  OWNERSHIP_OF_SHARES_MORE_THAN_25_PERCENT_AS_CONTROL_OVER_FIRM = 'ownership-of-shares-more-than-25-percent-as-control-over-firm-registered-overseas-entity',
+  VOTING_RIGHT_MORE_THAN_25_PERCENT_AS_CONTROL_OVER_FIRM = 'voting-rights-more-than-25-percent-as-control-over-firm-registered-overseas-entity',
+  RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS_AS_CONTROL_OVER_FIRM = 'right-to-appoint-and-remove-directors-as-control-over-firm-registered-overseas-entity',
+  SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_CONTROL_OVER_FIRM = 'significant-influence-or-control-as-control-over-firm-registered-overseas-entity',
+
+  REGISTERED_OWNER_AS_NOMINEE_PERSON_ENGLAND_AND_WALES = 'registered-owner-as-nominee-person-england-wales',
+  REGISTERED_OWNER_AS_NOMINEE_PERSON_SCOTLAND = 'registered-owner-as-nominee-person-scotland',
+  REGISTERED_OWNER_AS_NOMINEE_PERSON_NORTHERN_IRELAND = 'registered-owner-as-nominee-person-northern-ireland',
+
+  REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_ENGLAND_AND_WALES = 'registered-owner-as-nominee-another-entity-england-wales',
+  REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_SCOTLAND = 'registered-owner-as-nominee-another-entity-scotland',
+  REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_NORTHERN_IRELAND = 'registered-owner-as-nominee-another-entity-northern-ireland'
 }
 
 const natureTypeMap = new Map<string, BoTypes>([
@@ -133,14 +190,34 @@ const natureTypeMap = new Map<string, BoTypes>([
   [natureOfControl.VOTING_RIGHT_MORE_THAN_25_PERCENT_SHARE_ENTITY, BoTypes.BO_NATURE_OF_CONTROL],
   [natureOfControl.RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS, BoTypes.BO_NATURE_OF_CONTROL],
   [natureOfControl.SIGNIFICANT_INFLUENCE_OR_CONTROL_ENTITY, BoTypes.BO_NATURE_OF_CONTROL],
+
   [natureOfControl.RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS_AS_TRUST, BoTypes.TRUST_NATURE_OF_CONTROL],
   [natureOfControl.SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_TRUST, BoTypes.TRUST_NATURE_OF_CONTROL],
   [natureOfControl.VOTING_RIGHT_MORE_THAN_25_PERCENT_AS_TRUST, BoTypes.TRUST_NATURE_OF_CONTROL],
   [natureOfControl.OWNERSHIP_MORE_THAN_25_PERCENT_AS_TRUST, BoTypes.TRUST_NATURE_OF_CONTROL],
+
   [natureOfControl.SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_FIRM, BoTypes.NON_LEGAL_NATURE_OF_CONTROL],
   [natureOfControl.RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS_AS_FIRM, BoTypes.NON_LEGAL_NATURE_OF_CONTROL],
   [natureOfControl.VOTING_RIGHT_MORE_THAN_25_PERCENT_AS_FIRM, BoTypes.NON_LEGAL_NATURE_OF_CONTROL],
-  [natureOfControl.OWNERSHIP_MORE_THAN_25_PERCENT_AS_FIRM, BoTypes.NON_LEGAL_NATURE_OF_CONTROL]
+  [natureOfControl.OWNERSHIP_MORE_THAN_25_PERCENT_AS_FIRM, BoTypes.NON_LEGAL_NATURE_OF_CONTROL],
+
+  [natureOfControl.OWNERSHIP_OF_SHARES_MORE_THAN_25_PERCENT_AS_CONTROL_OVER_TRUST, BoTypes.CONTROL_OVER_TRUST_NATURE_OF_CONTROL],
+  [natureOfControl.VOTING_RIGHT_MORE_THAN_25_PERCENT_AS_CONTROL_OVER_TRUST, BoTypes.CONTROL_OVER_TRUST_NATURE_OF_CONTROL],
+  [natureOfControl.RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS_AS_CONTROL_OVER_TRUST, BoTypes.CONTROL_OVER_TRUST_NATURE_OF_CONTROL],
+  [natureOfControl.SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_CONTROL_OVER_TRUST, BoTypes.CONTROL_OVER_TRUST_NATURE_OF_CONTROL],
+
+  [natureOfControl.OWNERSHIP_OF_SHARES_MORE_THAN_25_PERCENT_AS_CONTROL_OVER_FIRM, BoTypes.CONTROL_OVER_FIRM_NATURE_OF_CONTROL],
+  [natureOfControl.VOTING_RIGHT_MORE_THAN_25_PERCENT_AS_CONTROL_OVER_FIRM, BoTypes.CONTROL_OVER_FIRM_NATURE_OF_CONTROL],
+  [natureOfControl.RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS_AS_CONTROL_OVER_FIRM, BoTypes.CONTROL_OVER_FIRM_NATURE_OF_CONTROL],
+  [natureOfControl.SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_CONTROL_OVER_FIRM, BoTypes.CONTROL_OVER_FIRM_NATURE_OF_CONTROL],
+
+  [natureOfControl.REGISTERED_OWNER_AS_NOMINEE_PERSON_ENGLAND_AND_WALES, BoTypes.REGISTERED_OWNER_AS_NOMINEE_PERSON_NATURE_OF_CONTROL],
+  [natureOfControl.REGISTERED_OWNER_AS_NOMINEE_PERSON_SCOTLAND, BoTypes.REGISTERED_OWNER_AS_NOMINEE_PERSON_NATURE_OF_CONTROL],
+  [natureOfControl.REGISTERED_OWNER_AS_NOMINEE_PERSON_NORTHERN_IRELAND, BoTypes.REGISTERED_OWNER_AS_NOMINEE_PERSON_NATURE_OF_CONTROL],
+
+  [natureOfControl.REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_ENGLAND_AND_WALES, BoTypes.REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_NATURE_OF_CONTROL],
+  [natureOfControl.REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_SCOTLAND, BoTypes.REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_NATURE_OF_CONTROL],
+  [natureOfControl.REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_NORTHERN_IRELAND, BoTypes.REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_NATURE_OF_CONTROL]
 ]);
 
 const natureOfControlTypeMap = new Map<string, NatureOfControlType>([
@@ -148,14 +225,36 @@ const natureOfControlTypeMap = new Map<string, NatureOfControlType>([
   [natureOfControl.VOTING_RIGHT_MORE_THAN_25_PERCENT_SHARE_ENTITY, NatureOfControlType.OVER_25_PERCENT_OF_VOTING_RIGHTS],
   [natureOfControl.RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS, NatureOfControlType.APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS],
   [natureOfControl.SIGNIFICANT_INFLUENCE_OR_CONTROL_ENTITY, NatureOfControlType.SIGNIFICANT_INFLUENCE_OR_CONTROL],
+
   [natureOfControl.OWNERSHIP_MORE_THAN_25_PERCENT_AS_TRUST, NatureOfControlType.OVER_25_PERCENT_OF_SHARES],
   [natureOfControl.VOTING_RIGHT_MORE_THAN_25_PERCENT_AS_TRUST, NatureOfControlType.OVER_25_PERCENT_OF_VOTING_RIGHTS],
   [natureOfControl.RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS_AS_TRUST, NatureOfControlType.APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS],
   [natureOfControl.SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_TRUST, NatureOfControlType.SIGNIFICANT_INFLUENCE_OR_CONTROL],
+
   [natureOfControl.OWNERSHIP_MORE_THAN_25_PERCENT_AS_FIRM, NatureOfControlType.OVER_25_PERCENT_OF_SHARES],
   [natureOfControl.VOTING_RIGHT_MORE_THAN_25_PERCENT_AS_FIRM, NatureOfControlType.OVER_25_PERCENT_OF_VOTING_RIGHTS],
   [natureOfControl.RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS_AS_FIRM, NatureOfControlType.APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS],
-  [natureOfControl.SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_FIRM, NatureOfControlType.SIGNIFICANT_INFLUENCE_OR_CONTROL]
+  [natureOfControl.SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_FIRM, NatureOfControlType.SIGNIFICANT_INFLUENCE_OR_CONTROL],
+
+  [natureOfControl.OWNERSHIP_OF_SHARES_MORE_THAN_25_PERCENT_AS_CONTROL_OVER_TRUST, NatureOfControlType.OVER_25_PERCENT_OF_SHARES],
+  [natureOfControl.VOTING_RIGHT_MORE_THAN_25_PERCENT_AS_CONTROL_OVER_TRUST, NatureOfControlType.OVER_25_PERCENT_OF_VOTING_RIGHTS],
+  [natureOfControl.RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS_AS_CONTROL_OVER_TRUST, NatureOfControlType.APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS],
+  [natureOfControl.SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_CONTROL_OVER_TRUST, NatureOfControlType.SIGNIFICANT_INFLUENCE_OR_CONTROL],
+
+  [natureOfControl.OWNERSHIP_OF_SHARES_MORE_THAN_25_PERCENT_AS_CONTROL_OVER_FIRM, NatureOfControlType.OVER_25_PERCENT_OF_SHARES],
+  [natureOfControl.VOTING_RIGHT_MORE_THAN_25_PERCENT_AS_CONTROL_OVER_FIRM, NatureOfControlType.OVER_25_PERCENT_OF_VOTING_RIGHTS],
+  [natureOfControl.RIGHT_TO_APPOINT_AND_REMOVE_DIRECTORS_AS_CONTROL_OVER_FIRM, NatureOfControlType.APPOINT_OR_REMOVE_MAJORITY_BOARD_DIRECTORS],
+  [natureOfControl.SIGNIFICANT_INFLUENCE_OR_CONTROL_AS_CONTROL_OVER_FIRM, NatureOfControlType.SIGNIFICANT_INFLUENCE_OR_CONTROL]
+]);
+
+const natureOfControlJurisdictionMap = new Map<string, NatureOfControlJurisdiction>([
+  [natureOfControl.REGISTERED_OWNER_AS_NOMINEE_PERSON_ENGLAND_AND_WALES, NatureOfControlJurisdiction.ENGLAND_AND_WALES],
+  [natureOfControl.REGISTERED_OWNER_AS_NOMINEE_PERSON_SCOTLAND, NatureOfControlJurisdiction.SCOTLAND],
+  [natureOfControl.REGISTERED_OWNER_AS_NOMINEE_PERSON_NORTHERN_IRELAND, NatureOfControlJurisdiction.NORTHERN_IRELAND],
+
+  [natureOfControl.REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_ENGLAND_AND_WALES, NatureOfControlJurisdiction.ENGLAND_AND_WALES],
+  [natureOfControl.REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_SCOTLAND, NatureOfControlJurisdiction.SCOTLAND],
+  [natureOfControl.REGISTERED_OWNER_AS_NOMINEE_OTHER_ENTITY_NORTHERN_IRELAND, NatureOfControlJurisdiction.NORTHERN_IRELAND]
 ]);
 
 export const mapIndividualBOPrivateData = (boPrivateData: BeneficialOwnerPrivateData[], beneficialOwner: BeneficialOwnerIndividual) => {

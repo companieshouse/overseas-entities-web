e463794f3e1ea15082b1b3ca865dc86ec40691d3
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-10-29 09:21:56 +0000

    feat: retrieve application data from DB for the beneficial-owner-individual page

--- src/controllers/beneficial.owner.individual.controller.ts
+++ src/controllers/beneficial.owner.individual.controller.ts
@@ -1,4 +1,7 @@
 import { NextFunction, Request, Response } from "express";
+import * as config from "../config";
+import { isActiveFeature } from "../utils/feature.flag";
+import { getUrlWithParamsToPath } from "../utils/url";
 
 import {
   getBeneficialOwnerIndividual,
@@ -7,11 +10,6 @@ import {
   removeBeneficialOwnerIndividual,
   updateBeneficialOwnerIndividual
 } from "../utils/beneficial.owner.individual";
-
-import * as config from "../config";
-
-import { isActiveFeature } from "../utils/feature.flag";
-import { getUrlWithParamsToPath } from "../utils/url";
 
 export const get = async (req: Request, res: Response) => {
   const backLinkUrl = isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL)
@@ -41,9 +39,8 @@ export const remove = async (req: Request, res: Response, next: NextFunction) =>
 
 const getBeneficialOwnerTypeUrl = (req: Request): string => {
   let nextPageUrl = config.BENEFICIAL_OWNER_TYPE_URL;
-  if (isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL)){
+  if (isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL)) {
     nextPageUrl = getUrlWithParamsToPath(config.BENEFICIAL_OWNER_TYPE_WITH_PARAMS_URL, req);
   }
-
   return nextPageUrl;
 };

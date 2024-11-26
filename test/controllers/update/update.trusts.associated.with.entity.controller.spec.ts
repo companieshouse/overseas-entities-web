31dc6dee3088f393a85a32285479345935267a68
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-11-26 15:44:34 +0000

    feat/roe-2674: retrieve application data from the DB for the `add-trust` page

--- test/controllers/update/update.trusts.associated.with.entity.controller.spec.ts
+++ test/controllers/update/update.trusts.associated.with.entity.controller.spec.ts
@@ -1,57 +1,76 @@
 jest.mock('ioredis');
-jest.mock('../../../src/utils/feature.flag' );
+jest.mock('../../../src/utils/feature.flag');
 jest.mock('../../../src/utils/application.data');
 jest.mock('../../../src/middleware/authentication.middleware');
 jest.mock('../../../src/middleware/company.authentication.middleware');
 jest.mock('../../../src/middleware/service.availability.middleware');
+jest.mock("../../../src/utils/url");
 
-import mockCsrfProtectionMiddleware from "../../__mocks__/csrfProtectionMiddleware.mock";
-import { beforeEach, jest, test, describe } from '@jest/globals';
 import request from 'supertest';
 import { NextFunction } from 'express';
 
+import mockCsrfProtectionMiddleware from "../../__mocks__/csrfProtectionMiddleware.mock";
 import app from '../../../src/app';
+
+import { authentication } from '../../../src/middleware/authentication.middleware';
+import { companyAuthentication } from '../../../src/middleware/company.authentication.middleware';
+import { serviceAvailabilityMiddleware } from '../../../src/middleware/service.availability.middleware';
+import { isActiveFeature } from '../../../src/utils/feature.flag';
+import { stringCount } from '../../utils/test.utils';
+import { ADD_TRUST_TEXTS } from "../../../src/utils/add.trust";
+import { isRegistrationJourney } from "../../../src/utils/url";
+
+import { fetchApplicationData, getApplicationData } from '../../../src/utils/application.data';
+import { Trust, TrustKey } from '../../../src/model/trust.model';
+import { APPLICATION_DATA_MOCK, TRUST } from '../../__mocks__/session.mock';
+
+import {
+  beneficialOwnerIndividualType,
+  beneficialOwnerOtherType
+} from "../../../src/model";
+
 import {
   UPDATE_TRUSTS_ASSOCIATED_WITH_THE_OVERSEAS_ENTITY_URL,
   UPDATE_TRUSTS_TELL_US_ABOUT_IT_PAGE,
   UPDATE_BENEFICIAL_OWNER_TYPE_URL,
   UPDATE_BENEFICIAL_OWNER_STATEMENTS_URL,
 } from '../../../src/config';
-import { authentication } from '../../../src/middleware/authentication.middleware';
-import { companyAuthentication } from '../../../src/middleware/company.authentication.middleware';
-import { serviceAvailabilityMiddleware } from '../../../src/middleware/service.availability.middleware';
-import { getApplicationData } from '../../../src/utils/application.data';
-import { isActiveFeature } from '../../../src/utils/feature.flag';
 
-import { APPLICATION_DATA_MOCK, TRUST } from '../../__mocks__/session.mock';
-import { PAGE_TITLE_ERROR, UPDATE_TRUSTS_ASSOCIATED_ADDED_HEADING, UPDATE_MANAGE_TRUSTS_REVIEWED_HEADING, SAVE_AND_CONTINUE_BUTTON_TEXT } from '../../__mocks__/text.mock';
-import { Trust, TrustKey } from '../../../src/model/trust.model';
-import { stringCount } from '../../utils/test.utils';
-import { beneficialOwnerIndividualType, beneficialOwnerOtherType } from "../../../src/model";
-import { ADD_TRUST_TEXTS } from "../../../src/utils/add.trust";
+import {
+  PAGE_TITLE_ERROR,
+  UPDATE_TRUSTS_ASSOCIATED_ADDED_HEADING,
+  UPDATE_MANAGE_TRUSTS_REVIEWED_HEADING,
+  SAVE_AND_CONTINUE_BUTTON_TEXT
+} from '../../__mocks__/text.mock';
 
 mockCsrfProtectionMiddleware.mockClear();
 const mockGetApplicationData = getApplicationData as jest.Mock;
+const mockFetchApplicationData = fetchApplicationData as jest.Mock;
 
 const mockAuthenticationMiddleware = authentication as jest.Mock;
-mockAuthenticationMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
+mockAuthenticationMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next());
 
 const mockCompanyAuthenticationMiddleware = companyAuthentication as jest.Mock;
-mockCompanyAuthenticationMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
+mockCompanyAuthenticationMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next());
 
 const mockServiceAvailabilityMiddleware = serviceAvailabilityMiddleware as jest.Mock;
-mockServiceAvailabilityMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
+mockServiceAvailabilityMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next());
 
 const mockIsActiveFeature = isActiveFeature as jest.Mock;
 mockIsActiveFeature.mockReturnValue(true);
 
+const mockIsRegistrationJourney = isRegistrationJourney as jest.Mock;
+mockIsRegistrationJourney.mockReturnValue(false);
+
 describe('Update - Trusts - Trusts associated with the overseas entity', () => {
+
   beforeEach(() => {
     jest.clearAllMocks();
     mockIsActiveFeature.mockReset();
   });
 
   describe('GET tests', () => {
+
     test.each([
       [
         "are BOs eligible for trusts",
@@ -71,7 +90,8 @@ describe('Update - Trusts - Trusts associated with the overseas entity', () => {
       ],
     ])("when FEATURE_FLAG_ENABLE_UPDATE_TRUSTS feature flag is on and there %s, page is returned %s the add another trust option", async (_description1, _description2, appData, isAddTrustsOptionToBeShown) => {
       mockIsActiveFeature.mockReturnValue(true); // FEATURE_FLAG_ENABLE_UPDATE_TRUSTS
-      mockGetApplicationData.mockReturnValue( appData );
+      mockGetApplicationData.mockReturnValue(appData);
+      mockFetchApplicationData.mockReturnValue(appData);
 
       const resp = await request(app).get(UPDATE_TRUSTS_ASSOCIATED_WITH_THE_OVERSEAS_ENTITY_URL);
 
@@ -88,8 +108,7 @@ describe('Update - Trusts - Trusts associated with the overseas entity', () => {
     });
 
     test('when reviewed and added trusts exist, page is returned with 2 separate summary tables', async () => {
-      mockIsActiveFeature.mockReturnValue(true);
-      mockGetApplicationData.mockReturnValue({
+      const mockAppData = {
         ...APPLICATION_DATA_MOCK,
         [TrustKey]: [
           TRUST,
@@ -99,7 +118,10 @@ describe('Update - Trusts - Trusts associated with the overseas entity', () => {
             ch_reference: "123"
           } as Trust
         ]
-      });
+      };
+      mockIsActiveFeature.mockReturnValue(true);
+      mockFetchApplicationData.mockReturnValue(mockAppData);
+      mockGetApplicationData.mockReturnValue(mockAppData);
 
       const resp = await request(app).get(UPDATE_TRUSTS_ASSOCIATED_WITH_THE_OVERSEAS_ENTITY_URL);
 
@@ -114,9 +136,7 @@ describe('Update - Trusts - Trusts associated with the overseas entity', () => {
     });
 
     test('Reviewed trust status will be shown in summary table', async () => {
-      mockIsActiveFeature.mockReturnValue(true);
-      // trust with ch_references indicates a reviewable trust (ie it would be an existing trust that has come from chips)
-      mockGetApplicationData.mockReturnValue({
+      const mockAppData = {
         ...APPLICATION_DATA_MOCK,
         [TrustKey]: [
           TRUST,
@@ -136,7 +156,11 @@ describe('Update - Trusts - Trusts associated with the overseas entity', () => {
             ch_reference: "123"
           } as Trust
         ]
-      });
+      };
+      mockIsActiveFeature.mockReturnValue(true);
+      // trust with ch_references indicates a reviewable trust (ie it would be an existing trust that has come from chips)
+      mockGetApplicationData.mockReturnValue(mockAppData);
+      mockFetchApplicationData.mockReturnValue(mockAppData);
 
       const resp = await request(app).get(UPDATE_TRUSTS_ASSOCIATED_WITH_THE_OVERSEAS_ENTITY_URL);
 
@@ -151,31 +175,29 @@ describe('Update - Trusts - Trusts associated with the overseas entity', () => {
   });
 
   describe('POST tests', () => {
+
     test('when no trusts are to be added, redirect to check your answers page', async () => {
       mockIsActiveFeature.mockReturnValueOnce(true); // FEATURE_FLAG_ENABLE_UPDATE_TRUSTS
-      mockGetApplicationData.mockReturnValue( APPLICATION_DATA_MOCK );
-
+      mockGetApplicationData.mockReturnValue(APPLICATION_DATA_MOCK);
+      mockFetchApplicationData.mockReturnValue(APPLICATION_DATA_MOCK);
       const resp = await request(app).post(UPDATE_TRUSTS_ASSOCIATED_WITH_THE_OVERSEAS_ENTITY_URL).send({ addTrust: '0' });
-
       expect(resp.status).toEqual(302);
       expect(resp.header.location).toEqual(UPDATE_BENEFICIAL_OWNER_STATEMENTS_URL);
     });
 
     test('when statement validation flag is on, and no trusts are to be added, redirect to beneficial owner statements page', async () => {
       mockIsActiveFeature.mockReturnValueOnce(true);
-      mockGetApplicationData.mockReturnValue( APPLICATION_DATA_MOCK );
-
+      mockGetApplicationData.mockReturnValue(APPLICATION_DATA_MOCK);
+      mockFetchApplicationData.mockReturnValue(APPLICATION_DATA_MOCK);
       const resp = await request(app).post(UPDATE_TRUSTS_ASSOCIATED_WITH_THE_OVERSEAS_ENTITY_URL).send({ addTrust: '0' });
-
       expect(resp.status).toEqual(302);
       expect(resp.header.location).toEqual(UPDATE_BENEFICIAL_OWNER_STATEMENTS_URL);
     });
 
     test('when trusts are to be added, redirect to add trust page', async () => {
-      mockGetApplicationData.mockReturnValue( APPLICATION_DATA_MOCK );
-
+      mockGetApplicationData.mockReturnValue(APPLICATION_DATA_MOCK);
+      mockFetchApplicationData.mockReturnValue(APPLICATION_DATA_MOCK);
       const resp = await request(app).post(UPDATE_TRUSTS_ASSOCIATED_WITH_THE_OVERSEAS_ENTITY_URL).send({ addTrust: 'addTrustYes' });
-
       expect(resp.status).toEqual(302);
       expect(resp.header.location).toContain(UPDATE_TRUSTS_TELL_US_ABOUT_IT_PAGE);
     });

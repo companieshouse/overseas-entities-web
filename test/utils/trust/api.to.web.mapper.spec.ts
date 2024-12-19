b1780b81ae0377352c19f46a53dc1a051bf53043
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-12-19 19:59:07 +0000

    test: centralise mock method definition

--- test/utils/trust/api.to.web.mapper.spec.ts
+++ test/utils/trust/api.to.web.mapper.spec.ts
@@ -7,6 +7,8 @@ import { APPLICATION_DATA_MOCK } from "../../__mocks__/session.mock";
 import { mapTrustApiToWebWhenFlagsAreSet } from "../../../src/utils/trust/api.to.web.mapper";
 
 const mockMapTrustApiReturnModelToWebModel = mapTrustApiReturnModelToWebModel as jest.Mock;
+mockMapTrustApiReturnModelToWebModel.mockReturnValue(true);
+
 const mockIsActiveFeature = isActiveFeature as jest.Mock;
 
 describe("mapTrustApiToWebWhenFlagsAreSet tests", () => {
@@ -18,7 +20,6 @@ describe("mapTrustApiToWebWhenFlagsAreSet tests", () => {
   });
 
   test('is not invoked when isRegistration flag is false', () => {
-    mockMapTrustApiReturnModelToWebModel.mockReturnValue(true);
     mapTrustApiToWebWhenFlagsAreSet(APPLICATION_DATA_MOCK, false);
     expect(mockMapTrustApiReturnModelToWebModel).not.toHaveBeenCalled();
   });
@@ -26,7 +27,6 @@ describe("mapTrustApiToWebWhenFlagsAreSet tests", () => {
   test('is not invoked when REDIS_removal flag is set to OFF', () => {
     mockIsActiveFeature.mockReturnValueOnce(false); // FEATURE_FLAG_ENABLE_REDIS_REMOVAL
     mockIsActiveFeature.mockReturnValueOnce(true); // FEATURE_FLAG_ENABLE_TRUSTS_WEB
-    mockMapTrustApiReturnModelToWebModel.mockReturnValue(true);
     mapTrustApiToWebWhenFlagsAreSet(APPLICATION_DATA_MOCK as any, true);
     expect(mockMapTrustApiReturnModelToWebModel).not.toHaveBeenCalled();
   });
@@ -34,7 +34,6 @@ describe("mapTrustApiToWebWhenFlagsAreSet tests", () => {
   test('is not invoked when Trusts_web flag is set to OFF', () => {
     mockIsActiveFeature.mockReturnValueOnce(true); // FEATURE_FLAG_ENABLE_REDIS_REMOVAL
     mockIsActiveFeature.mockReturnValueOnce(false); // FEATURE_FLAG_ENABLE_TRUSTS_WEB
-    mockMapTrustApiReturnModelToWebModel.mockReturnValue(true);
     mapTrustApiToWebWhenFlagsAreSet(APPLICATION_DATA_MOCK as any, true);
     expect(mockMapTrustApiReturnModelToWebModel).not.toHaveBeenCalled();
   });
@@ -42,14 +41,12 @@ describe("mapTrustApiToWebWhenFlagsAreSet tests", () => {
   test('is not invoked when REDIS_removal flag is set to OFF and Trusts_web flag is set to OFF', () => {
     mockIsActiveFeature.mockReturnValueOnce(false); // FEATURE_FLAG_ENABLE_REDIS_REMOVAL
     mockIsActiveFeature.mockReturnValueOnce(false); // FEATURE_FLAG_ENABLE_TRUSTS_WEB
-    mockMapTrustApiReturnModelToWebModel.mockReturnValue(true);
     mapTrustApiToWebWhenFlagsAreSet(APPLICATION_DATA_MOCK as any, true);
     expect(mockMapTrustApiReturnModelToWebModel).not.toHaveBeenCalled();
   });
 
   test('is invoked when isRegistration is set to TRUE and both flags are set to ON', () => {
     mockIsActiveFeature.mockReturnValue(true); // ALL_FLAGS
-    mockMapTrustApiReturnModelToWebModel.mockReturnValue(true);
     mapTrustApiToWebWhenFlagsAreSet(APPLICATION_DATA_MOCK as any, true);
     expect(mockMapTrustApiReturnModelToWebModel).toHaveBeenCalledTimes(1);
   });

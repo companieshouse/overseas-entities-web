1f00bcf21a63e80e678d78da163651868e2c6be4
rmcnally-ch <rmcnally@companieshouse.gov.uk>
2025-04-28 17:05:30 +0100

    Create mvc for new page

---
+++ test/controllers/update/relevant.period.submit.by.paper.controller.spec.ts
@@ -0,0 +1,58 @@
+jest.mock("ioredis");
+jest.mock("../../../src/utils/logger");
+jest.mock('../../../src/middleware/authentication.middleware');
+jest.mock('../../../src/middleware/company.authentication.middleware');
+jest.mock('../../../src/utils/application.data');
+jest.mock('../../../src/middleware/navigation/update/has.presenter.middleware');
+jest.mock('../../../src/middleware/service.availability.middleware');
+jest.mock('../../../src/utils/feature.flag');
+
+import mockCsrfProtectionMiddleware from "../../__mocks__/csrfProtectionMiddleware.mock";
+import { NextFunction, Request, Response } from "express";
+import { beforeEach, expect, jest, test, describe } from "@jest/globals";
+import request from "supertest";
+
+import * as config from "../../../src/config";
+import app from "../../../src/app";
+import { RELEVANT_PERIOD_SUBMIT_BY_PAPER_TITLE } from "../../__mocks__/text.mock";
+import { APPLICATION_DATA_MOCK } from "../../__mocks__/session.mock";
+import { getApplicationData } from "../../../src/utils/application.data";
+import { authentication } from "../../../src/middleware/authentication.middleware";
+import { companyAuthentication } from "../../../src/middleware/company.authentication.middleware";
+import { hasUpdatePresenter } from "../../../src/middleware/navigation/update/has.presenter.middleware";
+import { serviceAvailabilityMiddleware } from "../../../src/middleware/service.availability.middleware";
+import { isActiveFeature } from "../../../src/utils/feature.flag";
+
+mockCsrfProtectionMiddleware.mockClear();
+const mockHasUpdatePresenter = hasUpdatePresenter as jest.Mock;
+mockHasUpdatePresenter.mockImplementation((req: Request, res: Response, next: NextFunction) => next());
+
+const mockAuthenticationMiddleware = authentication as jest.Mock;
+mockAuthenticationMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
+
+const mockCompanyAuthenticationMiddleware = companyAuthentication as jest.Mock;
+mockCompanyAuthenticationMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next());
+
+const mockServiceAvailabilityMiddleware = serviceAvailabilityMiddleware as jest.Mock;
+mockServiceAvailabilityMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next());
+
+const mockGetApplicationData = getApplicationData as jest.Mock;
+
+const mockIsActiveFeature = isActiveFeature as jest.Mock;
+mockIsActiveFeature.mockReturnValue(true);
+
+describe("Relevant period submit by paper controller", () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  describe("GET tests", () => {
+    test.skip(`renders the ${config.RELEVANT_PERIOD_SUBMIT_BY_PAPER_PAGE} page`, async () => {
+      mockGetApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
+      const resp = await request(app).get(config.RELEVANT_PERIOD_SUBMIT_BY_PAPER_URL);
+
+      expect(resp.status).toEqual(200);
+      expect(resp.text).toContain(RELEVANT_PERIOD_SUBMIT_BY_PAPER_TITLE);
+    });
+  });
+});

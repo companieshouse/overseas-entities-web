0cc4e83fdb2b43d0e2a50da381e7034d141c3ba7
snotta-ch <snotta@companieshouse.gov.uk>
2025-06-25 14:00:44 +0100

    ROECCT-631 Add paper form filing page

---
+++ src/controllers/update/relevant.period.submit.year.by.paper.controller.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+
+import { logger } from "../../utils/logger";
+import * as config from "../../config";
+import { ApplicationData } from "../../model";
+import { getApplicationData } from "../../utils/application.data";
+
+export const get = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    logger.debugRequest(req, `GET ${config.RELEVANT_PERIOD_SUBMIT_YEAR_BY_PAPER_PAGE}`);
+    const appData: ApplicationData = await getApplicationData(req.session);
+
+    return res.render(config.RELEVANT_PERIOD_SUBMIT_YEAR_BY_PAPER_PAGE, {
+      backLinkUrl: '',
+      templateName: config.RELEVANT_PERIOD_SUBMIT_YEAR_BY_PAPER_PAGE,
+      ...appData,
+    });
+  } catch (error) {
+    logger.errorRequest(req, error);
+    next(error);
+  }
+};

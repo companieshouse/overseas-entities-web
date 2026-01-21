ff9bd5dd846830ed1af8c70bc595022150ec73fe
mwejuli-ch <mwejuli@companieshouse.gov.uk>
2026-01-21 13:12:42 +0000

    feat: get and save app data using db for the due-diligence-overseas-entity page

--- src/utils/overseas.due.diligence.ts
+++ src/utils/overseas.due.diligence.ts
@@ -3,15 +3,15 @@ import { Session } from "@companieshouse/node-session-handler";
 import { logger } from "../utils/logger";
 import * as config from "../config";
 import { ApplicationData } from "../model";
-import { isRegistrationJourney } from "./url";
+import { isRemoveJourney } from "./url";
 import { isActiveFeature } from "./feature.flag";
 import { saveAndContinue } from "./save.and.continue";
 import { DueDiligenceKey } from "../model/due.diligence.model";
 import { updateOverseasEntity } from "../service/overseas.entities.service";
 
-import { IdentityAddressKey, IdentityAddressKeys } from "../model/address.model";
 import { AddressKeys, InputDateKeys } from "../model/data.types.model";
 import { IdentityDateKey, IdentityDateKeys } from "../model/date.model";
+import { IdentityAddressKey, IdentityAddressKeys } from "../model/address.model";
 
 import {
   OverseasEntityDueDiligenceKey,
@@ -19,12 +19,12 @@ import {
 } from "../model/overseas.entity.due.diligence.model";
 
 import {
-  setApplicationData,
   prepareData,
+  setExtraData,
+  setApplicationData,
+  fetchApplicationData,
   mapDataObjectToFields,
   mapFieldsToDataObject,
-  fetchApplicationData,
-  setExtraData,
 } from "../utils/application.data";
 
 export const getDueDiligence = async (
@@ -39,8 +39,8 @@ export const getDueDiligence = async (
 
     logger.debugRequest(req, `${req.method} ${req.route.path}`);
 
-    const isRegistration = isRegistrationJourney(req);
-    const appData: ApplicationData = await fetchApplicationData(req, isRegistration);
+    const isRemove = isRemoveJourney(req);
+    const appData: ApplicationData = await fetchApplicationData(req, !isRemove);
     const data = appData[OverseasEntityDueDiligenceKey];
     const identityAddress = (data?.[IdentityAddressKey]) ? mapDataObjectToFields(data[IdentityAddressKey], IdentityAddressKeys, AddressKeys) : {};
     const identityDate = (data?.[IdentityDateKey]) ? mapDataObjectToFields(data[IdentityDateKey], IdentityDateKeys, InputDateKeys) : {};
@@ -64,14 +64,14 @@ export const postDueDiligence = async (req: Request, res: Response, next: NextFu
 
     logger.debugRequest(req, `${req.method} ${req.route.path}`);
 
-    const isRegistration = isRegistrationJourney(req);
+    const isRemove = await isRemoveJourney(req);
     const session = req.session as Session;
     const data = prepareData(req.body, OverseasEntityDueDiligenceKeys);
     data[IdentityAddressKey] = mapFieldsToDataObject(req.body, IdentityAddressKeys, AddressKeys);
     data[IdentityDateKey] = mapFieldsToDataObject(req.body, IdentityDateKeys, InputDateKeys);
 
-    if (isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL) && isRegistration) {
-      let appData: ApplicationData = await fetchApplicationData(req, isRegistration);
+    if (isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL) && !isRemove) {
+      let appData: ApplicationData = await fetchApplicationData(req, !isRemove);
       appData = Object.assign(appData, { [OverseasEntityDueDiligenceKey]: data });
       appData = Object.assign(appData, { [DueDiligenceKey]: {} }); // set dueDiligence object to empty
       await updateOverseasEntity(req, session, appData);

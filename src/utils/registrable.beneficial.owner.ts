096ee69ca600ad6d9e370767c652c255a6c5aa14
mwejuli-ch <mwejuli@companieshouse.gov.uk>
2026-02-11 09:07:32 +0000

    feat: get and save app data using db for the `no-change-registrable-beneficial-owner` page

--- src/utils/registrable.beneficial.owner.ts
+++ src/utils/registrable.beneficial.owner.ts
@@ -1,18 +1,23 @@
 import { NextFunction, Request, Response } from "express";
+import { Session } from "@companieshouse/node-session-handler";
 import { logger } from "../utils/logger";
 import * as config from "../config";
 import { ApplicationData } from "../model/application.model";
-import { getApplicationData, setExtraData } from "../utils/application.data";
-import { RegistrableBeneficialOwnerKey } from "../model/update.type.model";
 import { yesNoResponse } from "../model/data.types.model";
-import { Session } from "@companieshouse/node-session-handler";
 import { saveAndContinue } from "./save.and.continue";
-import { isRemoveJourney } from "../utils/url";
+import { RegistrableBeneficialOwnerKey } from "../model/update.type.model";
+import { getRedirectUrl, isRemoveJourney } from "../utils/url";
+import { fetchApplicationData, setExtraData } from "../utils/application.data";
+import { isActiveFeature } from "./feature.flag";
+import { updateOverseasEntity } from "../service/overseas.entities.service";
 
 export const getRegistrableBeneficialOwner = async (req: Request, res: Response, next: NextFunction, noChangeFlag?: boolean): Promise<void> => {
+
   try {
+
     logger.debugRequest(req, `${req.method} ${req.route.path}`);
-    const appData: ApplicationData = await getApplicationData(req.session);
+    const isRemove: boolean = await isRemoveJourney(req);
+    const appData: ApplicationData = await fetchApplicationData(req, !isRemove);
     let templateName: string;
     let backLinkUrl: string;
 
@@ -20,50 +25,57 @@ export const getRegistrableBeneficialOwner = async (req: Request, res: Response,
       backLinkUrl = config.UPDATE_NO_CHANGE_BENEFICIAL_OWNER_STATEMENTS_PAGE;
       templateName = config.UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_PAGE;
     } else {
-      backLinkUrl = config.UPDATE_BENEFICIAL_OWNER_STATEMENTS_URL;
       templateName = config.UPDATE_REGISTRABLE_BENEFICIAL_OWNER_PAGE;
+      backLinkUrl = getRedirectUrl({
+        req,
+        urlWithEntityIds: config.UPDATE_BENEFICIAL_OWNER_STATEMENTS_WITH_PARAMS_URL,
+        urlWithoutEntityIds: config.UPDATE_BENEFICIAL_OWNER_STATEMENTS_URL,
+      });
     }
     return res.render(templateName, {
-      backLinkUrl: backLinkUrl,
-      templateName: templateName,
       ...appData,
+      backLinkUrl,
+      templateName,
+      noChangeFlag,
       [RegistrableBeneficialOwnerKey]: appData.update?.[RegistrableBeneficialOwnerKey],
-      noChangeFlag
     });
   } catch (error) {
     next(error);
   }
 };
 
-export const postRegistrableBeneficialOwner = async (req: Request, res: Response, next: NextFunction, noChangeFlag?: boolean): Promise<void> => {
+export const postRegistrableBeneficialOwner = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+
   try {
+
     logger.debugRequest(req, `${req.method} ${req.route.path}`);
     const isRegistrableBeneficialOwner = req.body[RegistrableBeneficialOwnerKey];
     const session = req.session as Session;
-    const appData: ApplicationData = await getApplicationData(session);
+    const isRemove: boolean = await isRemoveJourney(req);
+    const appData: ApplicationData = await fetchApplicationData(req, !isRemove);
 
     if (appData.update) {
       appData.update.registrable_beneficial_owner = isRegistrableBeneficialOwner === '1' ? yesNoResponse.Yes : yesNoResponse.No;
     }
-    setExtraData(req.session, appData);
-    await saveAndContinue(req, session);
 
-    const isRemove: boolean = await isRemoveJourney(req);
+    if (isActiveFeature(config.FEATURE_FLAG_ENABLE_REDIS_REMOVAL) && !isRemove) {
+      await updateOverseasEntity(req, session, appData);
+    } else {
+      await saveAndContinue(req, session);
+    }
+    setExtraData(req.session, appData);
 
     if (isRemove) {
       return res.redirect(config.UPDATE_STATEMENT_VALIDATION_ERRORS_URL);
     }
 
-    if (noChangeFlag) {
-      noChangeHandler(req, res);
-    } else {
-      return res.redirect(config.UPDATE_STATEMENT_VALIDATION_ERRORS_URL);
-    }
+    return res.redirect(getRedirectUrl({
+      req,
+      urlWithEntityIds: config.UPDATE_STATEMENT_VALIDATION_ERRORS_WITH_PARAMS_URL,
+      urlWithoutEntityIds: config.UPDATE_STATEMENT_VALIDATION_ERRORS_URL,
+    }));
   } catch (error) {
     next(error);
   }
 };
 
-const noChangeHandler = (req: Request, res: Response) => {
-  return res.redirect(config.UPDATE_STATEMENT_VALIDATION_ERRORS_URL);
-};
--- test/controllers/update/no.change.registrable.beneficial.owner.controller.spec.ts
+++ test/controllers/update/no.change.registrable.beneficial.owner.controller.spec.ts
@@ -5,63 +5,79 @@ jest.mock('../../../src/middleware/company.authentication.middleware');
 jest.mock('../../../src/utils/application.data');
 jest.mock('../../../src/middleware/service.availability.middleware');
 jest.mock('../../../src/middleware/navigation/update/has.overseas.entity.middleware');
-jest.mock("../../../src/utils/feature.flag" );
+jest.mock("../../../src/utils/feature.flag");
 jest.mock('../../../src/utils/save.and.continue');
+jest.mock('../../../src/service/overseas.entities.service');
 
 // import remove journey middleware mock before app to prevent real function being used instead of mock
 import mockJourneyDetectionMiddleware from "../../__mocks__/journey.detection.middleware.mock";
 import mockCsrfProtectionMiddleware from "../../__mocks__/csrfProtectionMiddleware.mock";
-
 import { NextFunction, Request, Response } from "express";
 import { beforeEach, expect, jest, test, describe } from "@jest/globals";
 import request from "supertest";
+
 import app from "../../../src/app";
 
-import {
-  UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_PAGE,
-  UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_URL,
-  UPDATE_STATEMENT_VALIDATION_ERRORS_URL
-} from "../../../src/config";
-import { getApplicationData, setExtraData } from "../../../src/utils/application.data";
-import { APPLICATION_DATA_MOCK, APPLICATION_DATA_MOCK_WITHOUT_UPDATE } from "../../__mocks__/session.mock";
-import { authentication } from "../../../src/middleware/authentication.middleware";
-import { companyAuthentication } from "../../../src/middleware/company.authentication.middleware";
-import { serviceAvailabilityMiddleware } from "../../../src/middleware/service.availability.middleware";
-import { logger } from "../../../src/utils/logger";
-import { RegistrableBeneficialOwnerKey } from "../../../src/model/update.type.model";
-import { hasOverseasEntity } from "../../../src/middleware/navigation/update/has.overseas.entity.middleware";
-import {
-  ANY_MESSAGE_ERROR,
-  PAGE_TITLE_ERROR,
-  RADIO_BUTTON_NO_SELECTED,
-  RADIO_BUTTON_YES_SELECTED,
-  SERVICE_UNAVAILABLE,
-  UPDATE_REGISTRABLE_BENEFICIAL_OWNER_TITLE } from "../../__mocks__/text.mock";
 import { yesNoResponse } from "@companieshouse/api-sdk-node/dist/services/overseas-entities";
 import { ErrorMessages } from "../../../src/validation/error.messages";
 import { isActiveFeature } from "../../../src/utils/feature.flag";
 import { saveAndContinue } from "../../../src/utils/save.and.continue";
+import { authentication } from "../../../src/middleware/authentication.middleware";
+import { companyAuthentication } from "../../../src/middleware/company.authentication.middleware";
+import { hasOverseasEntity } from "../../../src/middleware/navigation/update/has.overseas.entity.middleware";
+import { updateOverseasEntity } from "../../../src/service/overseas.entities.service";
+import { serviceAvailabilityMiddleware } from "../../../src/middleware/service.availability.middleware";
+import { logger } from "../../../src/utils/logger";
+import { RegistrableBeneficialOwnerKey } from "../../../src/model/update.type.model";
+
+import { getApplicationData, fetchApplicationData, setExtraData } from "../../../src/utils/application.data";
+import {
+  OVERSEAS_ENTITY_ID,
+  APPLICATION_DATA_MOCK,
+  APPLICATION_DATA_MOCK_WITHOUT_UPDATE,
+} from "../../__mocks__/session.mock";
+
+import {
+  UPDATE_STATEMENT_VALIDATION_ERRORS_URL,
+  UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_URL,
+  UPDATE_STATEMENT_VALIDATION_ERRORS_WITH_PARAMS_URL,
+  UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_PAGE,
+  UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_WITH_PARAMS_URL,
+} from "../../../src/config";
+
+import {
+  PAGE_TITLE_ERROR,
+  ANY_MESSAGE_ERROR,
+  SERVICE_UNAVAILABLE,
+  RADIO_BUTTON_NO_SELECTED,
+  RADIO_BUTTON_YES_SELECTED,
+  UPDATE_REGISTRABLE_BENEFICIAL_OWNER_TITLE,
+} from "../../__mocks__/text.mock";
 
 mockJourneyDetectionMiddleware.mockClear();
 mockCsrfProtectionMiddleware.mockClear();
 
 const mockHasOverseasEntity = hasOverseasEntity as jest.Mock;
-mockHasOverseasEntity.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
+mockHasOverseasEntity.mockImplementation((req: Request, res: Response, next: NextFunction) => next());
+
+const mockUpdateOverseasEntity = updateOverseasEntity as jest.Mock;
+mockUpdateOverseasEntity.mockReturnValue(OVERSEAS_ENTITY_ID);
 
 const mockAuthenticationMiddleware = authentication as jest.Mock;
-mockAuthenticationMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
+mockAuthenticationMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next());
 
 const mockCompanyAuthenticationMiddleware = companyAuthentication as jest.Mock;
-mockCompanyAuthenticationMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
+mockCompanyAuthenticationMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next());
 
 const mockServiceAvailabilityMiddleware = serviceAvailabilityMiddleware as jest.Mock;
-mockServiceAvailabilityMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next() );
+mockServiceAvailabilityMiddleware.mockImplementation((req: Request, res: Response, next: NextFunction) => next());
 
 const mockIsActiveFeature = isActiveFeature as jest.Mock;
 mockIsActiveFeature.mockReturnValue(false);
 
 const mockLoggerDebugRequest = logger.debugRequest as jest.Mock;
 const mockGetApplicationData = getApplicationData as jest.Mock;
+const mockFetchApplicationData = fetchApplicationData as jest.Mock;
 const mockSetExtraData = setExtraData as jest.Mock;
 const mockSaveAndContinue = saveAndContinue as jest.Mock;
 
@@ -74,7 +90,7 @@ describe("No change registrable beneficial owner", () => {
   describe("GET tests", () => {
 
     test(`that ${UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_PAGE} page is rendered`, async () => {
-      mockGetApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
+      mockFetchApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
       const resp = await request(app).get(UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_URL);
       expect(resp.status).toEqual(200);
       expect(resp.text).toContain(UPDATE_REGISTRABLE_BENEFICIAL_OWNER_TITLE);
@@ -83,29 +99,27 @@ describe("No change registrable beneficial owner", () => {
     });
 
     test(`renders the ${UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_PAGE} page with radios selected to ${yesNoResponse.Yes}`, async () => {
-      if (APPLICATION_DATA_MOCK.update){
+      if (APPLICATION_DATA_MOCK.update) {
         APPLICATION_DATA_MOCK.update.registrable_beneficial_owner = yesNoResponse.Yes;
       }
-      mockGetApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
+      mockFetchApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
       const resp = await request(app).get(UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_URL);
-
       expect(resp.status).toEqual(200);
       expect(resp.text).toContain(RADIO_BUTTON_YES_SELECTED);
     });
 
     test(`renders the ${UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_PAGE} page with radios selected to ${yesNoResponse.No}`, async () => {
-      if (APPLICATION_DATA_MOCK.update){
+      if (APPLICATION_DATA_MOCK.update) {
         APPLICATION_DATA_MOCK.update.registrable_beneficial_owner = yesNoResponse.No;
       }
-      mockGetApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
+      mockFetchApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
       const resp = await request(app).get(UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_URL);
-
       expect(resp.status).toEqual(200);
       expect(resp.text).toContain(RADIO_BUTTON_NO_SELECTED);
     });
 
     test(`renders the ${UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_PAGE} page with unselected radio button if no update data`, async () => {
-      mockGetApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK_WITHOUT_UPDATE });
+      mockFetchApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK_WITHOUT_UPDATE });
       const resp = await request(app).get(UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_URL);
       expect(resp.status).toEqual(200);
       expect(resp.text).not.toContain(RADIO_BUTTON_NO_SELECTED);
@@ -114,17 +128,17 @@ describe("No change registrable beneficial owner", () => {
     });
 
     test("catch error when rendering the page", async () => {
-      mockLoggerDebugRequest.mockImplementationOnce( () => { throw new Error(ANY_MESSAGE_ERROR); });
+      mockLoggerDebugRequest.mockImplementationOnce(() => { throw new Error(ANY_MESSAGE_ERROR); });
       const resp = await request(app).get(UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_URL);
-
       expect(resp.status).toEqual(500);
       expect(resp.text).toContain(SERVICE_UNAVAILABLE);
     });
   });
 
   describe("POST tests", () => {
+
     test(`Test redirect to ${UPDATE_STATEMENT_VALIDATION_ERRORS_URL} page when 'no reasonable cause' is selected`, async () => {
-      mockGetApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
+      mockFetchApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
       const resp = await request(app)
         .post(UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_URL)
         .send({ [RegistrableBeneficialOwnerKey]: "1" });
@@ -134,22 +148,9 @@ describe("No change registrable beneficial owner", () => {
       expect(mockSetExtraData).toHaveBeenCalledTimes(1);
     });
 
-    test(`redirects to the update-statement-validation-errors page when 'has reasonable cause' is selected`, async () => {
-      mockGetApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
-      const resp = await request(app)
-        .post(UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_URL)
-        .send({ [RegistrableBeneficialOwnerKey]: "0" });
-
-      expect(resp.status).toEqual(302);
-      expect(resp.header.location).toEqual(UPDATE_STATEMENT_VALIDATION_ERRORS_URL);
-      expect(mockSetExtraData).toHaveBeenCalledTimes(1);
-    });
-
-    test(`with statement validation flag on, redirects to the update-statement-validation-errors page when 'has reasonable cause' is selected`, async () => {
-      mockGetApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
-      mockIsActiveFeature.mockReturnValueOnce(true);
-      mockSaveAndContinue.mockReturnValueOnce(Promise.resolve());
-
+    test(`redirects to the update-statement-validation-errors page when 'has reasonable cause' is selected and REDIS_flag is set to OFF`, async () => {
+      mockFetchApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
+      mockIsActiveFeature.mockReturnValue(false);
       const resp = await request(app)
         .post(UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_URL)
         .send({ [RegistrableBeneficialOwnerKey]: "0" });
@@ -158,10 +159,42 @@ describe("No change registrable beneficial owner", () => {
       expect(resp.header.location).toEqual(UPDATE_STATEMENT_VALIDATION_ERRORS_URL);
       expect(mockSetExtraData).toHaveBeenCalledTimes(1);
       expect(mockSaveAndContinue).toHaveBeenCalledTimes(1);
+      expect(mockUpdateOverseasEntity).not.toHaveBeenCalled();
+    });
+
+    test(`redirects to the update-statement-validation-errors page when 'has reasonable cause' is selected and REDIS_flag is set to ON`, async () => {
+      mockFetchApplicationData.mockReturnValue({ ...APPLICATION_DATA_MOCK });
+      mockIsActiveFeature.mockReturnValue(true);
+      const resp = await request(app)
+        .post(UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_WITH_PARAMS_URL)
+        .send({ [RegistrableBeneficialOwnerKey]: "0" });
+
+      expect(resp.status).toEqual(302);
+      expect(resp.header.location).toEqual(UPDATE_STATEMENT_VALIDATION_ERRORS_WITH_PARAMS_URL);
+      expect(mockSetExtraData).toHaveBeenCalledTimes(1);
+      expect(mockUpdateOverseasEntity).toHaveBeenCalledTimes(1);
+      expect(mockSaveAndContinue).not.toHaveBeenCalled();
+    });
+
+    test(`with statement validation flag on, redirects to the update-statement-validation-errors page when 'has reasonable cause' is selected`, async () => {
+      mockGetApplicationData.mockReturnValue({ ...APPLICATION_DATA_MOCK });
+      mockFetchApplicationData.mockReturnValue({ ...APPLICATION_DATA_MOCK });
+      mockIsActiveFeature.mockReturnValue(true);
+      mockSaveAndContinue.mockReturnValue(Promise.resolve());
+
+      const resp = await request(app)
+        .post(UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_WITH_PARAMS_URL)
+        .send({ [RegistrableBeneficialOwnerKey]: "0" });
+
+      expect(resp.status).toEqual(302);
+      expect(resp.header.location).toEqual(UPDATE_STATEMENT_VALIDATION_ERRORS_WITH_PARAMS_URL);
+      expect(mockSetExtraData).toHaveBeenCalledTimes(1);
+      expect(mockUpdateOverseasEntity).toHaveBeenCalledTimes(1);
+      expect(mockSaveAndContinue).not.toHaveBeenCalled();
     });
 
     test("Test validation error is displayed when posting empty object", async () => {
-      mockGetApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
+      mockFetchApplicationData.mockReturnValueOnce({ ...APPLICATION_DATA_MOCK });
       const resp = await request(app).post(UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_URL);
       expect(resp.status).toEqual(200);
       expect(resp.text).toContain(PAGE_TITLE_ERROR);
@@ -170,11 +203,10 @@ describe("No change registrable beneficial owner", () => {
     });
 
     test("catch error when posting the page", async () => {
-      mockLoggerDebugRequest.mockImplementationOnce( () => { throw new Error(ANY_MESSAGE_ERROR); });
+      mockLoggerDebugRequest.mockImplementationOnce(() => { throw new Error(ANY_MESSAGE_ERROR); });
       const resp = await request(app)
         .post(UPDATE_NO_CHANGE_REGISTRABLE_BENEFICIAL_OWNER_URL)
         .send({ [RegistrableBeneficialOwnerKey]: yesNoResponse.Yes });
-
       expect(resp.status).toEqual(500);
       expect(resp.text).toContain(SERVICE_UNAVAILABLE);
     });

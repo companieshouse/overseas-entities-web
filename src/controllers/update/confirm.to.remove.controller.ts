271872d3d8da6af07af997516f420a2216365ff2
mwejuli-ch <mwejuli@companieshouse.gov.uk>
2026-03-03 17:42:48 +0000

    feat: get and save app data using db for the `update-beneficial-owner-individual` page

--- src/controllers/update/confirm.to.remove.controller.ts
+++ src/controllers/update/confirm.to.remove.controller.ts
@@ -1,33 +1,34 @@
 import { NextFunction, Request, Response } from "express";
-
 import { Session } from "@companieshouse/node-session-handler";
-
-import { ApplicationData } from "../../model";
-import { findBoOrMo, getApplicationData } from "../../utils/application.data";
-
 import { logger } from "../../utils/logger";
-import {
-  UPDATE_CONFIRM_TO_REMOVE_PAGE,
-  PARAM_BO_MO_TYPE,
-  PARAM_BENEFICIAL_OWNER_GOV,
-  PARAM_BENEFICIAL_OWNER_INDIVIDUAL,
-  PARAM_BENEFICIAL_OWNER_OTHER,
-  PARAM_MANAGING_OFFICER_CORPORATE,
-  PARAM_MANAGING_OFFICER_INDIVIDUAL,
-  UPDATE_BENEFICIAL_OWNER_TYPE_PAGE,
-  UPDATE_BENEFICIAL_OWNER_TYPE_URL,
-} from "../../config";
-import { DoYouWantToRemoveKey, ID } from "../../model/data.types.model";
-import { removeBeneficialOwnerIndividual } from "../../utils/beneficial.owner.individual";
+import { ApplicationData } from "../../model";
+import { getRedirectUrl } from "../../utils/url";
+import { removeManagingOfficer } from "../../utils/managing.officer.individual";
 import { removeBeneficialOwnerGov } from "../../utils/beneficial.owner.gov";
 import { removeBeneficialOwnerOther } from "../../utils/beneficial.owner.other";
-import { removeManagingOfficer } from "../../utils/managing.officer.individual";
+import { removeBeneficialOwnerIndividual } from "../../utils/beneficial.owner.individual";
 import { removeManagingOfficerCorporate } from "../../utils/managing.officer.corporate";
+import { DoYouWantToRemoveKey, ID } from "../../model/data.types.model";
+import { findBoOrMo, getApplicationData } from "../../utils/application.data";
+
+import {
+  PARAM_BO_MO_TYPE,
+  PARAM_BENEFICIAL_OWNER_GOV,
+  PARAM_BENEFICIAL_OWNER_OTHER,
+  UPDATE_CONFIRM_TO_REMOVE_PAGE,
+  UPDATE_BENEFICIAL_OWNER_TYPE_URL,
+  PARAM_MANAGING_OFFICER_CORPORATE,
+  UPDATE_BENEFICIAL_OWNER_TYPE_PAGE,
+  PARAM_MANAGING_OFFICER_INDIVIDUAL,
+  PARAM_BENEFICIAL_OWNER_INDIVIDUAL,
+  UPDATE_BENEFICIAL_OWNER_TYPE_WITH_PARAMS_URL,
+} from "../../config";
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    logger.debugRequest(req, `${req.method} ${req.route.path}`);
 
+  try {
+
+    logger.debugRequest(req, `${req.method} ${req.route.path}`);
     const session = req.session as Session;
     const appData: ApplicationData = await getApplicationData(session);
 
@@ -43,27 +44,30 @@ export const get = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 export const post = async (req: Request, res: Response, next: NextFunction) => {
+
   try {
+
     logger.debugRequest(req, `DELETE ${req.route.path}`);
 
     if (req.body[DoYouWantToRemoveKey] === '1') {
       switch (req.params[PARAM_BO_MO_TYPE]) {
           case PARAM_BENEFICIAL_OWNER_INDIVIDUAL:
-            return await removeBeneficialOwnerIndividual(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
+            return await removeBeneficialOwnerIndividual(req, res, next, getBoTypeUrl(req));
           case PARAM_BENEFICIAL_OWNER_GOV:
-            return await removeBeneficialOwnerGov(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
+            return await removeBeneficialOwnerGov(req, res, next, getBoTypeUrl(req));
           case PARAM_BENEFICIAL_OWNER_OTHER:
-            return await removeBeneficialOwnerOther(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
+            return await removeBeneficialOwnerOther(req, res, next, getBoTypeUrl(req));
           case PARAM_MANAGING_OFFICER_INDIVIDUAL:
-            return await removeManagingOfficer(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
+            return await removeManagingOfficer(req, res, next, getBoTypeUrl(req));
           case PARAM_MANAGING_OFFICER_CORPORATE:
-            return await removeManagingOfficerCorporate(req, res, next, UPDATE_BENEFICIAL_OWNER_TYPE_URL);
+            return await removeManagingOfficerCorporate(req, res, next, getBoTypeUrl(req));
           default:
             break;
       }
     }
 
-    return res.redirect(UPDATE_BENEFICIAL_OWNER_TYPE_URL);
+    return res.redirect(getBoTypeUrl(req));
+
   } catch (error) {
     logger.errorRequest(req, error);
     next(error);
@@ -76,4 +80,12 @@ const getBoMoName = (appData: ApplicationData, boMoType: string, id: string) => 
   return boMoType === PARAM_BENEFICIAL_OWNER_INDIVIDUAL || boMoType === PARAM_MANAGING_OFFICER_INDIVIDUAL
     ? boMo.first_name + " " + boMo.last_name
     : boMo.name;
+};
+
+const getBoTypeUrl = (req: Request) => {
+  return getRedirectUrl({
+    req,
+    urlWithEntityIds: UPDATE_BENEFICIAL_OWNER_TYPE_WITH_PARAMS_URL,
+    urlWithoutEntityIds: UPDATE_BENEFICIAL_OWNER_TYPE_URL,
+  });
 };
